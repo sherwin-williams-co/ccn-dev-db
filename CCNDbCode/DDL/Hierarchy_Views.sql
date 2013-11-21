c6d26ff7a786d27fa66e45214b025dcc93077e9e
sw\jxc517 <>
2013-11-21 15:38:15 +0000

    Dev - Test Sync : Found missing components Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- CCNDbCode/DDL/Hierarchy_Views.sql
+++ CCNDbCode/DDL/Hierarchy_Views.sql
@@ -1,31 +1,3 @@
-create or replace type hd_type is object(
-             COST_CENTER_CODE       VARCHAR2(10),
-             HRCHY_HDR_NAME         VARCHAR2(100),
-             HRCHY_DTL_LEVEL        VARCHAR2(2),
-             HRCHY_DTL_EFF_DATE     DATE,
-             HRCHY_DTL_EXP_DATE     DATE,
-             HRCHY_DTL_DESC         VARCHAR2(100),
-             HRCHY_DTL_CURR_ROW_VAL VARCHAR2(100));
-/
-create or replace type hd_table is table of hd_type;
-/
-
-  CREATE OR REPLACE FORCE VIEW "COSTCNTR"."HIERARCHY_DETAIL_VIEW" ("COST_CENTER_CODE", "Domain", "Group", "Division", "Area", "District", "City_Sales_Manager", "Zone", "Special_Roles", "Cost_Center", "Effective_Date", "Expiration_Date") AS 
-  SELECT B.COST_CENTER_CODE,
-         MAX(DECODE(HRCHY_DTL_LEVEL,1,HRCHY_DTL_CURR_ROW_VAL)) AS "Domain",
-         MAX(DECODE(HRCHY_DTL_LEVEL,2,HRCHY_DTL_CURR_ROW_VAL)) AS "Group",
-         MAX(DECODE(HRCHY_DTL_LEVEL,3,HRCHY_DTL_CURR_ROW_VAL)) AS "Division",
-         MAX(DECODE(HRCHY_DTL_LEVEL,4,HRCHY_DTL_CURR_ROW_VAL)) AS "Area",
-         MAX(DECODE(HRCHY_DTL_LEVEL,5,HRCHY_DTL_CURR_ROW_VAL)) AS "District",
-         MAX(DECODE(HRCHY_DTL_LEVEL,6,HRCHY_DTL_CURR_ROW_VAL)) AS "City_Sales_Manager",
-         MAX(DECODE(HRCHY_DTL_LEVEL,7,HRCHY_DTL_CURR_ROW_VAL)) AS "Zone",
-         MAX(DECODE(HRCHY_DTL_LEVEL,8,HRCHY_DTL_CURR_ROW_VAL)) AS "Special_Roles",
-         MAX(DECODE(HRCHY_DTL_LEVEL,9,HRCHY_DTL_CURR_ROW_VAL)) AS "Cost_Center",
-         MAX(DECODE(HRCHY_DTL_LEVEL,9,HRCHY_DTL_EFF_DATE)) AS "Effective_Date",
-         MAX(DECODE(HRCHY_DTL_LEVEL,9,HRCHY_DTL_EXP_DATE)) AS "Expiration_Date"
-    FROM COST_CENTER B, TABLE(CCN_HIERARCHY.GLOBAL_HIERARCHY_VIEW_RESULT('GLOBAL_HIERARCHY',B.COST_CENTER_CODE,'9')) A
-   GROUP BY B.COST_CENTER_CODE;
-/
 
   CREATE OR REPLACE FORCE VIEW "COSTCNTR"."HIERARCHY_DETAIL_DESC_VIEW" ("HRCHY_HDR_NAME", "HRCHY_DTL_LEVEL", "HRCHY_DTL_DESC", "HRCHY_DTL_CURR_LVL_VAL") AS 
   SELECT DISTINCT hr.HRCHY_HDR_NAME,
@@ -36,3 +8,139 @@ create or replace type hd_table is table of hd_type;
    WHERE HRCHY_HDR_NAME         = 'GLOBAL_HIERARCHY';
 /
 
+
+  CREATE OR REPLACE FORCE VIEW "COSTCNTR"."HIERARCHY_DETAIL_VIEW" ("STATEMENT_TYPE", "HRCHY_HDR_NAME", "COST_CENTER_CODE", "HRCHY_DTL_EFF_DATE", "HRCHY_DTL_EXP_DATE", "DOMAIN", "GROUP", "DIVISION", "LEGACY_DIVISION", "AREA", "DISTRICT", "CITY_SALES_MANAGER", "ZONE", "SPECIAL_ROLES", "COST_CENTER", "DOMAIN_NAME", "GROUP_NAME", "DIVISION_NAME", "LEGACY_DIVISION_NAME", "AREA_NAME", "DISTRICT_NAME", "CITY_SALES_MANAGER_NAME", "ZONE_NAME", "SPECIAL_ROLES_NAME", "COST_CENTER_NAME") AS 
+  select statement_type,
+       HRCHY_HDR_NAME,
+       HRCHY_DTL_CURR_ROW_VAL COST_CENTER_CODE,
+       HRCHY_DTL_EFF_DATE,
+       HRCHY_DTL_EXP_DATE,
+       --HRCHY_HDR_LVL_NBR,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,1,val)) AS Domain,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,2,val)) AS "GROUP",
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,3,val)) AS Division,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'LEGACY_GL_DIVISION' THEN 4
+                                      ELSE NULL
+                                      END,val)) AS Legacy_Division,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 4
+                                      WHEN 'ADMIN_TO_SALES_AREA' THEN 4
+                                      WHEN 'ALTERNATE_DAD' THEN 4
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 4
+                                      ELSE NULL
+                                      END,val)) AS Area,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 5
+                                      WHEN 'ALTERNATE_DAD' THEN 5
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 5
+                                      ELSE NULL
+                                      END,val)) AS District,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 6
+                                      WHEN 'ALTERNATE_DAD' THEN 6
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 6
+                                      ELSE NULL
+                                      END,val)) AS City_Sales_Manager,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 7
+                                      WHEN 'ALTERNATE_DAD' THEN 7
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 7
+                                      ELSE NULL
+                                      END,val)) AS "ZONE",
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 8
+                                      WHEN 'ALTERNATE_DAD' THEN 8
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 8
+                                      ELSE NULL
+                                      END,val)) AS Special_Roles,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 9
+                                      WHEN 'ALTERNATE_DAD' THEN 9
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 9
+                                      WHEN 'LEGACY_GL_DIVISION' THEN 5
+                                      WHEN 'ADMIN_TO_SALES_AREA' THEN 5
+                                      WHEN 'FACTS_DIVISION' THEN 4
+                                      WHEN 'ADMIN_TO_SALES_DIVISION' THEN 4
+                                      ELSE NULL
+                                      END,val)) AS Cost_Center,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,1,description)) AS Domain_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,2,description)) AS Group_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,3,description)) AS Division_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'LEGACY_GL_DIVISION' THEN 4
+                                      ELSE NULL
+                                      END,description)) AS Legacy_Division_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 4
+                                      WHEN 'ADMIN_TO_SALES_AREA' THEN 4
+                                      WHEN 'ALTERNATE_DAD' THEN 4
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 4
+                                      ELSE NULL
+                                      END,description)) AS Area_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 5
+                                      WHEN 'ALTERNATE_DAD' THEN 5
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 5
+                                      ELSE NULL
+                                      END,description)) AS District_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 6
+                                      WHEN 'ALTERNATE_DAD' THEN 6
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 6
+                                      ELSE NULL
+                                      END,description)) AS City_Sales_Manager_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 7
+                                      WHEN 'ALTERNATE_DAD' THEN 7
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 7
+                                      ELSE NULL
+                                      END,description)) AS Zone_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 8
+                                      WHEN 'ALTERNATE_DAD' THEN 8
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 8
+                                      ELSE NULL
+                                      END,description)) AS Special_Roles_Name,
+         MAX(DECODE(HRCHY_HDR_LVL_NBR,CASE HRCHY_HDR_NAME
+                                      WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 9
+                                      WHEN 'ALTERNATE_DAD' THEN 9
+                                      WHEN 'GLOBAL_HIERARCHY' THEN 9
+                                      WHEN 'LEGACY_GL_DIVISION' THEN 5
+                                      WHEN 'ADMIN_TO_SALES_AREA' THEN 5
+                                      WHEN 'FACTS_DIVISION' THEN 4
+                                      WHEN 'ADMIN_TO_SALES_DIVISION' THEN 4
+                                      ELSE NULL
+                                      END,description)) AS Cost_Center_Name
+ from (
+select --hd.hrchy_dtl_curr_lvl_val,
+       substr(hd.hrchy_dtl_curr_lvl_val,1 + (select NVL(sum(LVL_VALUE_SIZE),0)
+                                               from hierarchy_description
+                                              where HRCHY_HDR_LVL_NBR < hdesc.HRCHY_HDR_LVL_NBR
+                                                and HRCHY_HDR_NAME = hd.HRCHY_HDR_NAME),hdesc.lvl_value_size) val,
+       (select HRCHY_DTL_DESC
+          from hierarchy_detail
+         where HRCHY_DTL_CURR_LVL_VAL||'' = substr(hd.hrchy_dtl_curr_lvl_val,1,(select sum(LVL_VALUE_SIZE)
+                                                                             from hierarchy_description
+                                                                            where HRCHY_HDR_LVL_NBR <= hdesc.HRCHY_HDR_LVL_NBR
+                                                                              and HRCHY_HDR_NAME = hd.HRCHY_HDR_NAME))
+           and HRCHY_HDR_NAME = hd.HRCHY_HDR_NAME
+           and HRCHY_DTL_LEVEL = hdesc.HRCHY_HDR_LVL_NBR
+           and rownum < 2) description,
+       cc.statement_type,
+       hd.HRCHY_HDR_NAME,
+       hdesc.HRCHY_HDR_LVL_NBR,
+       hd.HRCHY_DTL_CURR_ROW_VAL,
+       hd.HRCHY_DTL_EFF_DATE,
+       hd.HRCHY_DTL_EXP_DATE
+  from hierarchy_description hdesc,
+       hierarchy_detail hd,
+       cost_center cc
+ where cc.cost_center_code = hd.HRCHY_DTL_CURR_ROW_VAL
+   and hd.HRCHY_HDR_NAME = hdesc.HRCHY_HDR_NAME
+   and nvl(hd.hrchy_dtl_next_lvl_val,'~~~') = '~~~'
+   --and cc.cost_center_code = '708090'
+   )
+   --where HRCHY_DTL_CURR_ROW_VAL = '708092'
+ group by statement_type,HRCHY_HDR_NAME,HRCHY_DTL_CURR_ROW_VAL,HRCHY_DTL_EFF_DATE, HRCHY_DTL_EXP_DATE;
+/
