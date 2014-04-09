9ff4abb56f8d226828abfcf53a5c75d0620f0ef8
sw\jxc517 <>
2014-04-09 18:25:53 +0000

    Indentatoin corrections Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- CCNDbCode/DDL/CCN_PHONE_INFO_VW.sql
+++ CCNDbCode/DDL/CCN_PHONE_INFO_VW.sql
@@ -1,21 +1,17 @@
-CREATE OR REPLACE FORCE VIEW COSTCNTR.CCN_PHONE_INFO_VW
-(COST_CENTER_CODE, COST_CENTER_DESCRIPTION, PRIMARY_PHONE_NUMBER, SECONDARY_PHONE_NUMBER, FAX_PHONE_NUMBER)
+CREATE OR REPLACE VIEW COSTCNTR.CCN_PHONE_INFO_VW 
 AS 
-SELECT DISTINCT cc.cost_center_code   cost_center_code,
-                cc.cost_center_name   cost_center_description,
-       (SELECT P.PHONE_AREA_CODE  P.phone_number
-        FROM   costcntr.phone P
-        WHERE  P.phone_number_type = 'PRI'
-               AND P.cost_center_code = cc.cost_center_code)
-       primary_phone_number,
-       (SELECT P.PHONE_AREA_CODE P.phone_number
-        FROM   costcntr.phone P
-        WHERE  P.phone_number_type = 'SCD'
-               AND P.cost_center_code = cc.cost_center_code)
-       secondary_phone_number,
-       (SELECT P.PHONE_AREA_CODE P.phone_number
-        FROM   costcntr.phone P
-        WHERE  P.phone_number_type = 'FAX'
-               AND P.cost_center_code = cc.cost_center_code) 
-               fax_phone_number
-FROM   costcntr.cost_center cc;
+SELECT DISTINCT CC.COST_CENTER_CODE   COST_CENTER_CODE,
+                CC.COST_CENTER_NAME   COST_CENTER_DESCRIPTION,
+       (SELECT P.PHONE_AREA_CODE  P.PHONE_NUMBER
+          FROM COSTCNTR.PHONE P
+         WHERE P.PHONE_NUMBER_TYPE = 'PRI'
+           AND P.COST_CENTER_CODE = CC.COST_CENTER_CODE) PRIMARY_PHONE_NUMBER,
+       (SELECT P.PHONE_AREA_CODE P.PHONE_NUMBER
+          FROM COSTCNTR.PHONE P
+         WHERE P.PHONE_NUMBER_TYPE = 'SCD'
+           AND P.COST_CENTER_CODE = CC.COST_CENTER_CODE) SECONDARY_PHONE_NUMBER,
+       (SELECT P.PHONE_AREA_CODE P.PHONE_NUMBER
+          FROM COSTCNTR.PHONE P
+         WHERE P.PHONE_NUMBER_TYPE = 'FAX'
+           AND P.COST_CENTER_CODE = CC.COST_CENTER_CODE) FAX_PHONE_NUMBER
+  FROM COSTCNTR.COST_CENTER CC;
