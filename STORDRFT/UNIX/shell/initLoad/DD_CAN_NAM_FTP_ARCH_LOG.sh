2148eea75bf07e64243c7d01c7d66a5d7c1fcf4c
sw\axk326 <>
2014-11-12 18:46:17 +0000

    Shell scripts to have individual logging as well as date field modified to remove the weekend logic, so it can be setup on daily basis. Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- STORDRFT/UNIX/shell/initLoad/DD_CAN_NAM_FTP_ARCH_LOG.sh
+++ STORDRFT/UNIX/shell/initLoad/DD_CAN_NAM_FTP_ARCH_LOG.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 #################################################################
-# Script name   : DLY_DRAFT_MAINT.sh
+# Script name   : DD_CAN_NAM_FTP_ARCH_LOG.sh
 #
-# Description   : This script is to run the daily maintenance load for AutoMotive and Non AutoMotive
+# Description   : This script is to run the FTP, Archiving and Logging for daily Non Automotive Canada issue files
 #
-# Created  : 10/22/2014 jxc517 CCN Project Team.....
+# Created  : 11/12/2014 axk326 CCN Project Team.....
 # Modified :
 #################################################################
 # below command will get the path for stordrft.config respective to the environment from which it is run from
 . /app/stordrft/host.sh
 
-proc_name="DLY_DRAFT_MAINT"
+proc_name="DD_CAN_NAM_FTP_ARCH_LOG"
+proc_name1="DD_CAN_NAM_FTP_ARCH"
+LOGDIR=$HOME/dailyLoad/logs
+TimeStamp=`date '+%Y%m%d%H%M%S'`
 TIME=`date +"%H:%M:%S"`
 DATE=`date +"%m/%d/%Y"`
 echo "Processing Started for $proc_name at $TIME on $DATE"
 
-./DLY_MAINT_DRAFT_US_AM.sh
-./DLY_MAINT_DRAFT_US_NAM.sh
+./DD_CAN_NAM_FTP_ARCH.sh >> $LOGDIR/$proc_name1"_"$TimeStamp.log 
 
 ############################################################################
 #                           ERROR STATUS CHECK 
--- STORDRFT/UNIX/shell/initLoad/DLY_DRAFT_LOAD.sh
+++ STORDRFT/UNIX/shell/initLoad/DLY_DRAFT_LOAD.sh
@@ -12,12 +12,6 @@
 . /app/stordrft/host.sh
 
 proc_name="DLY_DRAFT_LOAD"
-proc_name1="DD_US_AM_FTP_ARCH"
-proc_name2="DD_US_NAM_FTP_ARCH"
-proc_name3="DD_CAN_AM_FTP_ARCH"
-proc_name4="DD_CAN_NAM_FTP_ARCH"
-LOGDIR=$HOME/dailyLoad/logs
-TimeStamp=`date '+%Y%m%d%H%M%S'`
 TIME=`date +"%H:%M:%S"`
 DATE=`date +"%m/%d/%Y"`
 echo "Processing Started for $proc_name at $TIME on $DATE"
@@ -26,10 +20,6 @@ echo "Processing Started for $proc_name at $TIME on $DATE"
 ./DLY_DRAFT_US_NAM.sh
 ./DLY_DRAFT_CAN_AM.sh
 ./DLY_DRAFT_CAN_NAM.sh
-./DD_US_AM_FTP_ARCH.sh >> $LOGDIR/$proc_name1"_"$TimeStamp.log 
-./DD_US_NAM_FTP_ARCH.sh >> $LOGDIR/$proc_name2"_"$TimeStamp.log 
-./DD_CAN_AM_FTP_ARCH.sh >> $LOGDIR/$proc_name3"_"$TimeStamp.log 
-./DD_CAN_NAM_FTP_ARCH.sh >> $LOGDIR/$proc_name4"_"$TimeStamp.log 
 
 ############################################################################
 #                           ERROR STATUS CHECK 
