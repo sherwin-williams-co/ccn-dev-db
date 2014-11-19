7bdf2824057d2c02b2e9bc806a57ecd99621974d
nxk927 <>
2014-11-19 20:43:18 +0000

    customer account number update Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

---
+++ STORDRFT/UNIX/shell/initLoad/plgain_ftp.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# plgain_FTP.sh
+# Use to ftp to mainframe to be loaded to Mobius manframe
+
+. /app/strdrft/dataloadInfo.txt
+
+cd /app/strdrft/sdReport/reports/final
+
+# ftp to mainframe
+ftp -n ${mainframe_host} <<FTP_MF
+quote USER ${mainframe_user}
+quote PASS ${mainframe_pw}
+put plreport.txt 'STST.STBD3340(+1)'
+
+bye
+FTP_MF
+
+exit 0
