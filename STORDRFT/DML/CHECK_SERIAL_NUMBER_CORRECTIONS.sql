d214b9cf66a5c97af7bfaf4f8af06e3832c0c9c4
sw\axk326 <>
2014-12-10 16:04:54 +0000

    MADE CHANGES FOR THE DELETE STATEMENT Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- STORDRFT/DML/CHECK_SERIAL_NUMBER_CORRECTIONS.sql
+++ STORDRFT/DML/CHECK_SERIAL_NUMBER_CORRECTIONS.sql
@@ -3,60 +3,52 @@ THIS DML will take care of the check_serial_number corrections
 CREATED : axk326 CCN Project team....
 MODIFIED: 
 **********************************************************/
--- redundant check_serial_numbers and their counts
-select COST_CENTER_CODE, CHECK_SERIAL_NUMBER, count(*) 
-  from store_drafts
-  group by COST_CENTER_CODE, CHECK_SERIAL_NUMBER
-  having count(*)>1;
+-- redundant check_serial_numbers and their counts from STORE_DRAFTS table
+SELECT COST_CENTER_CODE, CHECK_SERIAL_NUMBER, count(*) 
+  FROM store_drafts
+  GROUP BY COST_CENTER_CODE, CHECK_SERIAL_NUMBER
+  HAVING COUNT(*)>1;
 
--- Complete record set for a check_serial_number from store_drafts table 
-select * from STORE_DRAFTS where CHECK_SERIAL_NUMBER in 
-(select CHECK_SERIAL_NUMBER 
-   from store_drafts
-  group by CHECK_SERIAL_NUMBER
-  having count(*)>1)
-order by 2 desc;
+-- DISPLAY THE RECORDS IN STORE-DRAFTS
+SELECT * FROM STORE_DRAFTS
+ WHERE CHECK_SERIAL_NUMBER IN (SELECT CHECK_SERIAL_NUMBER 
+                                 FROM store_drafts
+                             GROUP BY COST_CENTER_CODE, 
+                                      CHECK_SERIAL_NUMBER
+                               HAVING COUNT(*)>1)
+ ORDER BY 2 DESC;
 
--- Complete record set for a check_serial_number from store_drafts_detail table 
-select * from STORE_DRAFTS_DETAIL where CHECK_SERIAL_NUMBER in 
-(select CHECK_SERIAL_NUMBER 
-   from store_drafts
-  group by CHECK_SERIAL_NUMBER
-  having count(*)>1)
-order by 3 desc;
+-- Complete record set for a check_serial_number from STORE_DRAFTS_DETAIL table 
+SELECT * FROM STORE_DRAFTS_DETAIL 
+ WHERE CHECK_SERIAL_NUMBER IN (SELECT CHECK_SERIAL_NUMBER 
+                                 FROM STORE_DRAFTS
+                             GROUP BY CHECK_SERIAL_NUMBER
+                               HAVING COUNT(*)>1)
+ORDER BY 3 DESC;
 
--- To get the max of transaction_date from the redundant check_serial_number records
-select CHECK_SERIAL_NUMBER, max(STORE_DRAFTS.TRANSACTION_DATE) from store_drafts group by CHECK_SERIAL_NUMBER having count(*)>1;
+-- SELECT the records from store_drafts that needs to be deleted
+SELECT sd.cost_center_code, sd.check_serial_number, sd.transaction_date
+  FROM store_drafts sd, 
+       STORE_DRAFTS_DETAIL sdd
+  WHERE SD.COST_CENTER_CODE    = SDD.COST_CENTER_CODE
+    AND SD.CHECK_SERIAL_NUMBER = SDD.CHECK_SERIAL_NUMBER
+    AND SD.TRANSACTION_DATE    <> SDD.TRANSACTION_DATE
+    AND SD.CHECK_SERIAL_NUMBER IN (SELECT CHECK_SERIAL_NUMBER
+                                     FROM STORE_DRAFTS
+                                 GROUP BY COST_CENTER_CODE, 
+                                          CHECK_SERIAL_NUMBER
+                                   HAVING COUNT(*)>1);
 
-
--- To get the min of transaction_date from the redundant check_serial_number records
-select CHECK_SERIAL_NUMBER, MIN(TRANSACTION_DATE) 
-                          from store_drafts 
-                          group by CHECK_SERIAL_NUMBER 
-                          having count(*)>1 
-                          order by 1 desc;
-
--- Delete records from store_drafts_detail table
-DELETE FROM STORE_DRAFTS_DETAIL 
-where CHECK_SERIAL_NUMBER in (select CHECK_SERIAL_NUMBER 
-                                from store_drafts
-                                group by CHECK_SERIAL_NUMBER
-                                having count(*)>1);
-
--- Delete records from Store_drafts table
-DECLARE
-  CURSOR TEMP_CUR IS 
-  select CHECK_SERIAL_NUMBER, 
-         MIN(TRANSACTION_DATE) 
-                          from store_drafts 
-                          group by CHECK_SERIAL_NUMBER 
-                          having count(*)>1;
-BEGIN
-  FOR REC IN TEMP_CUR LOOP
-    DELETE FROM STORE_DRAFTS
-     WHERE check_serial_number  = rec.check_serial_number
-       and transaction_date     = rec.transaction_date;
-   END LOOP;
-END;
-                     
-                          
+-- DELETE redundant records from STORE_DRAFTS table which are not available in STORE_DRAFTS_DETAIL table
+DELETE FROM store_drafts where (cost_center_code, check_serial_number, transaction_date) in 
+(select sd.cost_center_code, sd.check_serial_number, sd.transaction_date
+  from store_drafts sd, 
+       STORE_DRAFTS_DETAIL sdd
+  where SD.COST_CENTER_CODE    = SDD.COST_CENTER_CODE
+    and SD.CHECK_SERIAL_NUMBER = SDD.CHECK_SERIAL_NUMBER
+    and SD.TRANSACTION_DATE    <> SDD.TRANSACTION_DATE
+    and SD.CHECK_SERIAL_NUMBER in (select CHECK_SERIAL_NUMBER
+                                     from store_drafts
+                                 group by COST_CENTER_CODE, 
+                                          CHECK_SERIAL_NUMBER
+                                   having count(*)>1));
