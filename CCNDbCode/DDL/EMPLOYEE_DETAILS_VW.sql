fde71f6190e72a338dfc0b577495ab824833fae9
sw\sxt410 <>
2015-01-22 20:56:37 +0000

    View for Store information and employee information Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

---
+++ CCNDbCode/DDL/EMPLOYEE_DETAILS_VW.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE VIEW EMPLOYEE_DETAILS_VW
+AS 
+SELECT 
+/* Comments
+This view holds data about an Employee
+Such as Employee Number, Cost Center, Employee Name and Job title
+who's Payroll status is Active.
+Created  : 12/10/2014 SXT410 CCN project
+Modified : 
+*/
+      EMPLOYEE_NUMBER,
+      COST_CENTER_CODE,      
+      EMPLOYEE_NAME,
+      JOB_TITLE
+FROM  EMPLOYEE_DETAILS
+WHERE UPPER(EMP_PAYROLL_STATUS) = 'ACTIVE';
