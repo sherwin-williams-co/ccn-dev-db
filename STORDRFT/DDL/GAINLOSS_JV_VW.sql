b2762029886e56ff47b9f91b79534bfc4ae7fa78
sw\sxt410 <>
2015-02-04 16:15:59 +0000

    Added comments, Grants and Synonym for GainLoss_jv and Paid_jv Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- STORDRFT/DDL/GAINLOSS_JV_VW.sql
+++ STORDRFT/DDL/GAINLOSS_JV_VW.sql
@@ -2,10 +2,12 @@ CREATE OR REPLACE VIEW GAINLOSS_JV_VW
 AS 
 SELECT 
 /*******************************************************************************
-This view holds all the required gain and loss data for the cost center.
+This view holds all the required monthly Gain and Loss data for the cost center.
 
-Created  : 02/04/2015 SXT410 CCN Project...
+Created  : 02/04/2015 SXT410 CCN Project Team...
 Modified : 
 *******************************************************************************/
-*
-FROM GAINLOSS_JV;
+       COST_CENTER_CODE,
+       NET_AMOUNT,
+       BOOK_DATE
+  FROM GAINLOSS_JV;
