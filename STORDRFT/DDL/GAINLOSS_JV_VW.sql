2037da7b3a90d794641709f2971ee5b425c91682
sw\sxt410 <>
2015-02-04 14:28:49 +0000

    GAINLOSS_JV_VW Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

---
+++ STORDRFT/DDL/GAINLOSS_JV_VW.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW GAINLOSS_JV_VW
+AS 
+SELECT 
+/*******************************************************************************
+This view holds all the required gain and loss data for the cost center.
+
+Created  : NXK927 CCN Project...
+Modified : 02/04/2015 SXT410 Added Comments.
+*******************************************************************************/
+*
+FROM GAINLOSS_JV;
