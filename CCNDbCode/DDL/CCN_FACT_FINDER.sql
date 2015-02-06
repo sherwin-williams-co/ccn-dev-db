2171d32754f1c9f0aafda11f8ba049d47307f4ba
nxk927 <>
2015-02-06 18:13:47 +0000

    function added for ccn_fact_finder_view Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- CCNDbCode/DDL/CCN_FACT_FINDER.sql
+++ CCNDbCode/DDL/CCN_FACT_FINDER.sql
@@ -1,136 +1,71 @@
-CREATE OR REPLACE VIEW CCN_FACT_FINDER
-AS 
-SELECT 
-/*******************************************************************************
-This View holds all the required data for a cost center, its name, category, domain,
-group, roles, sales_volume_class  it is associated with it.
 
-Created  : 01/28/2015 axk326 CCN Project  
-Modified : 01/29/2015 axk326 CCN Project added few more columns to existing view 
-*******************************************************************************/  
-	   COST_CENTER,
-       COST_CENTER_NAME,
-       FACTS_DIVISION,
-       CLOSE_DATE,
-       COST_CENTER_CATEGORY,
-       STATEMENT_TYPE,
-       MAX(DECODE(HRCHY_HDR_LVL_NBR, 1, VAL)) AS "DOMAIN",
-       MAX(DECODE(HRCHY_HDR_LVL_NBR, 2, VAL)) AS "GROUP",
-       MAX(DECODE(HRCHY_HDR_LVL_NBR, CASE HRCHY_HDR_NAME
-                                     WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 8
-                                     WHEN 'ALTERNATE_DAD' THEN 8
-                                     WHEN 'GLOBAL_HIERARCHY' THEN 8
-                                     ELSE NULL
-                                     END, VAL)) AS "SPECIAL_ROLES",
-	   MAX(DECODE(HRCHY_HDR_LVL_NBR, CASE HRCHY_HDR_NAME
-								     WHEN 'ADMIN_TO_SALES_DISTRICT' THEN 7
-								     WHEN 'ALTERNATE_DAD' THEN 7
-								     WHEN 'GLOBAL_HIERARCHY' THEN 7
-								     ELSE NULL
-								     END,val)) AS "ZONE",
-       ACQUISITION_CODE,
-       DIVISION,
-       AREA,
-       DISTRICT,
-       CITY_SALES_NUM,
-       STATUS,
-       TYPE,
-       SALES_VOLUME_CLASS,
-       MANAGER_FIRST_NAME,
-       MANAGER_LAST_NAME,
-       TERR_HOME_STORE,
-       ALT_DIVISION,
-       ALT_AREA,
-       ALT_DISTRICT,
-       DIST_NAME
-FROM   (SELECT C.COST_CENTER_CODE
-               COST_CENTER,
-               C.COST_CENTER_NAME,
-               (SELECT SUBSTR(HRCHY_DTL_PREV_LVL_VAL, 5)
-                FROM   HIERARCHY_DETAIL
-                WHERE  HRCHY_DTL_CURR_ROW_VAL = C.COST_CENTER_CODE
-                       AND HRCHY_HDR_NAME = 'FACTS_DIVISION')FACTS_DIVISION,
-               C.CLOSE_DATE,
-               C.CATEGORY COST_CENTER_CATEGORY,
-               C.STATEMENT_TYPE,
-               HDESC.HRCHY_HDR_LVL_NBR,
-               HD.HRCHY_HDR_NAME,                                   
-               SUBSTR(HD.HRCHY_DTL_CURR_LVL_VAL, 1 + (SELECT NVL(SUM(LVL_VALUE_SIZE), 0)
-                                                      FROM   HIERARCHY_DESCRIPTION
-                                                      WHERE  HRCHY_HDR_LVL_NBR < HDESC.HRCHY_HDR_LVL_NBR
-                                                      AND HRCHY_HDR_NAME = HD.HRCHY_HDR_NAME), HDESC.LVL_VALUE_SIZE)  VAL,
-               C.ACQUISITION_CODE,
-               SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 5, 2) DIVISION,
-               SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 7, 2) AREA,
-               SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 9, 2) DISTRICT,
-               SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 11, 2)CITY_SALES_NUM,
-               STATUS_CODE STATUS,
-               TYPE_CODE   TYPE,
-               (SELECT SALES_VOL_CLASS_CODE
-                FROM   STORE
-                WHERE  COST_CENTER_CODE = C.COST_CENTER_CODE) SALES_VOLUME_CLASS,
-               UPPER(ED.FIRST_NAME) MANAGER_FIRST_NAME,
-               UPPER(ED.LAST_NAME) MANAGER_LAST_NAME,
-               (SELECT HOME_STORE
-                FROM   TERRITORY
-                WHERE  COST_CENTER_CODE = C.COST_CENTER_CODE) TERR_HOME_STORE,
-               (SELECT SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 5, 2)
-                FROM   HIERARCHY_DETAIL
-                WHERE  HRCHY_HDR_NAME = 'ALTERNATE_DAD'
-                   AND HRCHY_DTL_LEVEL = '9'
-                   AND HRCHY_DTL_CURR_ROW_VAL = C.COST_CENTER_CODE) ALT_DIVISION,
-               (SELECT SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 7, 2)
-                FROM   HIERARCHY_DETAIL
-                WHERE  HRCHY_HDR_NAME = 'ALTERNATE_DAD'
-                   AND HRCHY_DTL_LEVEL = '9'
-                   AND HRCHY_DTL_CURR_ROW_VAL = C.COST_CENTER_CODE) ALT_AREA,
-               (SELECT SUBSTR(HD.HRCHY_DTL_PREV_LVL_VAL, 9, 2)
-                FROM   HIERARCHY_DETAIL
-                WHERE  HRCHY_HDR_NAME = 'ALTERNATE_DAD'
-                   AND HRCHY_DTL_LEVEL = '9'
-                   AND HRCHY_DTL_CURR_ROW_VAL = C.COST_CENTER_CODE) ALT_DISTRICT,
-               (SELECT RTRIM(HRCHY_DTL_DESC, 'DST')
-                FROM   HIERARCHY_DETAIL
-                WHERE  HRCHY_HDR_NAME = 'ADMIN_TO_SALES_DISTRICT'
-                   AND HRCHY_DTL_LEVEL = '5'
-                   AND HRCHY_DTL_CURR_LVL_VAL = SUBSTR(HD.HRCHY_DTL_CURR_LVL_VAL, 1, 10)) DIST_NAME
-        FROM   HIERARCHY_DETAIL HD,
-               HIERARCHY_DESCRIPTION HDESC,
-               COST_CENTER C,
-               STATUS S,
-               TYPE T,
-               EMPLOYEE_DETAILS ED
-        WHERE  HD.HRCHY_DTL_CURR_ROW_VAL = C.COST_CENTER_CODE
-               AND HD.HRCHY_HDR_NAME = HDESC.HRCHY_HDR_NAME
-               AND NVL(HD.HRCHY_DTL_NEXT_LVL_VAL, '~~~') = '~~~'
-               AND HD.HRCHY_HDR_NAME = 'GLOBAL_HIERARCHY'
-               AND C.COST_CENTER_CODE = S.COST_CENTER_CODE
-               AND S.COST_CENTER_CODE = T.COST_CENTER_CODE
-               AND C.COST_CENTER_CODE = T.COST_CENTER_CODE
-               AND ED.COST_CENTER_CODE = C.COST_CENTER_CODE
-               AND UPPER(ED.EMP_PAYROLL_STATUS) = 'ACTIVE'
-               AND UPPER(ED.JOB_TITLE) = 'STORE MGR'
-               AND ED.TERM_DATE IS NULL
-               AND S.EXPIRATION_DATE IS NULL
-               AND T.EXPIRATION_DATE IS NULL)
-GROUP  BY COST_CENTER,
-          COST_CENTER_NAME,
-          FACTS_DIVISION,
-          CLOSE_DATE,
-          COST_CENTER_CATEGORY,
-          STATEMENT_TYPE,
-          ACQUISITION_CODE,
-          DIVISION,
-          AREA,
-          DISTRICT,
-          CITY_SALES_NUM,
-          STATUS,
-          TYPE,
-          SALES_VOLUME_CLASS,
-          MANAGER_FIRST_NAME,
-          MANAGER_LAST_NAME,
-          TERR_HOME_STORE,
-          ALT_DIVISION,
-          ALT_AREA,
-          ALT_DISTRICT,
-          DIST_NAME;
+  CREATE OR REPLACE VIEW "CCN_FACT_FINDER" ("COST_CENTER_CODE", "COST_CENTER_NAME", "CLOSE_DATE", "COST_CENTER_CATEGORY", "STATEMENT_TYPE", "ACQUISITION_CODE", "FACTS_DIVISION", "DOMAIN", "GROUP ", "DIVISION", "AREA", "DISTRICT", "CITY_SALES_NUM", "ZONE", "SPECIAL_ROLES", "ALT_DIVISION", "ALT_AREA", "ALT_DISTRICT", "ADMIN_DISTRICT", "DIST_NAME", "STATUS_CODE", "TYPE", "SALES_VOLUME_CLASS", "MANAGER_FIRST_NAME", "MANAGER_LAST_NAME", "TERR_HOME_STORE") AS 
+  SELECT
+  /*******************************************************************************
+  This View holds all the required data for a cost center, its name, category, domain,
+  group, roles, sales_volume_class  it is associated with it.
+
+  Created  : 01/28/2015 nxk927 CCN Project  
+  Modified : 01/29/2015 axk326 CCN Project added few more columns to existing view 
+                      eliminated join conditions as well
+           : 02/03/2015 nxk927 / axk326 CCN Project optimized the select query 
+             that creates the view
+  *******************************************************************************/ 
+         COST_CENTER_CODE,
+         COST_CENTER_NAME,
+         CLOSE_DATE,
+         CATEGORY COST_CENTER_CATEGORY,
+         STATEMENT_TYPE,
+         ACQUISITION_CODE,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('FACTS_DIVISION', 'DIVISION',C.COST_CENTER_CODE ) FACTS_DIVISION,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'DOMAIN',C.COST_CENTER_CODE ) "DOMAIN",
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'GROUP',C.COST_CENTER_CODE ) "GROUP ",
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'DIVISION',C.COST_CENTER_CODE ) DIVISION,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'AREA',C.COST_CENTER_CODE ) AREA,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'DISTRICT',C.COST_CENTER_CODE ) DISTRICT,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'CITY/SALES MANAGER', C.COST_CENTER_CODE ) CITY_SALES_NUM,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'ZONE',C.COST_CENTER_CODE ) "ZONE",
+         COMMON_TOOLS.GET_CURR_LEV_VAL('GLOBAL_HIERARCHY', 'SPECIAL ROLES',C.COST_CENTER_CODE ) SPECIAL_ROLES,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('ALTERNATE_DAD', 'DIVISION', C.COST_CENTER_CODE ) ALT_DIVISION,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('ALTERNATE_DAD', 'AREA',C.COST_CENTER_CODE ) ALT_AREA,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('ALTERNATE_DAD', 'DISTRICT',C.COST_CENTER_CODE ) ALT_DISTRICT,
+         COMMON_TOOLS.GET_CURR_LEV_VAL('ADMIN_TO_SALES_DISTRICT', 'DISTRICT',C.COST_CENTER_CODE ) ADMIN_DISTRICT,
+         (SELECT RTRIM(HRCHY_DTL_DESC, 'DST')
+            FROM HIERARCHY_DETAIL
+           WHERE HRCHY_HDR_NAME = 'ADMIN_TO_SALES_DISTRICT'
+             AND HRCHY_DTL_LEVEL = '9'
+             AND HRCHY_DTL_CURR_ROW_VAL = COST_CENTER_CODE) AS DIST_NAME,
+         (SELECT STATUS_CODE
+            FROM STATUS
+           WHERE COST_CENTER_CODE = C.COST_CENTER_CODE
+             AND EXPIRATION_DATE IS NULL) STATUS_CODE,
+         (SELECT TYPE_CODE
+            FROM TYPE
+           WHERE COST_CENTER_CODE = C.COST_CENTER_CODE
+             AND EXPIRATION_DATE IS NULL) TYPE,
+         (SELECT SALES_VOL_CLASS_CODE
+            FROM STORE
+           WHERE COST_CENTER_CODE = C.COST_CENTER_CODE) SALES_VOLUME_CLASS,
+         (SELECT UPPER(FIRST_NAME)
+            FROM EMPLOYEE_DETAILS
+           WHERE COST_CENTER_CODE = C.COST_CENTER_CODE
+             AND UPPER(EMP_PAYROLL_STATUS) = UPPER('ACTIVE')
+             AND UPPER(JOB_TITLE) IN (SELECT JOB_TITLE 
+                                        FROM JOB_TITLE_GROUP 
+                                       WHERE NONMGR_IND = 'Y')
+             AND ROWNUM < 2) MANAGER_FIRST_NAME,
+         (SELECT UPPER(LAST_NAME)
+            FROM EMPLOYEE_DETAILS
+           WHERE COST_CENTER_CODE = C.COST_CENTER_CODE
+             AND UPPER(EMP_PAYROLL_STATUS) = UPPER('ACTIVE')
+             AND UPPER(JOB_TITLE) IN (SELECT JOB_TITLE 
+                                        FROM JOB_TITLE_GROUP 
+                                       WHERE NONMGR_IND = 'Y')
+             AND ROWNUM < 2) MANAGER_LAST_NAME,
+         (SELECT HOME_STORE
+            FROM TERRITORY
+           WHERE COST_CENTER_CODE = C.COST_CENTER_CODE) TERR_HOME_STORE
+    FROM COST_CENTER C;
+
+
+  GRANT SELECT ON "CCN_FACT_FINDER" TO "SWFF_CP2D11E_DBLU";
