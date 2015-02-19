6b7422f76b40dcf4041afa17d14ee0391be601ad
sw\sxt410 <>
2015-02-19 21:03:50 +0000

    changed code to set effective date for current flag Y Committed on the Free edition of March Hare Software CVSNT Server. Upgrade to CVS Suite for more features and support: http://march-hare.com/cvsnt/

--- CCNDbCode/SQL/UPDATE_EFFECTIVE_DATE.sql
+++ CCNDbCode/SQL/UPDATE_EFFECTIVE_DATE.sql
@@ -1,51 +1,25 @@
-/* this block is to update effective_date column in Polling table */
-
+/* this block is to update effective_date and expiration_date column in Polling table based on conditions */
 DECLARE
-    CNT NUMBER;
-    CURSOR CUR IS SELECT DISTINCT COST_CENTER_CODE FROM POLLING;
+    V_DATE DATE;
 BEGIN
-    FOR REC IN CUR LOOP
-        SELECT COUNT(POLLING_STATUS_CODE)
-          INTO CNT 
-          FROM POLLING
-         WHERE COST_CENTER_CODE = REC.COST_CENTER_CODE;
-        
-        IF CNT = 1 THEN
-           UPDATE POLLING 
-              SET EFFECTIVE_DATE = SYSDATE
-            WHERE POLLING_STATUS_CODE = 'I'
-              AND COST_CENTER_CODE = REC.COST_CENTER_CODE;
-        END IF;
-        
-        IF CNT = 2  THEN 
-           UPDATE POLLING 
-              SET EFFECTIVE_DATE =  SYSDATE - 1, EXPIRATION_DATE = SYSDATE
-            WHERE POLLING_STATUS_CODE = 'I'
-              AND COST_CENTER_CODE = REC.COST_CENTER_CODE;
-              
-           UPDATE POLLING
-              SET EFFECTIVE_DATE = SYSDATE
-            WHERE POLLING_STATUS_CODE = 'Q'
-              AND COST_CENTER_CODE = REC.COST_CENTER_CODE;        
-        END IF;
-         
-        IF CNT = 3  THEN 
-           UPDATE POLLING
-              SET EFFECTIVE_DATE = SYSDATE - 2, EXPIRATION_DATE = SYSDATE -1 
-            WHERE POLLING_STATUS_CODE = 'I'
-              AND COST_CENTER_CODE  = REC.COST_CENTER_CODE;
-                          
-           UPDATE POLLING
-              SET EFFECTIVE_DATE = SYSDATE - 1, EXPIRATION_DATE = SYSDATE
-            WHERE POLLING_STATUS_CODE = 'Q'
-             AND COST_CENTER_CODE = REC.COST_CENTER_CODE;
-              
-           UPDATE POLLING
-              SET EFFECTIVE_DATE = SYSDATE
-            WHERE POLLING_STATUS_CODE = 'P'
-              AND COST_CENTER_CODE = REC.COST_CENTER_CODE;               
-        END IF;
-        
-     END LOOP;
-    COMMIT;
+    FOR rec IN (SELECT * FROM POLLING WHERE CURRENT_FLAG = 'Y') LOOP
+        V_DATE := SYSDATE;
+        UPDATE POLLING
+           SET EFFECTIVE_DATE = V_DATE
+         WHERE COST_CENTER_CODE    = rec.COST_CENTER_CODE
+           AND POLLING_STATUS_CODE = rec.POLLING_STATUS_CODE;
+        FOR in_rec IN (SELECT *
+                         FROM POLLING
+                        WHERE COST_CENTER_CODE = rec.COST_CENTER_CODE
+                          AND CURRENT_FLAG <> 'Y'
+                        ORDER BY DECODE(POLLING_STATUS_CODE, 'I', 1, DECODE(POLLING_STATUS_CODE, 'Q', 2, 3)) DESC) LOOP
+            V_DATE := V_DATE - 1;
+            UPDATE POLLING
+               SET EFFECTIVE_DATE  = V_DATE,
+                   EXPIRATION_DATE = V_DATE + 1
+             WHERE COST_CENTER_CODE    = rec.COST_CENTER_CODE
+               AND POLLING_STATUS_CODE = in_rec.POLLING_STATUS_CODE;
+        END LOOP;
+        COMMIT;
+    END LOOP;
 END;
