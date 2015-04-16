905511d25b43aaeca9f71e6c93ecc2380dc3de89
unknown <sxt410@MJ003Z1H.sw.sherwin.com>
2015-04-16 13:41:38 -0400

    Added commit only at end of statement ; updating bank_paid_amount field with original_net_amount. No Jira task.

--- STORDRFT/DML/UpdateDraftsAsPaid.sql
+++ STORDRFT/DML/UpdateDraftsAsPaid.sql
@@ -1,44 +1,43 @@
 --SXT410 04/16/2015
 -- script to update the following drafts as paid on our system.
 
-update store_drafts
-   set paid_date = '26-MAR-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0828011544'
-   and cost_center_code = '708280';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '26-MAR-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0828011544'
+   AND COST_CENTER_CODE = '708280';
+COMMIT;
 
-update store_drafts
-   set paid_date = '18-MAR-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0820081024'
-   and cost_center_code = '708200';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '18-MAR-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0820081024'
+   AND COST_CENTER_CODE = '708200';
+COMMIT;
 
-update store_drafts
-   set paid_date = '09-MAR-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0820710051'
-   and cost_center_code = '708207';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '09-MAR-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0820710051'
+   AND COST_CENTER_CODE = '708207';
+COMMIT;
 
-update store_drafts
-   set paid_date = '16-MAR-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0816917868'
-   and cost_center_code = '708169';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '16-MAR-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0816917868'
+   AND COST_CENTER_CODE = '708169';
+COMMIT;
 
-update store_drafts
-   set paid_date = '13-FEB-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0730533460'
-   and cost_center_code = '707305';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '13-FEB-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0730533460'
+   AND COST_CENTER_CODE = '707305';
+COMMIT;
 
-update store_drafts
-   set paid_date = '19-FEB-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0769720509'
-   and cost_center_code = '707697';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '19-FEB-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0769720509'
+   AND COST_CENTER_CODE = '707697';
 
-update store_drafts
-   set paid_date = '26-FEB-2015' , PAY_INDICATOR = 'Y'
- where check_serial_number = '0816614010'
-   and cost_center_code = '708166';
-commit;
+UPDATE STORE_DRAFTS
+   SET PAID_DATE = '26-FEB-2015', PAY_INDICATOR = 'Y', BANK_PAID_AMOUNT = ORIGINAL_NET_AMOUNT
+ WHERE CHECK_SERIAL_NUMBER = '0816614010'
+   AND COST_CENTER_CODE = '708166';
+COMMIT;
