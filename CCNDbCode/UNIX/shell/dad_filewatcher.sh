3ef5cb3d38f3381ed9e6af73aa70f17149413850
abhitej <axk326@MJ003YLU.sw.sherwin.com>
2015-06-11 12:01:52 -0400

    ASP-357 Changed script to find the file with the name STFF1002.TXT

--- CCNDbCode/UNIX/shell/dad_filewatcher.sh
+++ CCNDbCode/UNIX/shell/dad_filewatcher.sh
@@ -6,6 +6,8 @@
 #                   comparison file is placed on the ccn db server by the mainframe and
 #                   when file is found it invokes the ccn_sd_dad_comparison.sh script
 # Created        :  AXK326 05/15/2015
+# Modified       :  AXK326 06/11/2015 
+#                   Modified script to find the file with the name STFF1002.TXT
 #########################################################################################
 # below command will get the path for ccn.config respective to the environment from which it is run from
 . /app/ccn/host.sh
@@ -14,15 +16,13 @@
  dad_file_path="$HOME/initLoad"
  dad_path="$HOME/batchJobs"
  
- # Search for the file named STFF1002_2015*.TXT 
+ # Search for the file named STFF1002.TXT 
 while true; 
 do
-   if [ -s $dad_file_path/STFF1002_*.TXT ]
+   if [ -s $dad_file_path/STFF1002.TXT ]
    then
-      echo "process started for $proc to load the dad values \n"
-	  echo "file found \n"
-	  cd $dad_file_path
-	  find -name STFF1002_\*.TXT -exec mv {} "STFF1002.TXT" \; > /dev/null 2>&1
+      echo "file found \n"
+      echo "process started for $proc to load the dad values \n"	  
 	  cd $dad_path
       ./ccn_dad_load_process.sh
 	  ./ccn_dad_archive.sh
