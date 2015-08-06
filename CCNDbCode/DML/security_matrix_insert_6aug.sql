e09f0000e6ac0ace31a6c6d67df2c5421a262bab
nxk927 <nirajan.karki@sherwin.com>
2015-08-06 11:06:15 -0400

    grant read only access

---
+++ CCNDbCode/DML/security_matrix_insert_6aug.sql
@@ -0,0 +1,11 @@
+
+--inserting only on prod, but moving all the way through the lower environment
+SET DEFINE OFF;
+Insert into SECURITY_MATRIX values ('sew901','sew901','CCNUS1');
+Insert into SECURITY_MATRIX values ('gakthr','gakthr','CCNUS1');
+Insert into SECURITY_MATRIX values ('pxj288','pxj288','CCNUS1');
+Insert into SECURITY_MATRIX values ('gap54c','gap54c','CCNUS1');
+Insert into SECURITY_MATRIX values ('tlc82c','tlc82c','CCNUS1');
+Insert into SECURITY_MATRIX values ('sjs468','sjs468','CCNUS1');
+Insert into SECURITY_MATRIX values ('beo162','beo162','CCNUS1');
+Commit;
