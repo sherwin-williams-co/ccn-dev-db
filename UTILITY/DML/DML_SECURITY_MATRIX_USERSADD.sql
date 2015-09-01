cb57ec08375fda4b7704bf613ab0e90ef321fd92
dxv848 <durga.sowjanya.vanaparti@sherwin.com>
2015-09-01 16:11:40 -0400

    ASP-422--adding User in the Cost center page application in CCN(read only aceess).

---
+++ UTILITY/DML/DML_SECURITY_MATRIX_USERSADD.sql
@@ -0,0 +1,14 @@
+/*
+Created :dxv848 09/01/2015 Adding Users(Read only access for Cost center page application in CCN).
+*/
+
+insert into SECURITY_MATRIX values ('rab54r','rab54r','CCNUS1');
+
+insert into SECURITY_MATRIX values ('rae4gr','rae4gr','CCNUS1');
+
+insert into SECURITY_MATRIX values ('jmk01r','jmk01r','CCNUS1');
+
+insert into SECURITY_MATRIX values ('ems17r','ems17r','CCNUS1');
+
+Commit;
+
