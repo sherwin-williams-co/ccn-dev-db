43228a7c1a1b69a7392ce2f9195818e5fd7d33c8
nxk927 <nirajan.karki@sherwin.com>
2015-09-03 16:40:53 -0400

    view update

--- CCNDbCode/DDL/Price_distrinct_vw.sql
+++ CCNDbCode/DDL/Price_distrinct_vw.sql
@@ -1,11 +1,12 @@
-  CREATE OR REPLACE VIEW PRICE_DISTRICT_DETAIL_VIEW AS 
-  SELECT HRCHY_DTL_CURR_ROW_VAL COST_CENTER,HRCHY_DTL_PREV_LVL_VAL PRICE_DISTRICT,HRCHY_DTL_CURR_LVL_VAL CURRENT_LEVEL,HRCHY_DTL_EFF_DATE EFFECTIVE_DATE,HRCHY_DTL_EXP_DATE EXPIRATION_DATE,HRCHY_DTL_DESC PRICE_DSTRCT_LEVEL_DESC
-/*******************************************************************************
-This View will give all the details of PRICE DISTRICT hierarchy 
+   CREATE OR REPLACE VIEW PRICE_DISTRICT_DETAIL_VIEW AS 
+   SELECT
+   /*******************************************************************************
+    This View will give all the details of PRICE DISTRICT hierarchy 
 
-Created  : 08/31/2015 nxk927 CCN Project....
-Modified : 
-*******************************************************************************/
-    FROM HIERARCHY_DETAIL
-   WHERE HRCHY_HDR_NAME = 'PRICE_DISTRICT'
-     AND HRCHY_DTL_LEVEL = 2;
+    Created  : 08/31/2015 nxk927 CCN Project....
+    Modified : 
+    *******************************************************************************/
+    COST_CENTER_CODE, DISTRICT PRICE_DISTRICT, DISTRICT||COST_CENTER_CODE CURRENT_LEVEL, HRCHY_DTL_EFF_DATE EFFECTIVE_DATE,HRCHY_DTL_EXP_DATE EXPIRATION_DATE, DISTRICT_NAME PRICE_DISTRICT_DESC
+    FROM HIERARCHY_DETAIL_VIEW
+   WHERE HRCHY_HDR_NAME = 'PRICE_DISTRICT';
+    
