664384fe4f4b914b0700883498b35524a95a5b3c
unknown <jxc517@PC06G5P5.sw.sherwin.com>
2015-11-04 08:58:31 -0500

    added BANK_DEP_BAG_FTR_ID column, changed some VARCHAR2(*) fields to NUMBER

--- BANKING/DDL/BANK_DEP_BAG_TICKORD_HIST.sql
+++ BANKING/DDL/BANK_DEP_BAG_TICKORD_HIST.sql
@@ -1,9 +1,17 @@
+/**********************************************************
+Changed : 11/03/2015 jxc517 CCN Project....
+          Modified the table to add BANK_DEP_BAG_FTR_ID column
+          This column is used to track multiple orders in a current/future tab
+          Modified table to change some VARCHAR2(*) fields to NUMBER
+**********************************************************/
+DROP TABLE BANK_DEP_BAG_TICKORD_HIST;
   CREATE TABLE BANK_DEP_BAG_TICKORD_HIST
    (	BANK_ACCOUNT_NBR           VARCHAR2(20 BYTE) NOT NULL ENABLE, 
 	COST_CENTER_CODE           VARCHAR2(6 BYTE) NOT NULL ENABLE, 
-	DEPOSIT_BAG_ORDER_PRIORITY VARCHAR2(3 BYTE), 
+	BANK_DEP_BAG_FTR_ID        NUMBER NOT NULL ENABLE,
+	DEPOSIT_BAG_ORDER_PRIORITY NUMBER, 
 	DEPOSIT_BAG_ORDER_STATUS   VARCHAR2(1 BYTE), 
-	DEPOSIT_BAG_ORDER_SEQ_NBR  VARCHAR2(3 BYTE), 
+	DEPOSIT_BAG_ORDER_SEQ_NBR  NUMBER, 
 	EFFECTIVE_DATE             DATE NOT NULL ENABLE, 
 	EXPIRATION_DATE            DATE, 
 	LAST_MAINTENANCE_DATE      DATE NOT NULL ENABLE, 
@@ -11,3 +19,4 @@
 	ORDER_DATE                 DATE,
 	EXTRACTED_USER_ID          VARCHAR2(8 BYTE)
    );
+
--- BANKING/DDL/BANK_DEP_TICKORD_HIST.sql
+++ BANKING/DDL/BANK_DEP_TICKORD_HIST.sql
@@ -1,14 +1,22 @@
+/**********************************************************
+Changed : 11/03/2015 jxc517 CCN Project....
+          Modified the table to add BANK_DEP_TICK_FTR_ID column
+          This column is used to track multiple orders in a current/future tab
+          Modified table to change some VARCHAR2(*) fields to NUMBER
+**********************************************************/
+DROP TABLE BANK_DEP_TICKORD_HIST;
   CREATE TABLE BANK_DEP_TICKORD_HIST 
    (BANK_ACCOUNT_NBR VARCHAR2(16) NOT NULL ENABLE, 
 	COST_CENTER_CODE VARCHAR2(6) NOT NULL ENABLE, 
-	DEPOSIT_ORDER_PRIORITY VARCHAR2(3), 
+	BANK_DEP_TICK_FTR_ID NUMBER NOT NULL ENABLE,
+	DEPOSIT_ORDER_PRIORITY NUMBER, 
 	DEPOSIT_ORDER_STATUS VARCHAR2(1), 
-	DEPOSIT_ORDER_SEQ_NBR VARCHAR2(3), 
+	DEPOSIT_ORDER_SEQ_NBR NUMBER, 
 	EFFECTIVE_DATE DATE NOT NULL ENABLE, 
 	EXPIRATION_DATE DATE NOT NULL ENABLE, 
 	LAST_MAINTENANCE_DATE DATE, 
 	LAST_MAINT_USER_ID VARCHAR2(6), 
-	EXTRACT_DATE DATE, 
+	ORDER_DATE DATE, 
 	EXTRACTED_USER_ID VARCHAR2(8)
    ) ;
   /
