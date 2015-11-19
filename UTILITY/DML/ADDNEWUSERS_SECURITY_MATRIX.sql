6e93e9ff5aff06150183d45ab5dd19ce1b7fef8d
dxv848 <durga.sowjanya.vanaparti@sherwin.com>
2015-11-19 10:27:49 -0500

    ASP-447--Adding new users for store drafts

---
+++ UTILITY/DML/ADDNEWUSERS_SECURITY_MATRIX.sql
@@ -0,0 +1,10 @@
+/*
+Created : dxv848 11/19/2015 Adding new users to only View the  Store draft Information. 
+*/
+
+Insert into SECURITY_MATRIX (USER_ID,PASSWORD,ROLE_CODE) values ('TLS01D','TLS01D','SDUS');
+Insert into SECURITY_MATRIX (USER_ID,PASSWORD,ROLE_CODE) values ('SRW172','SRW172','SDUS');
+Insert into SECURITY_MATRIX (USER_ID,PASSWORD,ROLE_CODE) values ('BXH367','BXH367','SDUS');
+Insert into SECURITY_MATRIX (USER_ID,PASSWORD,ROLE_CODE) values ('RXH125','RXH125','SDUS');
+
+COMMIT;
