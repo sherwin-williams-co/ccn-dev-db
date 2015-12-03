e5e4e455f7b129e52916f7e74084c9ec528f4bc0
nxk927 <nirajan.karki@sherwin.com>
2015-12-03 13:25:24 -0500

    automate matched and unmatched reports

--- STORDRFT/UNIX/shell/initLoad/1099_info_feed_to_AP.sh
+++ STORDRFT/UNIX/shell/initLoad/1099_info_feed_to_AP.sh
@@ -5,8 +5,8 @@
 # Description   : This shell program will initiate the monthly 1099 information feed to AP
 #
 # Created  : 07/30/2015 nxk927 CCN Project Team.....
-# Modified : 
-#
+# Modified : 12/03/2015 nxk927 CCN Project Team.....
+#            calling the wrapper procedure that will invoke all the procedure to generate all the 1099_consolidated_report
 ##############################################################################################################
 # below command will get the path for stordrft.config respective to the environment from which it is run from
 . /app/stordrft/host.sh
@@ -23,10 +23,7 @@ echo "Processing Started for $proc at $TIME on $DATE"
 sqlplus -s -l $sqlplus_user/$sqlplus_pw >> $LOGDIR/$proc"_"$TimeStamp.log <<END
 set heading off;
 set verify off;
-execute SD_FILE_BUILD_PKG.SD_1099_NO_MTCHD_VENDOR_RPRT(to_date('$DATE1','MM/DD/YYYY'));
-execute SD_FILE_BUILD_PKG.SD_1099_MTCHD_PRCSNG_RPRT(to_date('$DATE1','MM/DD/YYYY'));
-execute SD_FILE_BUILD_PKG.SD_1099_NO_VNDR_ON_BNK_TP_RPRT(to_date('$DATE1','MM/DD/YYYY'));
-execute SD_FILE_BUILD_PKG.SD_1099_TXPYR_ID_AP_TRNS_CRT(to_date('$DATE','MM/DD/YYYY'));
+execute SD_FILE_BUILD_PKG.SD_1099_CONSOLIDATED_RPT(to_date('$DATE','MM/DD/YYYY'), to_date('$DATE','MM/DD/YYYY'),to_date('$DATE1','MM/DD/YYYY'),to_date('$DATE1','MM/DD/YYYY'));
 exit;
 END
 
