72061562557ce74ccf5bc1661799dfde0dfc26ba
nxk927 <nirajan.karki@sherwin.com>
2015-12-15 13:32:42 -0500

    Updating the role code

--- UTILITY/DML/Enquiry_window_role_code.sql
+++ UTILITY/DML/Enquiry_window_role_code.sql
@@ -1,6 +1,6 @@
 --adding new role code for the enquiry window 
 --nxk927 12/07/2015
-Insert into ROLE_DETAILS values ('SDEUS','Store Drafts User - Enquiry Window Select','N','N','N','Y',
+Insert into ROLE_DETAILS values ('SDUES','Store Drafts User - Enquiry Window Select','N','N','N','Y',
 '<USER_RULES>
    <STORE_DRAFTS_WINDOW>
       <VALUE>ENQUIRY_WINDOW</VALUE>
@@ -14,7 +14,7 @@ Insert into ROLE_DETAILS values ('SDEUS','Store Drafts User - Enquiry Window Sel
    </STORE_DRAFTS_WINDOW>
 </USER_RULES_DESCRIPTION>');
 
---Adding users ama317 for storedraft(SDEUS)
-insert into security_matrix values('ama317', 'ama317', 'SDEUS');
+--Adding users ama317 for storedraft(SDUES)
+insert into security_matrix values('ama317', 'ama317', 'SDUES');
 
 COMMIT;
