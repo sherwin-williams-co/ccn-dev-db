096ff26f411e3554c53569a10e0b6b25e1650cba
unknown <sxh487@MJRMGGZ.sw.sherwin.com>
2016-01-08 14:41:47 -0500

    PCI MERCHANT ID Changes

---
+++ CCNDbCode/DDL/ADD_BANK_CARD.sql
@@ -0,0 +1,7 @@
+/**********************************************************
+ Adding a new column PCI_MERCHANT_ID to table BANK_CARD
+
+Created : sxh487 01/08/2016 CCN Project....
+Changed : 
+**********************************************************/
+ALTER TABLE BANK_CARD ADD PCI_MERCHANT_ID VARCHAR2(50);
