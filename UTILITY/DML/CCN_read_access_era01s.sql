c760f45151f12c3a2c9ddb535a24675298dbcb80
nxk927 <nirajan.karki@sherwin.com>
2016-02-03 10:57:43 -0500

    Read only access to era01s

---
+++ UTILITY/DML/CCN_read_access_era01s.sql
@@ -0,0 +1,24 @@
+---giving read only acces to era01s
+---cost center window and the global hierarchy window
+---nxk927 02/03/2016
+
+SET SCAN OFF;
+--INSERTING into ROLE_DETAILS 
+Insert into ROLE_DETAILS values ('GHWU','Global Hierarchy Window User','N','N','N','Y',
+'<USER_RULES>
+   <HIERARCHY_WINDOW>
+      <VALUE>HIERARCHY_NAME</VALUE>
+   </HIERARCHY_WINDOW>
+</USER_RULES>',
+'<USER_RULES_DESCRIPTION>
+   <HIERARCHY_WINDOW>
+      <HIERARCHY_NAME>
+         <VALUE>GLOBAL_HIERARCHY</VALUE>
+      </HIERARCHY_NAME>
+   </HIERARCHY_WINDOW>
+</USER_RULES_DESCRIPTION>');
+
+--INSERTING into SECURITY_MATRIX
+Insert into SECURITY_MATRIX values ('era01s','era01s','GHWU');
+Insert into SECURITY_MATRIX values ('era01s','era01s','CCNUS1');
+COMMIT;
