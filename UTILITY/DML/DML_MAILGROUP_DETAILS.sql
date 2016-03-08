3020ce517b594b61b0d6575116e7bd707b6863fe
dxv848 <durga.sowjanya.vanaparti@sherwin.com>
2016-03-08 16:06:46 -0500

    ASP-472--added mail group and deatils for initload SRA11000

---
+++ UTILITY/DML/DML_MAILGROUP_DETAILS.sql
@@ -0,0 +1,14 @@
+/*
+Created :dxv848 creating a email group and details for SRA11000 initLoad Process.
+*/
+
+
+INSERT INTO MAILING_GROUP (GROUP_ID, MAIL_ID) VALUES ('29', 'Keith.D.Parker@sherwin.com;Shahla.Husain@sherwin.com;Nirajan.Karki@sherwin.com;Jaydeep.Cheruku@sherwin.com;Abhitej.Kasula@sherwin.com;durga.sowjanya.vanaparti@sherwin.com;amarender.reddy.devidi@sherwin.com;mahesh.repala@sherwin.com');
+
+INSERT INTO MAILING_DETAILS (MAIL_CATEGORY, GROUP_ID, SUBJECT, FROM_P, MESSAGE, SIGNATURE) VALUES ('SRA11000_PROCESS', '29', 'SRA11000 PROCESS FAILED', 'Keith.D.Parker@sherwin.com', 'SRA11000 InitLoad Process Failed', 'Thanks,
+Keith D. Parker
+IT Manager
+Sherwin Williams - Stores IT');
+
+COMMIT;
+
