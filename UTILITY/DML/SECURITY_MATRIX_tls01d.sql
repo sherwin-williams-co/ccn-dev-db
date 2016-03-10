cf0bda02fd684d2feeaa563e23be8224cdb30d77
mxr916 <mahesh.repala@sherwin.com>
2016-03-10 14:56:24 -0500

    Added Comments.

--- UTILITY/DML/SECURITY_MATRIX_tls01d.sql
+++ UTILITY/DML/SECURITY_MATRIX_tls01d.sql
@@ -1,8 +1,9 @@
+--Created: 03/10/2016 mxr916
 --This sql Script Adds the User tls01d to give the access for CCNUS
 SET SCAN OFF;
 REM INSERTING into security_matrix
 SET DEFINE OFF;
 
-insert into ccn_utility.security_matrix(user_id, password, role_code) values('tls01d', 'tls01d', 'CCNUS');
+insert into security_matrix(user_id, password, role_code) values('tls01d', 'tls01d', 'CCNUS');
 
 COMMIT;
