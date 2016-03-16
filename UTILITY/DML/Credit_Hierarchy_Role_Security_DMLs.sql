d210ecc0eb8ba0e669ce1693587e7afb5366b931
unknown <jxc517@PC06G5P5.sw.sherwin.com>
2016-03-16 15:05:52 -0400

    roles correction

--- UTILITY/DML/Credit_Hierarchy_Role_Security_DMLs.sql
+++ UTILITY/DML/Credit_Hierarchy_Role_Security_DMLs.sql
@@ -29,7 +29,7 @@ Insert into ROLE_DETAILS (ROLE_CODE,ROLE_DESCRIPTION,ADMIN_FLAG,INSERT_FLAG,UPDA
    </HIERARCHY_WINDOW>
 </USER_RULES_DESCRIPTION>');
 
-Insert into ROLE_DETAILS (ROLE_CODE,ROLE_DESCRIPTION,ADMIN_FLAG,INSERT_FLAG,UPDATE_FLAG,SELECT_FLAG,USER_RULES,USER_RULES_DESCRIPTION) values ('HWCU','Hierarchy Window Credit Hierarchy Edit User','Y','Y','Y','Y','<USER_RULES>
+Insert into ROLE_DETAILS (ROLE_CODE,ROLE_DESCRIPTION,ADMIN_FLAG,INSERT_FLAG,UPDATE_FLAG,SELECT_FLAG,USER_RULES,USER_RULES_DESCRIPTION) values ('HWCU','Hierarchy Window Credit Hierarchy User','Y','Y','Y','Y','<USER_RULES>
    <HIERARCHY_WINDOW>
       <VALUE>HIERARCHY_NAME</VALUE>
    </HIERARCHY_WINDOW>
@@ -74,53 +74,53 @@ Insert into SECURITY_MATRIX values ('pmm4br','pmm4br','CCNUS3');
 Insert into SECURITY_MATRIX values ('jep01r','jep01r','CCNUS3');
 
 --INSERTING into SECURITY_MATRIX - credit users select
-Insert into SECURITY_MATRIX values ('alwser','alwser','HWCUS1');
-Insert into SECURITY_MATRIX values ('bar99r','bar99r','HWCUS1');
-Insert into SECURITY_MATRIX values ('cll316','cll316','HWCUS1');
-Insert into SECURITY_MATRIX values ('gxm577','gxm577','HWCUS1');
-Insert into SECURITY_MATRIX values ('hij565','hij565','HWCUS1');
-Insert into SECURITY_MATRIX values ('jxd347','jxd347','HWCUS1');
-Insert into SECURITY_MATRIX values ('jxm474','jxm474','HWCUS1');
-Insert into SECURITY_MATRIX values ('jxx183','jxx183','HWCUS1');
-Insert into SECURITY_MATRIX values ('kaftzr','kaftzr','HWCUS1');
-Insert into SECURITY_MATRIX values ('kxb659','kxb659','HWCUS1');
-Insert into SECURITY_MATRIX values ('kxt175','kxt175','HWCUS1');
-Insert into SECURITY_MATRIX values ('kxt851','kxt851','HWCUS1');
-Insert into SECURITY_MATRIX values ('mxm309','mxm309','HWCUS1');
-Insert into SECURITY_MATRIX values ('mxv771','mxv771','HWCUS1');
-Insert into SECURITY_MATRIX values ('nxk96r','nxk96r','HWCUS1');
-Insert into SECURITY_MATRIX values ('nxs290','nxs290','HWCUS1');
-Insert into SECURITY_MATRIX values ('ras46r','ras46r','HWCUS1');
-Insert into SECURITY_MATRIX values ('rbdarr','rbdarr','HWCUS1');
-Insert into SECURITY_MATRIX values ('rdsw6r','rdsw6r','HWCUS1');
-Insert into SECURITY_MATRIX values ('rxk163','rxk163','HWCUS1');
-Insert into SECURITY_MATRIX values ('rxm383','rxm383','HWCUS1');
-Insert into SECURITY_MATRIX values ('sxk419','sxk419','HWCUS1');
-Insert into SECURITY_MATRIX values ('tadarr','tadarr','HWCUS1');
-Insert into SECURITY_MATRIX values ('wxn683','wxn683','HWCUS1');
-Insert into SECURITY_MATRIX values ('ydrpur','ydrpur','HWCUS1');
-Insert into SECURITY_MATRIX values ('yxz430','yxz430','HWCUS1');
+Insert into SECURITY_MATRIX values ('alwser','alwser','HWCUS');
+Insert into SECURITY_MATRIX values ('bar99r','bar99r','HWCUS');
+Insert into SECURITY_MATRIX values ('cll316','cll316','HWCUS');
+Insert into SECURITY_MATRIX values ('gxm577','gxm577','HWCUS');
+Insert into SECURITY_MATRIX values ('hij565','hij565','HWCUS');
+Insert into SECURITY_MATRIX values ('jxd347','jxd347','HWCUS');
+Insert into SECURITY_MATRIX values ('jxm474','jxm474','HWCUS');
+Insert into SECURITY_MATRIX values ('jxx183','jxx183','HWCUS');
+Insert into SECURITY_MATRIX values ('kaftzr','kaftzr','HWCUS');
+Insert into SECURITY_MATRIX values ('kxb659','kxb659','HWCUS');
+Insert into SECURITY_MATRIX values ('kxt175','kxt175','HWCUS');
+Insert into SECURITY_MATRIX values ('kxt851','kxt851','HWCUS');
+Insert into SECURITY_MATRIX values ('mxm309','mxm309','HWCUS');
+Insert into SECURITY_MATRIX values ('mxv771','mxv771','HWCUS');
+Insert into SECURITY_MATRIX values ('nxk96r','nxk96r','HWCUS');
+Insert into SECURITY_MATRIX values ('nxs290','nxs290','HWCUS');
+Insert into SECURITY_MATRIX values ('ras46r','ras46r','HWCUS');
+Insert into SECURITY_MATRIX values ('rbdarr','rbdarr','HWCUS');
+Insert into SECURITY_MATRIX values ('rdsw6r','rdsw6r','HWCUS');
+Insert into SECURITY_MATRIX values ('rxk163','rxk163','HWCUS');
+Insert into SECURITY_MATRIX values ('rxm383','rxm383','HWCUS');
+Insert into SECURITY_MATRIX values ('sxk419','sxk419','HWCUS');
+Insert into SECURITY_MATRIX values ('tadarr','tadarr','HWCUS');
+Insert into SECURITY_MATRIX values ('wxn683','wxn683','HWCUS');
+Insert into SECURITY_MATRIX values ('ydrpur','ydrpur','HWCUS');
+Insert into SECURITY_MATRIX values ('yxz430','yxz430','HWCUS');
 
 --INSERTING into SECURITY_MATRIX - HWCU - Credit User
-Insert into SECURITY_MATRIX values ('kdp465','kdppswrd','CCNCU');
-Insert into SECURITY_MATRIX values ('jxc517','jxc517','CCNCU');
-Insert into SECURITY_MATRIX values ('sxh487','sxh487','CCNCU');
-Insert into SECURITY_MATRIX values ('nxk927','nxk927','CCNCU');
-Insert into SECURITY_MATRIX values ('axk326','axk326','CCNCU');
-Insert into SECURITY_MATRIX values ('dxv848','dxv848','CCNCU');
-Insert into SECURITY_MATRIX values ('axd783','axd783','CCNCU');
-Insert into SECURITY_MATRIX values ('mxr916','mxr916','CCNCU');
+Insert into SECURITY_MATRIX values ('kdp465','kdppswrd','HWCU');
+Insert into SECURITY_MATRIX values ('jxc517','jxc517','HWCU');
+Insert into SECURITY_MATRIX values ('sxh487','sxh487','HWCU');
+Insert into SECURITY_MATRIX values ('nxk927','nxk927','HWCU');
+Insert into SECURITY_MATRIX values ('axk326','axk326','HWCU');
+Insert into SECURITY_MATRIX values ('dxv848','dxv848','HWCU');
+Insert into SECURITY_MATRIX values ('axd783','axd783','HWCU');
+Insert into SECURITY_MATRIX values ('mxr916','mxr916','HWCU');
 
-Insert into SECURITY_MATRIX values ('sxk696','sxk696','CCNCU');
-Insert into SECURITY_MATRIX values ('axc415','what','CCNCU');
-Insert into SECURITY_MATRIX values ('axc539','mdh000','CCNCU');
-Insert into SECURITY_MATRIX values ('sxd392','sxd392','CCNCU');
-Insert into SECURITY_MATRIX values ('sxr128','sxr128','CCNCU');
-Insert into SECURITY_MATRIX values ('mxa875','mxa875','CCNCU');
+Insert into SECURITY_MATRIX values ('sxk696','sxk696','HWCU');
+Insert into SECURITY_MATRIX values ('axc415','what','HWCU');
+Insert into SECURITY_MATRIX values ('axc539','mdh000','HWCU');
+Insert into SECURITY_MATRIX values ('sxd392','sxd392','HWCU');
+Insert into SECURITY_MATRIX values ('sxr128','sxr128','HWCU');
+Insert into SECURITY_MATRIX values ('mxa875','mxa875','HWCU');
 
 --INSERTING into SECURITY_MATRIX - credit users
-Insert into SECURITY_MATRIX values ('cmaarr','cmaarr','CCNCU');
-Insert into SECURITY_MATRIX values ('pmm4br','pmm4br','CCNCU');
-Insert into SECURITY_MATRIX values ('jep01r','jep01r','CCNCU');
+Insert into SECURITY_MATRIX values ('cmaarr','cmaarr','HWCU');
+Insert into SECURITY_MATRIX values ('pmm4br','pmm4br','HWCU');
+Insert into SECURITY_MATRIX values ('jep01r','jep01r','HWCU');
 
 COMMIT;
