ea338b65f8353e27ab3fa287577a199ceb037f71
nxk927 <nirajan.karki@sherwin.com>
2016-04-06 15:49:45 -0400

    fix for the report running twice

--- STORDRFT/UNIX/shell/initLoad/Monthly_Reports_Run_bp.sh
+++ STORDRFT/UNIX/shell/initLoad/Monthly_Reports_Run_bp.sh
@@ -4,7 +4,9 @@
 # Description   : Checks for the monthly load trigger file and kicks off the reports run
 #
 # Created  : 01/15/2016 jxc517 CCN Project Team.....
-# Modified : 
+# Modified : 04/06/2016 nxk927 CCN Project Team.....
+#            changed the script as it was looping twice.
+#            pushed the remove command at the start
 #################################################################
 #Run below command to make the process run in the background even after shutdown
 #nohup sh /app/strdrft/sdReport/scripts/Monthly_Reports_Run_bp.sh > /app/strdrft/sdReport/logs/Monthly_Reports_Run_bp.log 2>&1 &
@@ -13,22 +15,20 @@
 #ps -eaf | grep Monthly_Reports_Run_bp.sh
 
 while true; do
-   if [ -f /app/strdrft/sdReport/data/sd_monthly_load.trg ]
+   if [ -f sd_monthly_load.trg ]
    then
-          DATE=`date +"%m/%d/%Y"`
-          TIME=`date +"%I:%M:%S"`
-	  echo "sd_monthly_load.trg trigger file found at $TIME on $DATE"
-	  echo "Running the Report Generation process at $TIME on $DATE"
-	  ./app/strdrft/sdReport/scripts/sdreport.sh	  
-          TIME=`date +"%I:%M:%S"`
-	  echo "Running the FTP Process at $TIME on $DATE"
-	  ./app/strdrft/sdReport/scripts/plgain_ftp.sh
-          TIME=`date +"%I:%M:%S"`
-	  echo "Removing sd_monthly_load.trg trigger file at $TIME on $DATE"
-	  rm -f /app/strdrft/sdReport/data/sd_monthly_load.trg
-          TIME=`date +"%I:%M:%S"`
+      rm sd_monthly_load.trg
+	  TIME=`date +"%I:%M:%S"`
+	  DATE=`date +"%m/%d/%Y"`
 	  echo "sd_monthly_load.trg trigger file removed at $TIME on $DATE"
-   fi
+	  echo "Running the Report Generation process at $TIME on $DATE"
+	  sh sdreport.sh
+      TIME=`date +"%I:%M:%S"`
+	  echo "Running the FTP Process at $TIME on $DATE"
+	  sh plgain_ftp.sh
+      TIME=`date +"%I:%M:%S"`
+	  echo "FTP Process completed at $TIME on $DATE"
+	fi
 done
 
 echo "process completed - but should not come to this point"
--- STORDRFT/UNIX/shell/initLoad/sd_monthly_app_srvr_trgr_file.sh
+++ STORDRFT/UNIX/shell/initLoad/sd_monthly_app_srvr_trgr_file.sh
@@ -4,7 +4,9 @@
 #                   triggers the report run
 #
 # Created         : 01/15/2016 jxc517 CCN Project Team....
-# Modified        :    
+# Modified        : 04/06/2016 nxk927 CCN Project Team....
+#                   changed the path where the trigger file will be sent
+#                   changed the error check to make it uniform and pushed the echo at the end
 #######################################################################################
 
 # below command will get the path for stordrft.config respective to the environment from which it is run from.
@@ -21,27 +23,27 @@ echo "" > sd_monthly_load.trg
 ftp -n ${appserver_host} <<END_SCRIPT
 quote USER ${appserver_user}
 quote PASS ${appserver_pw}
-cd /app/strdrft/sdReport/data
+cd /app/strdrft/sdReport/scripts
 
 put sd_monthly_load.trg
 
 quit
 END_SCRIPT
-echo " FTP Process Successful "
-
-rm -f sd_monthly_load.trg
 
 ############################################################################
 #                           ERROR STATUS CHECK 
 ############################################################################
 status=$?
-TIME=`date +"%H:%M:%S"`
-if test $status -ne 0
-then
-     echo "processing FAILED for $proc at ${TIME} on ${DATE}"
-     exit 1;
+if [ $status -ne 0 ]; then
+   TIME=`date +"%H:%M:%S"`
+   echo "processing FAILED for $proc at ${TIME} on ${DATE}"
+   exit 1;
 fi
 
+echo " FTP Process Successful "
+rm -f sd_monthly_load.trg
+
+TIME=`date +"%H:%M:%S"`
 echo "Processing finished for $proc at ${TIME} on ${DATE}"  
 
 exit 0
