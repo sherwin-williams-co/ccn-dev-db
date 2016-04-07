5a28610a5f3c851f28671cc0f62f5d5daa26e404
unknown <sxh487@MJRMGGZ.sw.sherwin.com>
2016-04-07 14:45:27 -0400

    Code Header Alter

---
+++ CCNDbCode/DDL/CODE_HEADER_DDL.sql
@@ -0,0 +1,7 @@
+/********************************************************************************
+Adding a new column CODE_HEADER_IDENTIFIER to CODE_HEADER for Credit Hierarchy
+
+Created : 03/14/2016 sxh487 CCN Project Team....
+Modified:
+********************************************************************************/
+ALTER TABLE CODE_HEADER ADD CODE_HEADER_IDENTIFIER XMLTYPE;
