73379da3b59f53469cd9d75bd42974858c715302
nxk927 <nirajan.karki@sherwin.com>
2016-05-16 14:02:07 -0400

    CCN read only access

---
+++ UTILITY/DML/CCN_READ_ACCESS05162016.sql
@@ -0,0 +1,12 @@
+---giving cost center window read only acces to gxm577, jep01r, cmaarr, kaftzr, ydrpur
+---nxk927 05/16/2016
+
+SET SCAN OFF;
+--INSERTING into SECURITY_MATRIX
+Insert into SECURITY_MATRIX values ('gxm577','gxm577','CCNUS3');
+Insert into SECURITY_MATRIX values ('jep01r','jep01r','CCNUS3');
+Insert into SECURITY_MATRIX values ('cmaarr','cmaarr','CCNUS3');
+Insert into SECURITY_MATRIX values ('kaftzr','kaftzr','CCNUS3');
+Insert into SECURITY_MATRIX values ('ydrpur','ydrpur','CCNUS3');
+COMMIT;
+
