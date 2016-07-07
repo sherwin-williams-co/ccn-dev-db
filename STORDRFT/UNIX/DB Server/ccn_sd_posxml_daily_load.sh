025fbb7bece268332f1d7b52a2a06cd6cf939773
abhitej <axk326@sw.sherwin.com>
2016-07-07 08:44:48 -0400

    POSXML related Components: 1. DDL for all the tables relating to POSXML 2. Shell scripts to invoke the load process 3. Package spec and package body corresponding to daily load

--- STORDRFT/UNIX/DB Server/ccn_sd_posxml_daily_load.sh
+++ STORDRFT/UNIX/DB Server/ccn_sd_posxml_daily_load.sh
@@ -2,22 +2,74 @@
 ###############################################################################################################################
 # Script name   : ccn_sd_posxml_daily_load.sh
 #
-# Description   : This script is to run the POSXML_SD_DAILY_LOAD_TMP.POSXML_SD_DAILY_LOAD_SP
+# Description   : This script is to run the following:
+#                 1. POSXML_PNP_TO_STRDRT_LOAD_PKG.PNP_TO_SD_LOAD_SP()
+#                 2. NEW_POSXML_SD_DAILY_LOAD_TMP.POSXML_SD_DAILY_LOAD_SP('$DATE')
 #
-# Created  : 04/14/2016 axk326 CCN Project Team.....
+# Created  : 06/03/2016 axk326 CCN Project Team.....
 # Modified : 
 ###############################################################################################################################
 # below command will get the path for stordrft.config respective to the environment from which it is run from
 . /app/stordrft/host.sh
 
-proc_name="POSXML_SD_DAILY_LOAD_SP"
-TIME=`date +"%H:%M:%S"`
+proc_name="PNP_TO_SD_LOAD_SP"
+LOGDIR=$HOME/dailyLoad/logs
 DATE=`date +"%d-%^b-%Y"`
-
+TIME=`date +"%H:%M:%S"`
 echo "Processing Started for $proc_name at $TIME on $DATE"
 
-./EXEC_PROC_1PARAM.sh "POSXML_SD_DAILY_LOAD_TMP.POSXML_SD_DAILY_LOAD_SP" "$DATE"
+sqlplus -s -l $sqlplus_user/$sqlplus_pw >> $LOGDIR/$proc_name"_"$DATE.log <<END
+set heading off;
+set verify off;
+set serveroutput on;
+var exitCode number;
+WHENEVER OSERROR EXIT 1
+WHENEVER SQLERROR EXIT 1
+BEGIN
+:exitCode := 0;
+POSXML_PNP_TO_STRDRT_LOAD_PKG.PNP_TO_SD_LOAD_SP();
+EXCEPTION
+ when others then
+ :exitCode := 2;
+ END;
+ /
+exit :exitCode;
+END
+############################################################################
+#                           ERROR STATUS CHECK
+############################################################################
+status=$?
+if [ $status -ne 0 ]; then
+     TIME=`date +"%H:%M:%S"`
+     echo "processing FAILED for $proc_name at ${TIME} on ${DATE}"
+     exit 1;
+fi
 
+TIME=`date +"%H:%M:%S"`
+echo "Processing finished for $proc_name at ${TIME} on ${DATE}"
+############################################################################
+
+proc_name="POSXML_SD_DAILY_LOAD_SP"
+TIME=`date +"%H:%M:%S"`
+echo "Processing Started for $proc_name at $TIME on $DATE"
+
+sqlplus -s -l $sqlplus_user/$sqlplus_pw >> $LOGDIR/$proc_name"_"$DATE.log <<END
+set heading off;
+set verify off;
+set serveroutput on;
+var exitCode number;
+WHENEVER OSERROR EXIT 1
+WHENEVER SQLERROR EXIT 1
+BEGIN
+:exitCode := 0;
+NEW_POSXML_SD_DAILY_LOAD_TMP.POSXML_SD_DAILY_LOAD_SP('$DATE');
+EXCEPTION
+ when others then
+ :exitCode := 2;
+ END;
+ /
+exit :exitCode;
+END
 ############################################################################
 #                           ERROR STATUS CHECK 
 ############################################################################
---
+++ STORDRFT/UNIX/DB Server/mv_file_arch_to_datafiles.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+################################################################################################################
+# Script name   : mv_file_arch_to_datafiles.sh
+#
+# Description   : This shell program will Move the files from Archive folder to the datafiles folder 
+#
+# Created  : 02/19/2016 axk326 CCN Project Team.....
+# Modified : 
+################################################################################################################
+
+# below command will get the path for stordrft.config respective to the environment from which it is run from
+. /app/stordrft/host.sh
+
+proc_name="mv_file_arch_to_datafiles"
+ARCHIVE_PATH="$HOME/dailyLoad/archieve/drafts"
+FLDR_DATE=`date -d ${DAILY_LOAD_RUNDATE} +"%m%d%Y"`
+DATE=`date +"%m/%d/%Y"`
+CUR_PATH="$HOME/dailyLoad/archieve/drafts/dailyLoad_"$FLDR_DATE""
+DATAFILE_PATH="$HOME/datafiles"
+TIME=`date +"%H:%M:%S"`
+echo "Processing Started for $proc_name at $TIME on $DATE"
+echo $DATE
+echo $FLDR_DATE
+# Control will output if $DIRECTORY exists.
+if [ -d "$ARCHIVE_PATH/"dailyLoad"_"$FLDR_DATE"" ]; then
+   echo " Directory exists "
+   cd $CUR_PATH
+   #Move STORE_DRAFT.TXT file from archive folder to datafiles folder.
+   if 
+      ls STORE_DRAFT.TXT &> /dev/null; then
+      echo " store draft file exist "
+      find -maxdepth 1 -name STORE_DRAFT.TXT -exec mv {} $DATAFILE_PATH \; > /dev/null 2>&1
+      echo " STORE_DRAFT.TXT file is moved from archive folder to datafiles folder "
+   else
+      echo " STORE_DRAFT.TXT file doesn't exists "
+   fi
+   #Move CUSTOMER_LABOR.TXT file from archive folder to datafiles folder. 
+   if 
+      ls CUSTOMER_LABOR.TXT &> /dev/null; then
+      echo " customer labor file exist "
+      find -maxdepth 1 -name CUSTOMER_LABOR.TXT -exec mv {} $DATAFILE_PATH \; > /dev/null 2>&1
+      echo " CUSTOMER_LABOR.TXT file is moved from archive folder to datafiles folder "
+   else
+      echo " CUSTOMER_LABOR.TXT file doesn't exists "
+   fi
+else
+   echo " Directory do not exists "
+fi
+
+#############################################################################
+##                           ERROR STATUS CHECK 
+#############################################################################
+status=$?
+if test $status -ne 0
+then
+     TIME=`date +"%H:%M:%S"`
+     echo "processing FAILED for $proc_name at ${TIME} on ${DATE}"
+     exit 1;
+fi
+
+TIME=`date +"%H:%M:%S"`
+echo "Processing finished for $proc_name at ${TIME} on ${DATE}"  
+
+exit 0
+#############################################################################
