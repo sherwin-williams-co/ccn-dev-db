6581363b9fa87dc5ba3b1d53ab4610795cd0ad3b
Vijay Varada <vijay.varada@sherwin.com>
2016-09-06 11:34:55 -0400

    removed dbms_output

--- CCNDbCode/DML/Delete_Code_Detail_Header.sql
+++ CCNDbCode/DML/Delete_Code_Detail_Header.sql
@@ -6,8 +6,4 @@ Modified:
 ********************************************************************************/
 
 DELETE FROM CODE_DETAIL WHERE CODE_HEADER_NAME IN ('DCM', 'ACM', 'RCM');
-DBMS_OUTPUT.PUT_LINE('CODE_DETAIL - DCM, ACM, RCM : '|| SQL%ROWCOUNT || ' Row(s) deleted ' );
-
 DELETE FROM CODE_HEADER WHERE CODE_HEADER_NAME IN ('DCM', 'ACM', 'RCM');
-DBMS_OUTPUT.PUT_LINE('CODE_HEADER - DCM, ACM, RCM : '|| SQL%ROWCOUNT || ' Row(s) deleted ' );
-
