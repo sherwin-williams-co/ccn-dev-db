f45ed8368329e9985e0b8dc99b63386008b1af42
Vijay Varada <vijay.varada@sherwin.com>
2016-09-06 10:39:28 -0400

    Drop tables that are no longer needed for the new credit hierarchy load

---
+++ CCNDbCode/DDL/CredHier_Drop_Tables.sql
@@ -0,0 +1,11 @@
+/********************************************************************************
+Below script will DROP tables which are no longer needed
+
+Created : 09/06/2016 vxv336 CCN Project Team
+Modified: 
+********************************************************************************/
+
+DROP TABLE TEMP_CREDIT_HIERARCHY_DCM_DTLS;
+DROP TABLE TEMP_CREDIT_HIERARCHY_ACM_DTLS;
+DROP TABLE TEMP_CREDIT_HIERARCHY_RCM_DTLS;
+ 
