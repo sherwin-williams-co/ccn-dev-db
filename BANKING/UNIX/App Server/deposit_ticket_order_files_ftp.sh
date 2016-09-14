eae752dc56e376d1447d0371268fe0553a50529b
nxk927 <nirajan.karki@sherwin.com>
2016-09-14 13:25:49 -0400

    update after review

--- BANKING/UNIX/App Server/deposit_ticket_order_files_ftp.sh
+++ BANKING/UNIX/App Server/deposit_ticket_order_files_ftp.sh
@@ -8,7 +8,7 @@
 # Modified : 08/30/2016 nxk927 CCN Project Team.....
 #            sending email and archieving the missing corresponding file
 # Modified : 09/14/2016 nxk927 CCN Project Team.....
-#            added extra check if the file exists
+#            added condition to check if the file exists
 #################################################################
 # below command will get the path for banking.config respective to the environment from which it is run from
 . /app/banking/dev/banking.config
@@ -22,16 +22,15 @@ TIME=`date +"%H:%M:%S"`
 echo "Process started for $proc_name at $TIME on $DATE"
 #move to the datafiles folder
 cd $file_path
-for file in DEPOSIT_TICKET_*.xml
+xml_files=`ls DEPOSIT_TICKET*.xml`
+for file in $xml_files
 do
-  if [ -e $file ]
+  f1=${file:0:21}
+  file1=$f1.txt
+  if [ -e $file1 ]
   then
-     f1=${file:0:21}
-     file1=$f1.txt
-     if [ -e $file1 ]
-     then
-        TIME=`date +"%H:%M:%S"`
-        echo "FTP Process started for $proc_name at $TIME on $DATE"
+     TIME=`date +"%H:%M:%S"`
+     echo "FTP Process started for $proc_name at $TIME on $DATE"
 ftp -inv ${mainframe_host} <<FTP_MF
 quote user ${mainframe_user}
 quote pass ${mainframe_pw}
@@ -41,26 +40,25 @@ bye
 END_SCRIPT
 echo "bye the transfer is complete"
 FTP_MF
-        TIME=`date +"%H:%M:%S"`
-        echo "bye the transfer is complete at ${TIME} on ${DATE}"
+     TIME=`date +"%H:%M:%S"`
+     echo "bye the transfer is complete at ${TIME} on ${DATE}"
 
-        TIME=`date +"%H:%M:%S"`
-        echo "Archieving Process started for $proc_name at $TIME on $DATE"
-        dt=$(date +%Y%m%d%H%M%S)
-        mv $file $archieve_path/${f1}_${dt}.xml
-        dt=$(date +%Y%m%d%H%M%S)
-        mv $file1 $archieve_path/${f1}_${dt}.txt
-        TIME=`date +"%H:%M:%S"`
-        echo "Archieving Process finished for $proc_name at $TIME on $DATE"
-     else
-        CC=${file:15:6}
-        echo "Corresponding deposit ticket file for cost center $CC not found for processing"
-        TIME=`date +"%H:%M:%S"`
-        echo "Sending email for $CC for corresponding file check at $TIME on $DATE"
-        sh $HOME/dpst_tkt_send_mail.sh $f1
-        TIME=`date +"%H:%M:%S"`
-        echo "Sending email Process finished at $TIME on $DATE"
-     fi
+     TIME=`date +"%H:%M:%S"`
+     echo "Archieving Process started for $proc_name at $TIME on $DATE"
+     dt=$(date +%Y%m%d%H%M%S)
+     mv $file $archieve_path/${f1}_${dt}.xml
+     dt=$(date +%Y%m%d%H%M%S)
+     mv $file1 $archieve_path/${f1}_${dt}.txt
+     TIME=`date +"%H:%M:%S"`
+     echo "Archieving Process finished for $proc_name at $TIME on $DATE"
+  else
+     CC=${file:15:6}
+     echo "Corresponding deposit ticket file for cost center $CC not found for processing"
+     TIME=`date +"%H:%M:%S"`
+     echo "Sending email for $CC for corresponding file check at $TIME on $DATE"
+     sh $HOME/dpst_tkt_send_mail.sh $f1
+     TIME=`date +"%H:%M:%S"`
+     echo "Sending email Process finished at $TIME on $DATE"
   fi
 done
 ############################################################################
