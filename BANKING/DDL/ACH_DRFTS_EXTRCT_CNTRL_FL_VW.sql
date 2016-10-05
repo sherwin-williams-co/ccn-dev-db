d96ea6d3728d36588014996339b0cdc6c9efcc96
nxk927 <nirajan.karki@sherwin.com>
2016-10-05 11:17:38 -0400

    table name changed for the view

--- BANKING/DDL/ACH_DRFTS_EXTRCT_CNTRL_FL_VW.sql
+++ BANKING/DDL/ACH_DRFTS_EXTRCT_CNTRL_FL_VW.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE VIEW  ACH_DRFTS_EXTRCT_CNTRL_FL_VW AS
 SELECT 
   /*******************************************************************************
-  This View holds all the  data for ACH_DRFTS_EXTRCT_CNTRL_FL table
+  This View holds all the  data for ACH_DRFTS_EXT_CTRL table
 
-  Created  : 10/07/2015 nxk927 CCN Project  
-  Modified : 
+  Created  : 10/07/2015 nxk927 CCN Project
+  Modified : 10/05/2016 nxk927 CCN Project
+             the table name where we pull the data from  has been changed 
+             from ACH_DRFTS_EXTRCT_CNTRL_FL to ACH_DRFTS_EXT_CTRL
   *******************************************************************************/ 
-* FROM ACH_DRFTS_EXTRCT_CNTRL_FL;
+* FROM ACH_DRFTS_EXT_CTRL;
