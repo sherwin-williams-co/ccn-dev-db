ca59ba0c4efd597a4ffb9abc1e162b93f4ffc8f0
Vijay Varada <vijay.varada@sherwin.com>
2016-10-19 14:27:38 -0400

    Modified Delete and Drop scripts for Credit Hierarchy

--- CCNDbCode/DDL/CreditHierarchy_drop_tables.sql
+++ CCNDbCode/DDL/CreditHierarchy_drop_tables.sql
@@ -1,9 +1,11 @@
  /********************************************************************************
-Below script will DROP lookup tables which will be recreated.
+Below script will DROP lookup tables and external table.
 
 Created : 07/27/2016 vxv336 CCN Project Team....
-Modified: 
+Modified: 10/19/2016 vxv336
+                     added drop CRDT_HIER_RCM_LOOKUP
 ********************************************************************************/
  DROP TABLE CRDT_HIER_ACM_LOOKUP;
  DROP TABLE CRDT_HIER_DCM_LOOKUP;
+ DROP TABLE CRDT_HIER_RCM_LOOKUP;
  DROP TABLE CREDIT_HIERARCHY_EXT_DETAIL;
--- CCNDbCode/DML/Delete_Code_Detail_Header.sql
+++ CCNDbCode/DML/Delete_Code_Detail_Header.sql
@@ -2,8 +2,9 @@
 Below script will delete from CODE_DETAIL and CODE_HEADER
 
 Created : 09/06/2016 vxv336 CCN Project Team
-Modified: 
+Modified: 10/19/2016 vxv336
+                     Added SYSOUT
 ********************************************************************************/
 
-DELETE FROM CODE_DETAIL WHERE CODE_HEADER_NAME IN ('DCM', 'ACM', 'RCM');
-DELETE FROM CODE_HEADER WHERE CODE_HEADER_NAME IN ('DCM', 'ACM', 'RCM');
+DELETE FROM CODE_DETAIL WHERE CODE_HEADER_NAME IN ('DCM', 'ACM', 'RCM', 'SYSOUT');
+DELETE FROM CODE_HEADER WHERE CODE_HEADER_NAME IN ('DCM', 'ACM', 'RCM', 'SYSOUT');
