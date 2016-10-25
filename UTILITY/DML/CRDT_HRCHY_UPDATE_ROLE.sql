fb41b8d6586f3fcf3ae1773da8cea4409f5641bf
Vijay Varada <vijay.varada@sherwin.com>
2016-10-25 15:24:40 -0400

    update Credit Hierarchy roles for gxm577 and kaftzr

---
+++ UTILITY/DML/CRDT_HRCHY_UPDATE_ROLE.sql
@@ -0,0 +1,11 @@
+/********************************************************************************
+Below script will update Credit Hierarchy roles for gxm577 and kaftzr
+
+Created : 10/25/2016 vxv336 CCN Project Team.
+Modified: 
+
+NOTE: This script applies only to TEST and QA. Need to modify the user list when migrating to PROD. 
+********************************************************************************/
+SET DEFINE OFF;
+
+UPDATE SECURITY_MATRIX SET ROLE_CODE = 'HWCU' WHERE USER_ID IN ('gxm577', 'kaftzr') AND ROLE_CODE = 'HWCUS';
