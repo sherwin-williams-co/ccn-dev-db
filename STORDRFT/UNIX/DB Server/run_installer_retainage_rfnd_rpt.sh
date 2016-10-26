106671983b97c459ecc09ffa5449864784209b13
Nirajan Karki <nirajan.karki@sherwin.com>
2016-10-26 16:43:48 -0400

    run installer retainage rfnd report on 1st week day of the month

---
+++ STORDRFT/UNIX/DB Server/run_installer_retainage_rfnd_rpt.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#################################################################
+# Script name   : run_installer_retainage_rfnd_rpt.sh
+#
+# Description   : This script is to run the installer_retainage_rfnd_rpt.sh process on the 1st week day of the month
+#                 This process will check if current date matches the 1st business day of the month.
+#                 If it matches then the process will start
+# Created       : 10/26/2016 nxk927.....
+# Modified      : 
+#################################################################
+# below command will get the path for stordrft.config respective to the environment from which it is run from
+. /app/stordrft/host.sh
+
+
+PROC_NAME="RUN_INSTALLER_RETAINAGE_RFND_RPT"
+LOGDIR=$HOME/Reports/log
+TIME=`date +"%H:%M:%S"`
+DATE=`date +"%m/%d/%Y"`
+
+echo "RUN_INSTALLER_RETAINAGE_RFND_RPT PROCESS STARTED AT ${TIME} ON ${DATE}"
+
+TIME=`date +"%H:%M:%S"`
+SECOND_WEEKDAY=`cat $HOME/initLoad/SECOND_WEEKDAY.TXT`
+DT=$(date -d "$SECOND_WEEKDAY - 1 days" +"%m/%d/%Y")
+
+if [ $DT == $DATE ]
+   then echo "MONTHLY INSTALLER RETAINAGE REFUND REPORT PROCESS STARTED AT ${TIME} ON ${DATE}"
+   cd $HOME/initLoad
+   sh installer_retainage_rfnd_rpt.sh
+   else
+   echo "NOT A FIRST WEEK DAY OF THE MONTH TO RUN THE MONTHLY INSTALLER RETAINAGE REFUND REPORT PROCESS "
+fi
+
+############################################################################
+#                           ERROR STATUS CHECK
+############################################################################
+status=$?
+if [ $status -ne 0 ]; then
+   TIME=`date +"%H:%M:%S"`
+   echo "PROCESS EXITED DUE TO ERROR WHILE RUNNING THE $PROC_NAME AT $TIME ON $DATE"
+   exit 1;
+fi
+
+TIME=`date +"%H:%M:%S"`
+echo "PROCESSING FINISHED FOR $PROC_NAME AT ${TIME} ON ${DATE}"
+exit 0
