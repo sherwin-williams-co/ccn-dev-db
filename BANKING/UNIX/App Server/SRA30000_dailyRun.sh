99e84e84f1ec12d729c55e2702675929674c94d1
Vijay Varada <vijay.varada@sherwin.com>
2016-11-02 16:08:54 -0400

    Modifications for SRA30000

--- BANKING/UNIX/App Server/SRA30000_dailyRun.sh
+++ BANKING/UNIX/App Server/SRA30000_dailyRun.sh
@@ -11,13 +11,7 @@
 . /app/banking/dev/banking.config
 
 proc_name="SRA30000_dailyRun"
-DATA_FILES_PATH="$HOME/initLoad"
-ARCHIVE_PATH="$HOME/SRA30000"
-LOGDIR=$HOME/logs
-DATE=`date +"%m/%d/%Y"`
 TIME=`date +"%H:%M:%S"`
-TimeStamp=`date '+%Y%m%d%H%M%S'`
-FOLDER=`date +"%m%d%Y"`
 echo "Processing Started for $proc_name at $TIME on $DATE"
 
 #################################################################
@@ -48,16 +42,4 @@ fi
 TIME=`date +"%H:%M:%S"`
 echo "Processing finished for SRA30000_Arch_Output_file script at ${TIME} on ${DATE}"
 
-#################################################################
-#                                              ERROR STATUS CHECK
-#################################################################
-status=$?
-if test $status -ne 0
-then
-     TIME=`date +"%H:%M:%S"`
-     echo "processing FAILED for $proc_name at ${TIME} on ${DATE}"
-     exit 1;
-fi
-TIME=`date +"%H:%M:%S"`
-echo "Processing finished for $proc_name at ${TIME} on ${DATE}"  
 exit 0
