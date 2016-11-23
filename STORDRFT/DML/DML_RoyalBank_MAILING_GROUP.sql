404993f40e11dc9d47a697023ed5f87c39aaf328
Ganesh Gosavi <ganesh.gosavi@sherwin.com>
2016-11-23 10:37:22 -0500

    DML script for mailing group setup for Royal Bank report

---
+++ STORDRFT/DML/DML_RoyalBank_MAILING_GROUP.sql
@@ -0,0 +1,9 @@
+/***************************************************************************** 
+Below script is created to store mailing group for Royal Bank report 
+created : 11/18/2016 gxg192 CCN Project.... 
+changed : 
+*****************************************************************************/
+SET DEFINE OFF;
+
+INSERT INTO MAILING_GROUP (GROUP_ID, MAIL_ID)
+   VALUES ('47', 'ccnoracle.team@sherwin.com');
