2e9690852001fad06a2f71e0ab8f9caf69756265
Robert Shen <robert.shen@sherwin.com>
2016-12-06 11:08:50 -0500

    corrected bank num display in .csv

--- BANKING/SQL/deposit_bags_ord.sql
+++ BANKING/SQL/deposit_bags_ord.sql
@@ -5,6 +5,7 @@
 
 created : sxh487 11/21/2016
 modified: rxs349 12/05/2016  Corrected cursor name
+          sxh487 12/06/2016  corrected bank num display in .csv
 **********************************************************/
 declare
 CURSOR dep_bag_cur IS
@@ -22,19 +23,19 @@ BEGIN
    --Building the header
    V_CLOB := 'BANK_ACCOUNT_NBR,COST_CENTER_CODE,DEPOSIT_BAG_ORDER_PRIORITY,DEPOSIT_BAG_ORDER_STATUS,DEPOSIT_BAG_ORDER_SEQ_NBR,EFFECTIVE_DATE,EXPIRATION_DATE,LAST_MAINTENANCE_DATE,LAST_MAINT_USER_ID,ORDER_DATE,EXTRACTED_USER_ID '||  UTL_TCP.crlf;
 
-   FOR REC IN dep_bag_cur LOOP    
+   FOR REC IN dep_bag_cur LOOP        
             V_CLOB := V_CLOB 
-                      ||REC.BANK_ACCOUNT_NBR ||',' || CHR(9)
-                      ||REC.COST_CENTER_CODE ||',' || CHR(9)
+                      ||'"'||REC.BANK_ACCOUNT_NBR ||'"'|| CHR(9)|| ','  
+                      ||'"'||REC.COST_CENTER_CODE ||'"'|| CHR(9)|| ','
                       ||REC.DEPOSIT_BAG_ORDER_PRIORITY ||','      
                       ||REC.DEPOSIT_BAG_ORDER_STATUS ||','  
                       ||REC.DEPOSIT_BAG_ORDER_SEQ_NBR ||','     
                       ||REC.EFFECTIVE_DATE ||','        
                       ||REC.EXPIRATION_DATE ||','         
                       ||REC.LAST_MAINTENANCE_DATE ||','         
-                      ||REC.LAST_MAINT_USER_ID ||',' || CHR(9)  
+                      ||'"'||REC.LAST_MAINT_USER_ID ||'"'|| CHR(9)|| ',' 
                       ||REC.ORDER_DATE ||','      
-                      ||REC.EXTRACTED_USER_ID || CHR(9)|| CHR(10); 
+                      ||'"'||REC.EXTRACTED_USER_ID ||'"'|| CHR(9)|| CHR(10); 
         END LOOP;
     IF V_CLOB <> EMPTY_CLOB() THEN
        MAIL_PKG.SEND_MAIL('DEP_BAG_TICKORD_EXC_RPT', NULL, NULL, V_CLOB);
