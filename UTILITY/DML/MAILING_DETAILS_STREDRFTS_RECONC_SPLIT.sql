5e5d487443ea321994409792ecdea5265de93d3f
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2017-02-28 11:39:44 -0500

    Changed functionality as per comments for request id 2061 and 2062

--- UTILITY/DML/MAILING_DETAILS_STREDRFTS_RECONC_SPLIT.sql
+++ UTILITY/DML/MAILING_DETAILS_STREDRFTS_RECONC_SPLIT.sql
@@ -6,6 +6,6 @@ SET SCAN OFF;
 REM INSERTING into MAILING_DETAILS
 SET DEFINE OFF;
 
-Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) values ('SUNTRUST DRAFT MAINT RECORDS WRITTEN - STBD1300','12','SUNTRUST DRAFT MAINT RECORDS WRITTEN SPLIT From Daily Reconcilation details','ccnoracle.team@sherwin.com','Please find SUNTRUST DRAFT MAINT RECORDS WRITTEN Records split.','Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
-Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) values ('SUNTRUST DRAFT AUDIT RECORDS READ - STBD1300','12','SUNTRUST DRAFT AUDIT RECORDS READ SPLIT From Daily Reconcilation details','ccnoracle.team@sherwin.com','Please find SUNTRUST DRAFT AUDIT RECORDS READ Records split.','Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) values ('SD_DLY_SNTRST_MAINT','12','SUNTRUST DRAFT MAINT RECORDS WRITTEN SPLIT From Daily Reconcilation details','ccnoracle.team@sherwin.com','Please find SUNTRUST DRAFT MAINT RECORDS WRITTEN Records split.','Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) values ('SD_DLY_SNTRST_AUDIT','12','SUNTRUST DRAFT AUDIT RECORDS READ SPLIT From Daily Reconcilation details','ccnoracle.team@sherwin.com','Please find SUNTRUST DRAFT AUDIT RECORDS READ Records split.','Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
 COMMIT;
