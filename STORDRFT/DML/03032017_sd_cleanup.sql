ba77fb856e93185f26119da9700ceb420d052a98
Ganesh Gosavi <Ganesh.Gosavi@sherwin.com>
2017-03-06 09:26:14 -0500

    Changes for store drafts daily load manual drafts issue fix and data clean up

---
+++ STORDRFT/DML/03032017_sd_cleanup.sql
@@ -0,0 +1,79 @@
+/*
+Below script created to move all the store drafts that are unattached (terminal number on them is Null) 
+from STORE_DRAFTS table back to UNATTACHED_MNL_DRFT_DTL table
+Also remove the records from UNATTACHED_MNL_DRFT_DTL_HST from them and clean up the stop pay details.
+
+Created : 03/03/2017 gxg192 CCN Project Team....
+*/
+SET SERVEROUTPUT ON
+DECLARE
+    CURSOR STOREDRAFT_CLNUP_CUR
+    IS
+    SELECT * 
+      FROM STORE_DRAFTS 
+     WHERE CHECK_SERIAL_NUMBER IN (SELECT CHECK_SERIAL_NUMBER 
+                                     FROM UNATTACHED_MNL_DRFT_DTL_HST)
+       AND TERMINAL_NUMBER IS NULL;
+
+    rec_cnt      NUMBER := 0;
+    rec_clnd_cnt NUMBER := 0;
+
+BEGIN
+
+    FOR rec IN STOREDRAFT_CLNUP_CUR
+    LOOP
+
+        rec_cnt := rec_cnt + 1;
+
+        BEGIN
+
+            IF rec.STOP_INDICATOR = 'Y' 
+            THEN 
+                    rec.STOP_INDICATOR := 'N';
+                    rec.STOP_PAY_DATE  := NULL;
+                    rec.OPEN_INDICATOR := 'Y';
+            END IF;
+            
+            --Insert into UNATTACHED_MNL_DRFT_DTL table
+            INSERT INTO UNATTACHED_MNL_DRFT_DTL VALUES rec;
+            
+            --Delete from UNATTACHED_MNL_DRFT_DTL_HST table
+            DELETE UNATTACHED_MNL_DRFT_DTL_HST
+             WHERE CHECK_SERIAL_NUMBER = rec.CHECK_SERIAL_NUMBER;
+             
+            --Delete from STORE_DRAFTS table
+            DELETE STORE_DRAFTS
+             WHERE CHECK_SERIAL_NUMBER = rec.CHECK_SERIAL_NUMBER; 
+
+            rec_clnd_cnt := rec_clnd_cnt + 1;
+            
+            IF MOD(rec_clnd_cnt,500) = 0
+            THEN
+                COMMIT;
+            END IF;
+
+        EXCEPTION
+            WHEN OTHERS THEN
+                ERRPKG.INSERT_ERROR_LOG_SP(SQLCODE,
+                                           'STOREDRFT_CLEANUP',
+                                           SQLERRM,
+                                           rec.COST_CENTER_CODE,
+                                           NVL(rec.CHECK_SERIAL_NUMBER,'')||':'||
+                                           NVL(rec.TRANSACTION_DATE,'')||':'||
+                                           NVL(rec.STOP_INDICATOR,''));
+        END;
+    END LOOP;
+    
+    COMMIT;
+
+    dbms_output.put_line('Total number of records to be cleaned up = ' ||rec_cnt);
+    dbms_output.put_line('Number of records successfully cleaned up = '||rec_clnd_cnt);
+    
+EXCEPTION
+    WHEN OTHERS THEN
+        ERRPKG.INSERT_ERROR_LOG_SP(SQLCODE,
+                                   'STOREDRFT_CLEANUP',
+                                   SQLERRM,
+                                   '000000',
+                                   '0000000000');
+END;
