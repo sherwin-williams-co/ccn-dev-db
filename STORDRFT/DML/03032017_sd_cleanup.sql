758d4cfcd32eeb3643e18f1d57ea2eaf28eb5a2c
Ganesh Gosavi <Ganesh.Gosavi@sherwin.com>
2017-03-07 11:46:07 -0500

    Changes for store drafts daily load manual drafts issue fix and data clean up

--- STORDRFT/DML/03032017_sd_cleanup.sql
+++ STORDRFT/DML/03032017_sd_cleanup.sql
@@ -4,6 +4,7 @@ from STORE_DRAFTS table back to UNATTACHED_MNL_DRFT_DTL table
 Also remove the records from UNATTACHED_MNL_DRFT_DTL_HST from them and clean up the stop pay details.
 
 Created : 03/03/2017 gxg192 CCN Project Team....
+Changed : 03/03/2017 gxg192 Changes to set few fields if STOP_PAY_MARKED_BY_CCN_IND is Y
 */
 SET SERVEROUTPUT ON
 DECLARE
@@ -11,44 +12,42 @@ DECLARE
     IS
     SELECT * 
       FROM STORE_DRAFTS 
-     WHERE CHECK_SERIAL_NUMBER IN (SELECT CHECK_SERIAL_NUMBER 
-                                     FROM UNATTACHED_MNL_DRFT_DTL_HST)
-       AND TERMINAL_NUMBER IS NULL;
+     WHERE TERMINAL_NUMBER IS NULL;
 
     rec_cnt      NUMBER := 0;
     rec_clnd_cnt NUMBER := 0;
 
 BEGIN
 
-    FOR rec IN STOREDRAFT_CLNUP_CUR
-    LOOP
+    FOR rec IN STOREDRAFT_CLNUP_CUR LOOP
 
         rec_cnt := rec_cnt + 1;
 
         BEGIN
 
-            IF rec.STOP_INDICATOR = 'Y' 
-            THEN 
-                    rec.STOP_INDICATOR := 'N';
-                    rec.STOP_PAY_DATE  := NULL;
-                    rec.OPEN_INDICATOR := 'Y';
+            IF rec.STOP_PAY_MARKED_BY_CCN_IND = 'Y' THEN
+                rec.VOID_INDICATOR := 'N';
+                rec.PAY_INDICATOR  := 'N';
+                rec.STOP_INDICATOR := 'N';
+                rec.OPEN_INDICATOR := 'Y';
+                rec.STOP_PAY_DATE  := NULL;
+                rec.STOP_PAY_MARKED_BY_CCN_IND := NULL;
             END IF;
             
             --Insert into UNATTACHED_MNL_DRFT_DTL table
             INSERT INTO UNATTACHED_MNL_DRFT_DTL VALUES rec;
             
             --Delete from UNATTACHED_MNL_DRFT_DTL_HST table
-            DELETE UNATTACHED_MNL_DRFT_DTL_HST
+            DELETE FROM UNATTACHED_MNL_DRFT_DTL_HST
              WHERE CHECK_SERIAL_NUMBER = rec.CHECK_SERIAL_NUMBER;
              
             --Delete from STORE_DRAFTS table
-            DELETE STORE_DRAFTS
+            DELETE FROM STORE_DRAFTS
              WHERE CHECK_SERIAL_NUMBER = rec.CHECK_SERIAL_NUMBER; 
 
             rec_clnd_cnt := rec_clnd_cnt + 1;
             
-            IF MOD(rec_clnd_cnt,500) = 0
-            THEN
+            IF MOD(rec_clnd_cnt,500) = 0 THEN
                 COMMIT;
             END IF;
 
