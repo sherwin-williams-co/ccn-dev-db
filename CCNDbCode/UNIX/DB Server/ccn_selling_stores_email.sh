d397362e29adce7e4efaba8e5a0be39d6f30b3c8
Ganesh Gosavi <ganesh.gosavi@sherwin.com>
2017-04-04 14:41:11 -0400

    Changes to run the process for previous day gxg192

--- CCNDbCode/UNIX/DB Server/ccn_selling_stores_email.sh
+++ CCNDbCode/UNIX/DB Server/ccn_selling_stores_email.sh
@@ -6,7 +6,8 @@
 #                 CCN_BATCH_PROCESS.GENERATE_SELLING_STORE_FILE
 #
 # Created  : 09/29/2016 jxc517 CCN Project Team.....
-# Modified : 04/03/3017 gxg192 Added WHENEVER clause to track error
+# Modified : 04/03/2017 gxg192 Added WHENEVER clause to track error
+#            04/04/2017 gxg192 Changes to run the process for previous day.
 ###############################################################################################################################
 # below command will get the path for stordrft.config respective to the environment from which it is run from
 . /app/ccn/host.sh
@@ -15,7 +16,8 @@ proc="ccn_selling_stores_email"
 datafilepath=$HOME/datafiles
 LOGDIR="$HOME/batchJobs"
 DATE=`date +"%m/%d/%Y"`
-RUNDATE=`date +"%m%d%y"`
+RUNDATE=`date --date="yesterday" +%m/%d/%Y`
+FILEDATE=`date +"%m%d%y"`
 TimeStamp=`date '+%Y%m%d%H%M%S'`
 
 TIME=`date +"%H:%M:%S"`
@@ -27,7 +29,7 @@ set verify off;
 WHENEVER OSERROR EXIT 1
 WHENEVER SQLERROR EXIT 1
 
-execute CCN_BATCH_PROCESS.GENERATE_SELLING_STORE_FILE(TO_DATE('$DATE', 'MM/DD/YYYY'));
+execute CCN_BATCH_PROCESS.GENERATE_SELLING_STORE_FILE(TO_DATE('$RUNDATE', 'MM/DD/YYYY'));
 
 exit;
 END
@@ -36,15 +38,15 @@ END
 #                           ERROR STATUS CHECK
 ############################################################################
 status=$?
+TIME=`date +"%H:%M:%S"`
 if test $status -ne 0
 then
-   TIME=`date +"%H:%M:%S"`
    echo "processing FAILED for $proc at ${TIME} on ${DATE}"
    exit 1;
 fi
-
+echo "Process to generate selling store file executed for $RUNDATE at ${TIME} on ${DATE}"
 cd $datafilepath
-csv_files=`ls *_SELLING_STORES_$RUNDATE.csv`
+csv_files=`ls *_SELLING_STORES_$FILEDATE.csv`
 for file in $csv_files
 do
    filename=`echo "$file" | cut -d'.' -f1`
