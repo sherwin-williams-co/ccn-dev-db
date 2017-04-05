5f2a20cf0d104d0f7a56cc587407c1e01ef4cb3a
Ganesh Gosavi <Ganesh.Gosavi@sherwin.com>
2017-04-05 13:27:06 -0400

    Changes to add SELLING_STORE_FLAG_MARK_DT column in store table and code to populate it

---
+++ CCNDbCode/DDL/ALTER_STORE_ADD_SLLNG_STR_FLG_MRK_DT.sql
@@ -0,0 +1,6 @@
+/*******************************************************************************
+  Alter table script to add SELLING_STORE_FLAG_MARK_DT columns to Store table.
+  CREATED : 04/05/2017 gxg192 CCN Project...
+*******************************************************************************/
+ALTER TABLE STORE
+ADD SELLING_STORE_FLAG_MARK_DT DATE;
