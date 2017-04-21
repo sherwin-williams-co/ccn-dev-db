e821b0c3e9c8604551e89b1645cbaa0f843a3895
Rajasekar Ariga <rajasekar.ariga@sherwin.com>
2017-04-21 11:16:06 -0400

    Create Table DISPATCH_TERMINAL

---
+++ CCNDbCode/DDL/DISPATCH_TERMINAL.sql
@@ -0,0 +1,15 @@
+/********************************************************************************
+Creating new Cost Center Terminal Table to hold the Terminal home store attached to the cost center in case of Terminal Type cost centers 
+
+Created : 04/11/2017 rxa457 CCN Project Team....
+Created:
+********************************************************************************/
+
+CREATE TABLE DISPATCH_TERMINAL(
+      COST_CENTER_CODE 		VARCHAR2(6) NOT NULL , 
+      CATEGORY   			VARCHAR2(1) DEFAULT 'D' NOT NULL  , 
+      HOME_STORE   VARCHAR2(6) NOT NULL , 
+      CONSTRAINT DISPATCH_TERMINAL_PK PRIMARY KEY (COST_CENTER_CODE),
+      CONSTRAINT DISPATCH_TERMINAL_FK1 FOREIGN KEY (COST_CENTER_CODE) REFERENCES COST_CENTER (COST_CENTER_CODE) ,
+	  CONSTRAINT DISPATCH_TERMINAL_FK2 FOREIGN KEY (HOME_STORE) REFERENCES COST_CENTER (COST_CENTER_CODE) 
+);
