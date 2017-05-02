cf18ebfc1214cf7d0ea5bcc7ce374b011a8e1290
Ganesh Gosavi <ganesh.gosavi@sherwin.com>
2017-05-02 11:16:36 -0400

    Update DML_FSA_mailing_list_change.sql

--- UTILITY/DML/DML_FSA_mailing_list_change.sql
+++ UTILITY/DML/DML_FSA_mailing_list_change.sql
@@ -7,8 +7,12 @@ Created : 05/02/2017 gxg192 CCN Project....
 Changed : 05/02/2017 gxg192 Changes to use group id.
 */
 
+SELECT * FROM MAILING_GROUP WHERE GROUP_ID = '27';
+--1 Row(s) Selected
+
 UPDATE MAILING_GROUP
    SET mail_id = 'ccnoracle.team@sherwin.com; bpi.fin.ap.noam@sherwin.com'
  WHERE group_id = '27';
+--1 Row(s) Updated
 
 COMMIT;
