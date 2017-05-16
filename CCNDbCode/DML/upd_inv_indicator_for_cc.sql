027d1c7a8635653edb5144929d4760fa634808c6
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2017-05-16 14:59:28 -0400

    Updated Inventory Indicator to 'N' for given cost centers

---
+++ CCNDbCode/DML/upd_inv_indicator_for_cc.sql
@@ -0,0 +1,20 @@
+/*
+     Created: 05/16/2017 axt754 CCN Project Team..
+     DML script for updating INVENTORY_INDICATOR field to 'N' in STORE table
+     for COST_CENTER_CODE '778802','779025','768700'
+*/
+-- MANUAL BACKUP before doing updates 
+
+ SELECT * 
+   FROM STORE
+  WHERE COST_CENTER_CODE 
+     IN ('778802','779025','768700');
+
+
+-- UPDATE INVENTORY_INDICATOR Field in STORE table
+UPDATE STORE
+   SET INVENTORY_INDICATOR = 'N'
+ WHERE COST_CENTER_CODE IN ('778802','779025','768700');
+   
+-- COMMIT Trasanction 
+-- COMMIT;
