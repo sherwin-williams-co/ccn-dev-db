22282299d7cca61859f6d4d181836711c153a8ff
sudha <sudha.rani.pinnamaraju@sherwin.com>
2017-05-17 15:02:58 -0400

    ASP-778 : DROP StoreDraft Components as part of GIT clean up activity

--- STORDRFT/SQL/1099RerunScript.sql
+++
@@ -1,8 +0,0 @@
---SXT410 04/14/2015
---Below scripts to rerun the process from Jan 1st till April 1st
-SD_FILE_BUILD_PKG.BUILD_1099_FILE('01-JAN-2015','N'); --rerun for Feb 1st
-SD_FILE_BUILD_PKG.BUILD_1099_FILE('01-JAN-2015','Y'); --rerun for Feb 15th
-SD_FILE_BUILD_PKG.BUILD_1099_FILE('01-FEB-2015','N'); --rerun for Mar 1st
-SD_FILE_BUILD_PKG.BUILD_1099_FILE('01-FEB-2015','Y'); --rerun for Mar 15th
-SD_FILE_BUILD_PKG.BUILD_1099_FILE('01-MAR-2015','N'); --rerun for Apr 1st
-SD_FILE_BUILD_PKG.BUILD_1099_FILE('01-MAR-2015','Y'); --rerun for Apr 15th
