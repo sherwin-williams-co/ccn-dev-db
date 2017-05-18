d10991b8bbf1b24b68d710411eaa90774b2a9557
sudha <sudha.rani.pinnamaraju@sherwin.com>
2017-05-18 14:31:49 -0400

    ASP-788 Delete hierarchy_detail price district unattached cost center levels

--- CCNDbCode/SQL/delete_hier_det_price_levels_ASP_788.sql
+++ CCNDbCode/SQL/delete_hier_det_price_levels_ASP_788.sql
@@ -11,37 +11,15 @@ END;
 /
 
 DECLARE
-    V_COUNT           NUMBER := 0;
-    V_MAX_VALUE       NUMBER := 0;	
+    V_HRCHY_HDR_LVL_NBR     HIERARCHY_DESCRIPTION.HRCHY_HDR_LVL_NBR%TYPE;
 BEGIN
 
-       --Check if the PRICE_DISTRICT levels are there in HIERARCHY_DESCRIPTION table 
-       BEGIN
-           SELECT COUNT(1) 
-             INTO V_COUNT
-             FROM HIERARCHY_DESCRIPTION
-            WHERE HRCHY_HDR_NAME = 'PRICE_DISTRICT';
-       EXCEPTION
-           WHEN NO_DATA_FOUND THEN 
-               RETURN;  --Exiting as there are no PRICE_DISTRICT levels
-		   WHEN OTHERS THEN	   
-               V_COUNT := 0;		   
-       END;
-	   
-       --If record exists then get max value
-       IF V_COUNT > 0 THEN
-           --Below query will get the maximum level for PRICE_DISTRICT
-           SELECT MAX(HRCHY_HDR_LVL_NBR)
-             INTO V_MAX_VALUE
-             FROM HIERARCHY_DESCRIPTION
-            WHERE HRCHY_HDR_NAME = 'PRICE_DISTRICT';
-       END IF;	   
-
+       V_HRCHY_HDR_LVL_NBR := CCN_HIER_BUSINESS_RULES_PKG.GET_HDR_LVL_NBR_SP('PRICE_DISTRICT', 'Cost Center');
 	   
 		--SELECTING ALL THOSE RECORDS WHICH ARE NOT MAPPED TO ANY CHILD RECORD 
 		FOR REC IN ( SELECT HD.*, ROWID  FROM HIERARCHY_DETAIL HD 
 					  WHERE HD.HRCHY_HDR_NAME = 'PRICE_DISTRICT'    
-						AND HRCHY_DTL_LEVEL        <> V_MAX_VALUE       --EXCLUDED LEAF NODES, AS LEAF NODES ARE ALWAYS MAP WITH COST CENTER    
+						AND HRCHY_DTL_LEVEL        <> V_HRCHY_HDR_LVL_NBR       
 						AND NOT EXISTS (SELECT 1               
 										  FROM HIERARCHY_DETAIL       
 										 WHERE HRCHY_HDR_NAME         = HD.HRCHY_HDR_NAME         
@@ -56,7 +34,10 @@ BEGIN
 			   
 		COMMIT;	 
 		END LOOP;
-
+EXCEPTION
+    WHEN OTHERS THEN
+       DBMS_OUTPUT.PUT_LINE('FAILED ' || SQLCODE || ' ' || SQLERRM);
+	   :EXITCODE := 2;
 END;
 /
 
--- CCNDbCode/UNIX/DB Server/delete_hier_det_price_levels_onetime.sh
+++ CCNDbCode/UNIX/DB Server/delete_hier_det_price_levels_onetime.sh
@@ -1,58 +1,49 @@
 #!/bin/sh
 
-##########################################################################################
-# Script Name  : delete_hier_det_price_levels_onetime.sh
-# Date Created : 05/17/2017 sxp130 deleting the Hierarchy_detail table one time run.
-#                delete_hier_det_price_levels_onetime.sh calls the delete_hier_det_price_levels_ASP_788.sql to delete the
-#                hierarchy detail levels which are cost center not attached.    
-##########################################################################################
+#####################################################################################################################
+# script name   : delete_hier_det_price_levels_onetime.sh
+#
+# description   : script to delete unattached cost center PRICE_DISTRICT hierarchy levels from Hierarchy_Detail table
+#		          This script is one-time run.
+#   Created     : 05/18/2017 SXP130
+#   Modified    : 
+#####################################################################################################################
 
-echo "Starting  delete_hier_det_price_levels_onetime.sh script"
 
 # below command will get the path for ccn.config respective to the environment from which it is run from
 . /app/ccn/host.sh
 
-# establish parameter names for this script
-PROC="delete_hier_det_price_levels_ASP_788.sql"
-
-# establish the date and time
-DATE=`date +"%Y-%m-%d"`
 TIME=`date +"%H:%M:%S"`
+DATE=`date +"%m/%d/%Y"`
+proc_name="delete_hier_det_price_levels_ASP_788"
 
-cd $HOME/AdhocTaskScripts/sql
-pwd
 echo "Processing Started for $PROC at $TIME on $DATE"
-sqlplus -s -l $sqlplus_user/$sqlplus_pw <<EOF > delete_hier_det_price_levels_ASP_788.log
+
+sqlplus -s -l $sqlplus_user/$sqlplus_pw <<END > delete_hier_det_price_levels_ASP_788.log
 set heading off;
 set serveroutput on;
 set verify off;
- 
-@$PROC
+WHENEVER OSERROR EXIT 1
+WHENEVER SQLERROR EXIT 1
+var EXITCODE number;
+exec :EXITCODE := 0;
+@$HOME/AdhocTaskScripts/sql/delete_hier_det_price_levels_ASP_788.sql
+print :EXITCODE;
+exit :EXITCODE;
+END
 
-exit;
-EOF
-
-
-echo "return from $PROC"
-
-TIME=`date +"%H:%M:%S"`
-##############################################
-#  ERROR STATUS CHECK
-##############################################
+############################################################################
+#                           ERROR STATUS CHECK
+############################################################################
 status=$?
-if test $status -ne 0
-   then
-     
-     echo "processing of $PROC failed at ${TIME} on ${DATE}"
-     exit 1
-else
-   echo "processing of $PROC Successful at ${TIME} on ${DATE} "
+if [ $status -ne 0 ]; then
+     TIME=`date +"%H:%M:%S"`
+     echo "processing FAILED for $proc_name at $TIME on $DATE"
+     exit 1;
 fi
 
-echo "ending delete_hier_det_price_levels_onetime.sh script"
+TIME=`date +"%H:%M:%S"`
+echo "Processing finished for $proc_name at ${TIME} on ${DATE}"
 
 exit 0
-
-###################################################################################
-#                          END OF SCRIPT                                          #
-###################################################################################
+############################################################################
