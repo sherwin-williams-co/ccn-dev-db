a2004aaa046d2b6a16589ccb0ee3aa152227727e
sudha <sudha.rani.pinnamaraju@sherwin.com>
2017-05-19 11:09:00 -0400

    ASP-788 Implimenting code review comments

--- CCNDbCode/SQL/delete_hier_det_price_levels_ASP_788.sql
+++ CCNDbCode/SQL/delete_hier_det_price_levels_ASP_788.sql
@@ -2,7 +2,6 @@
 Below script to delete HIERARCHY_DETAIL price district levels which are cost centers are not attached to them.
 
 Created : 05/16/2017 sxp130 : ASP-788 Price district hierarchy : cleanup of unattached levels
-Approx execution Time : 2 to 3 minutes based on the volume
 */
 
 BEGIN
@@ -13,31 +12,33 @@ END;
 DECLARE
     V_HRCHY_HDR_LVL_NBR     HIERARCHY_DESCRIPTION.HRCHY_HDR_LVL_NBR%TYPE;
 BEGIN
+    --get Price District Cost center level number
+    V_HRCHY_HDR_LVL_NBR := CCN_HIER_BUSINESS_RULES_PKG.GET_HDR_LVL_NBR_SP('PRICE_DISTRICT',
+                                                                          'Cost Center');
 
-       V_HRCHY_HDR_LVL_NBR := CCN_HIER_BUSINESS_RULES_PKG.GET_HDR_LVL_NBR_SP('PRICE_DISTRICT', 'Cost Center');
-	   
-		--SELECTING ALL THOSE RECORDS WHICH ARE NOT MAPPED TO ANY CHILD RECORD 
-		FOR REC IN ( SELECT HD.*, ROWID  FROM HIERARCHY_DETAIL HD 
-					  WHERE HD.HRCHY_HDR_NAME = 'PRICE_DISTRICT'    
-						AND HRCHY_DTL_LEVEL        <> V_HRCHY_HDR_LVL_NBR       
-						AND NOT EXISTS (SELECT 1               
-										  FROM HIERARCHY_DETAIL       
-										 WHERE HRCHY_HDR_NAME         = HD.HRCHY_HDR_NAME         
-										   AND HRCHY_DTL_LEVEL        = (HD.HRCHY_DTL_LEVEL + '1')
-										   AND HRCHY_DTL_PREV_LVL_VAL = HD.HRCHY_DTL_CURR_LVL_VAL      
-										   AND HRCHY_DTL_CURR_LVL_VAL = HD.HRCHY_DTL_NEXT_LVL_VAL)) LOOP
-										   
-		    --DELETING ALL THOSE RECORDS WHICH ARE NOT MAPPED TO ANY CHILD RECORD 
-			DELETE 
-			  FROM HIERARCHY_DETAIL
-			 WHERE ROWID                  = rec.ROWID;
-			   
-		COMMIT;	 
-		END LOOP;
+    --selecting all those records which are not mapped to any child record 
+    FOR REC IN (SELECT HD.*,
+                       ROWID
+                  FROM HIERARCHY_DETAIL HD 
+                 WHERE HD.HRCHY_HDR_NAME = 'PRICE_DISTRICT'
+                   AND HRCHY_DTL_LEVEL   < V_HRCHY_HDR_LVL_NBR
+                   AND NOT EXISTS (SELECT 1
+                                     FROM HIERARCHY_DETAIL
+                                    WHERE HRCHY_HDR_NAME         = HD.HRCHY_HDR_NAME
+                                      AND HRCHY_DTL_LEVEL        = (HD.HRCHY_DTL_LEVEL + '1')
+                                      AND HRCHY_DTL_PREV_LVL_VAL = HD.HRCHY_DTL_CURR_LVL_VAL
+                                      AND HRCHY_DTL_CURR_LVL_VAL = HD.HRCHY_DTL_NEXT_LVL_VAL)) LOOP
+
+        --deleting all those records which are not mapped to any child record 
+        DELETE 
+          FROM HIERARCHY_DETAIL
+         WHERE ROWID = rec.ROWID;
+        COMMIT;
+    END LOOP;
 EXCEPTION
     WHEN OTHERS THEN
        DBMS_OUTPUT.PUT_LINE('FAILED ' || SQLCODE || ' ' || SQLERRM);
-	   :EXITCODE := 2;
+       :EXITCODE := 2;
 END;
 /
 
