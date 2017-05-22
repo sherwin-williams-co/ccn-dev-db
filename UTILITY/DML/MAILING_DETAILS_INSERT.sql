161541f81a25ec8e564b044241af9c0df36b270f
Rajasekar Ariga <Rajasekar.Ariga@sherwin.com>
2017-05-22 10:15:46 -0400

    Mailing details insert signature to include chr(10) and removed rxa457 from  mail_pkg

--- UTILITY/DML/MAILING_DETAILS_INSERT.sql
+++ UTILITY/DML/MAILING_DETAILS_INSERT.sql
@@ -3,8 +3,5 @@
 --------------------------------------------------------
 REM INSERTING into MAILING_DETAILS
 SET DEFINE OFF;
-Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) values ('MONTHLY_REPORTS_RUN_BP','25','Store Draft Monthly Reports Process Run Failed','ccnoracle.team@sherwin.com','Store Draft Monthly Reports Process Run Failed and no output files were sent using FTP. Please review attached Log file for more details and reschedule the job after taking necessary actions.','Thanks,
-Keith D. Parker
-IT Manager
-Sherwin Williams - Stores IT');
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) values ('MONTHLY_REPORTS_RUN_BP','25','Store Draft Monthly Reports Process Run Failed','ccnoracle.team@sherwin.com','Store Draft Monthly Reports Process Run Failed and no output files were sent using FTP. Please review attached Log file for more details and reschedule the job after taking necessary actions.','Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
 commit;
