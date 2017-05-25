e433897f07be6dbbb2bd24ef0279edb51875c548
Shahla Husain <shahla.husain@sherwin.com>
2017-05-25 16:29:53 -0400

    Price District business rules changes

---
+++ CCNDbCode/DDL/HIER_ERROR_LOG_SEQ.sql
@@ -0,0 +1,5 @@
+/*****************************************************************************
+ Created: 05/25/2012 sxh487 CCN Project.... 
+ sequence to be used in HIERARCHY_ERROR_LOG  
+**************************************************************************/
+CREATE SEQUENCE HIER_ERROR_LOG_SEQ MINVALUE 0 MAXVALUE 999999999 INCREMENT BY 1 START WITH 0 CACHE 20 NOORDER  NOCYCLE;
