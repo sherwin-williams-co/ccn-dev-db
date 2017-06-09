8dad44c8a96f8b9b8d3b3152059fa7635accb896
unknown <jxc517@PC06G5P5.sw.sherwin.com>
2017-06-09 08:42:40 -0400

    store pos file changes - contact name, address details

---
+++ CCNDbCode/DDL/CANADA_FIPS_CODE.sql
@@ -0,0 +1,10 @@
+/*
+Created : 06/08/2017 rxv940/jxc517 CCN Project Team....
+          This table will be sued to laod the data received from Pat Malloy to load the default FIPS codes for Canadian Provinces
+*/
+
+  CREATE TABLE CANADA_FIPS_CODE
+   (	FIPS_CODE       VARCHAR2(10), 
+	PROVINCE_CODE   VARCHAR2(2), 
+	PROVINCE_NAME   VARCHAR2(50)
+   );
---
+++ CCNDbCode/DDL/Grants_Store_POS_File.sql
@@ -0,0 +1,8 @@
+/*
+Created : 06/08/2017 jxc517 CCN Project Team....
+          This script provides access to the utility schema for POS data generation needed tables
+*/
+GRANT SELECT ON ADDRESS_MEX TO CCN_UTILITY;
+GRANT SELECT ON ADDRESS_BRB TO CCN_UTILITY;
+GRANT SELECT ON ADDRESS_OTHER TO CCN_UTILITY;
+GRANT SELECT ON CANADA_FIPS_CODE TO CCN_UTILITY;
