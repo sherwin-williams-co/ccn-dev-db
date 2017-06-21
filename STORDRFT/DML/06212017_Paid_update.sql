9f29a1ddec73af3c2fe1e9c397c0d9559d46e761
Nirajan Karki <nirajan.karki@sherwin.com>
2017-06-21 08:52:04 -0400

    draft update as paid

---
+++ STORDRFT/DML/06212017_Paid_update.sql
@@ -0,0 +1,19 @@
+SET DEFINE OFF;
+/*---------------------------------------
+Below script will manually mark the drafts as paid as per email sent by Christopher T. Greve
+
+Created: 06/21/2017 nxk927
+------------------------------------------*/
+
+   
+SELECT * FROM STORE_DRAFTS WHERE COST_CENTER_CODE = '701396' AND CHECK_SERIAL_NUMBER = '0139651350';
+UPDATE STORE_DRAFTS 
+   SET PAID_DATE = '13-APR-2017', 
+       PAY_INDICATOR = 'Y', 
+       BANK_PAID_AMOUNT = DECODE(AMOUNT_CHANGE_DATE, NULL, ORIGINAL_NET_AMOUNT, NET_AMOUNT), 
+       GROSS_AMOUNT = NVL(GROSS_AMOUNT, ORIGINAL_NET_AMOUNT), 
+       NET_AMOUNT = NVL(NET_AMOUNT, ORIGINAL_NET_AMOUNT) 
+ WHERE COST_CENTER_CODE = '701396' 
+   AND CHECK_SERIAL_NUMBER = '0139651350';
+
+COMMIT;
