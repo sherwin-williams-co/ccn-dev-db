7fed9bcffe30708fab38f2457ea9ee2c5fe3a7ae
Rajasekar Ariga <rajasekar.ariga@sherwin.com>
2017-06-26 15:06:40 -0400

    Including dispatch_terminal related  changes

--- CCNDbCode/DDL/address_vw.sql
+++ CCNDbCode/DDL/address_vw.sql
@@ -1,3 +1,4 @@
+
 CREATE OR REPLACE VIEW ADDRESS_VW AS 
  SELECT 
         /***********************************************************************
@@ -15,6 +16,8 @@ CREATE OR REPLACE VIEW ADDRESS_VW AS
                  added ADDRESS_BRB to the view
                : 06/13/2017 rxa457 CCN Project Team...
                      Performance Tuning Changes...ASP-804
+               : 06/22/2017 rxa457 CCN Project Team...
+                     Changes to include corresponding Home store address for Dispatch Terminal Cost centers
         ************************************************************************/
         COST_CENTER_CD AS COST_CENTER_CODE
         ,ADMIN_COST_CNTR_TYPE AS COST_CNTR_TYPE
@@ -45,155 +48,157 @@ CREATE OR REPLACE VIEW ADDRESS_VW AS
         ,VALID_ADDRESS
         ,COUNTRY_CODE
         ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('COUNTRY_CODE','COD',M.COUNTRY_CODE),'N/A') COUNTRY_CODE_DESCRIPTION
-   FROM (
-            SELECT  ADDRESS_TYPE
-                    ,COST_CENTER_CODE
-                    ,EFFECTIVE_DATE
-                    ,EXPIRATION_DATE
-                    ,ADDRESS_LINE_1
-                    ,ADDRESS_LINE_2
-                    ,ADDRESS_LINE_3
-                    ,CITY
-                    ,PROVINCE_CODE
-                    ,NULL AS PROVINCE_CODE_DESCRIPTION
-                    ,STATE_CODE
-                    ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('STATE_CODE','COD',STATE_CODE),'N/A') STATE_CODE_DESCRIPTION
-                    ,NULL AS PREMISES
-                    ,NULL AS AVENUE_LANE
-                    ,NULL AS DISTRICT
-                    ,NULL AS PARISH
-                    ,POSTAL_CODE
-                    ,NULL AS ZIP_CODE
-                    ,NULL AS ZIP_CODE_4
-                    ,NULL AS COUNTY
-                    ,NULL AS FIPS_CODE
-                    ,NULL AS DESTINATION_POINT
-                    ,NULL AS CHECK_DIGIT
-                    ,VALID_ADDRESS
-                    ,COUNTRY_CODE
-               FROM ADDRESS_OTHER
-            UNION ALL
-            SELECT  ADDRESS_TYPE
-                    ,COST_CENTER_CODE    
-                    ,EFFECTIVE_DATE
-                    ,EXPIRATION_DATE
-                    ,ADDRESS_LINE_1
-                    ,ADDRESS_LINE_2
-                    ,ADDRESS_LINE_3
-                    ,CITY
-                    ,NULL AS PROVINCE_CODE
-                    ,NULL AS PROVINCE_CODE_DESCRIPTION
-                    ,STATE_CODE
-                    ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('STATE_CODE','COD',STATE_CODE),'N/A') STATE_CODE_DESCRIPTION
-                    ,NULL AS PREMISES
-                    ,NULL AS AVENUE_LANE
-                    ,NULL AS DISTRICT
-                    ,NULL AS PARISH
-                    ,NULL AS POSTAL_CODE
-                    ,ZIP_CODE
-                    ,ZIP_CODE_4
-                    ,COUNTY
-                    ,FIPS_CODE
-                    ,DESTINATION_POINT
-                    ,CHECK_DIGIT
-                    ,VALID_ADDRESS
-                    ,COUNTRY_CODE
-               FROM ADDRESS_USA
-            UNION ALL
-            SELECT  ADDRESS_TYPE
-                    ,COST_CENTER_CODE    
-                    ,EFFECTIVE_DATE
-                    ,EXPIRATION_DATE
-                    ,ADDRESS_LINE_1
-                    ,ADDRESS_LINE_2
-                    ,ADDRESS_LINE_3
-                    ,CITY
-                    ,PROVINCE_CODE
-                    ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('PROVINCE_CODE_CAN','COD',PROVINCE_CODE),'N/A') PROVINCE_CODE_DESCRIPTION
-                    ,NULL AS STATE_CODE
-                    ,NULL AS STATE_CODE_DESCRIPTION
-                    ,NULL AS PREMISES
-                    ,NULL AS AVENUE_LANE
-                    ,NULL AS DISTRICT
-                    ,NULL AS PARISH
-                    ,POSTAL_CODE
-                    ,NULL AS ZIP_CODE
-                    ,NULL AS ZIP_CODE_4
-                    ,NULL AS COUNTY
-                    ,NULL AS FIPS_CODE
-                    ,NULL AS DESTINATION_POINT
-                    ,NULL AS CHECK_DIGIT
-                    ,VALID_ADDRESS
-                    ,COUNTRY_CODE
-               FROM ADDRESS_CAN
-            UNION ALL 
-            SELECT  ADDRESS_TYPE
-                    ,COST_CENTER_CODE    
-                    ,EFFECTIVE_DATE
-                    ,EXPIRATION_DATE
-                    ,ADDRESS_LINE_1
-                    ,ADDRESS_LINE_2
-                    ,ADDRESS_LINE_3
-                    ,CITY
-                    ,PROVINCE_CODE
-                    ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('PROVINCE_CODE_MEX','COD',PROVINCE_CODE),'N/A') PROVINCE_CODE_DESCRIPTION
-                    ,NULL AS STATE_CODE
-                    ,NULL AS STATE_CODE_DESCRIPTION
-                    ,NULL AS PREMISES
-                    ,NULL AS AVENUE_LANE
-                    ,NULL AS DISTRICT
-                    ,NULL AS PARISH
-                    ,POSTAL_CODE
-                    ,NULL AS ZIP_CODE
-                    ,NULL AS ZIP_CODE_4
-                    ,NULL AS COUNTY
-                    ,NULL AS FIPS_CODE
-                    ,NULL AS DESTINATION_POINT
-                    ,NULL AS CHECK_DIGIT
-                    ,VALID_ADDRESS
-                    ,COUNTRY_CODE
-               FROM ADDRESS_MEX
-            UNION ALL
-            SELECT  ADDRESS_TYPE
-                    ,COST_CENTER_CODE    
-                    ,EFFECTIVE_DATE
-                    ,EXPIRATION_DATE
-                    ,NULL AS ADDRESS_LINE_1
-                    ,NULL AS ADDRESS_LINE_2
-                    ,NULL AS ADDRESS_LINE_3
-                    ,NULL AS CITY
-                    ,NULL AS PROVINCE_CODE
-                    ,NULL AS PROVINCE_CODE_DESCRIPTION
-                    ,NULL AS STATE_CODE
-                    ,NULL AS STATE_CODE_DESCRIPTION
-                    ,PREMISES
-                    ,AVENUE_LANE
-                    ,DISTRICT
-                    ,PARISH
-                    ,POSTAL_CODE
-                    ,NULL AS ZIP_CODE
-                    ,NULL AS ZIP_CODE_4
-                    ,NULL AS COUNTY
-                    ,NULL AS FIPS_CODE
-                    ,NULL AS DESTINATION_POINT
-                    ,NULL AS CHECK_DIGIT
-                    ,VALID_ADDRESS
-                    ,COUNTRY_CODE
-               FROM ADDRESS_BRB
+   FROM (SELECT ADDRESS_TYPE
+                ,COST_CENTER_CODE
+                ,EFFECTIVE_DATE
+                ,EXPIRATION_DATE
+                ,ADDRESS_LINE_1
+                ,ADDRESS_LINE_2
+                ,ADDRESS_LINE_3
+                ,CITY
+                ,PROVINCE_CODE
+                ,NULL AS PROVINCE_CODE_DESCRIPTION
+                ,STATE_CODE
+                ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('STATE_CODE','COD',STATE_CODE),'N/A') STATE_CODE_DESCRIPTION
+                ,NULL AS PREMISES
+                ,NULL AS AVENUE_LANE
+                ,NULL AS DISTRICT
+                ,NULL AS PARISH
+                ,POSTAL_CODE
+                ,NULL AS ZIP_CODE
+                ,NULL AS ZIP_CODE_4
+                ,NULL AS COUNTY
+                ,NULL AS FIPS_CODE
+                ,NULL AS DESTINATION_POINT
+                ,NULL AS CHECK_DIGIT
+                ,VALID_ADDRESS
+                ,COUNTRY_CODE
+           FROM ADDRESS_OTHER
+        UNION ALL
+         SELECT ADDRESS_TYPE
+                ,COST_CENTER_CODE    
+                ,EFFECTIVE_DATE
+                ,EXPIRATION_DATE
+                ,ADDRESS_LINE_1
+                ,ADDRESS_LINE_2
+                ,ADDRESS_LINE_3
+                ,CITY
+                ,NULL AS PROVINCE_CODE
+                ,NULL AS PROVINCE_CODE_DESCRIPTION
+                ,STATE_CODE
+                ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('STATE_CODE','COD',STATE_CODE),'N/A') STATE_CODE_DESCRIPTION
+                ,NULL AS PREMISES
+                ,NULL AS AVENUE_LANE
+                ,NULL AS DISTRICT
+                ,NULL AS PARISH
+                ,NULL AS POSTAL_CODE
+                ,ZIP_CODE
+                ,ZIP_CODE_4
+                ,COUNTY
+                ,FIPS_CODE
+                ,DESTINATION_POINT
+                ,CHECK_DIGIT
+                ,VALID_ADDRESS
+                ,COUNTRY_CODE
+           FROM ADDRESS_USA
+        UNION ALL
+         SELECT ADDRESS_TYPE
+                ,COST_CENTER_CODE    
+                ,EFFECTIVE_DATE
+                ,EXPIRATION_DATE
+                ,ADDRESS_LINE_1
+                ,ADDRESS_LINE_2
+                ,ADDRESS_LINE_3
+                ,CITY
+                ,PROVINCE_CODE
+                ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('PROVINCE_CODE_CAN','COD',PROVINCE_CODE),'N/A') PROVINCE_CODE_DESCRIPTION
+                ,NULL AS STATE_CODE
+                ,NULL AS STATE_CODE_DESCRIPTION
+                ,NULL AS PREMISES
+                ,NULL AS AVENUE_LANE
+                ,NULL AS DISTRICT
+                ,NULL AS PARISH
+                ,POSTAL_CODE
+                ,NULL AS ZIP_CODE
+                ,NULL AS ZIP_CODE_4
+                ,NULL AS COUNTY
+                ,NULL AS FIPS_CODE
+                ,NULL AS DESTINATION_POINT
+                ,NULL AS CHECK_DIGIT
+                ,VALID_ADDRESS
+                ,COUNTRY_CODE
+           FROM ADDRESS_CAN
+        UNION ALL 
+         SELECT ADDRESS_TYPE
+                ,COST_CENTER_CODE    
+                ,EFFECTIVE_DATE
+                ,EXPIRATION_DATE
+                ,ADDRESS_LINE_1
+                ,ADDRESS_LINE_2
+                ,ADDRESS_LINE_3
+                ,CITY
+                ,PROVINCE_CODE
+                ,NVL(CCN_PICK_LIST_PKG.GET_CODE_DETAIL_VALUE_DSCRPTN('PROVINCE_CODE_MEX','COD',PROVINCE_CODE),'N/A') PROVINCE_CODE_DESCRIPTION
+                ,NULL AS STATE_CODE
+                ,NULL AS STATE_CODE_DESCRIPTION
+                ,NULL AS PREMISES
+                ,NULL AS AVENUE_LANE
+                ,NULL AS DISTRICT
+                ,NULL AS PARISH
+                ,POSTAL_CODE
+                ,NULL AS ZIP_CODE
+                ,NULL AS ZIP_CODE_4
+                ,NULL AS COUNTY
+                ,NULL AS FIPS_CODE
+                ,NULL AS DESTINATION_POINT
+                ,NULL AS CHECK_DIGIT
+                ,VALID_ADDRESS
+                ,COUNTRY_CODE
+           FROM ADDRESS_MEX
+        UNION ALL
+         SELECT ADDRESS_TYPE
+                ,COST_CENTER_CODE    
+                ,EFFECTIVE_DATE
+                ,EXPIRATION_DATE
+                ,NULL AS ADDRESS_LINE_1
+                ,NULL AS ADDRESS_LINE_2
+                ,NULL AS ADDRESS_LINE_3
+                ,NULL AS CITY
+                ,NULL AS PROVINCE_CODE
+                ,NULL AS PROVINCE_CODE_DESCRIPTION
+                ,NULL AS STATE_CODE
+                ,NULL AS STATE_CODE_DESCRIPTION
+                ,PREMISES
+                ,AVENUE_LANE
+                ,DISTRICT
+                ,PARISH
+                ,POSTAL_CODE
+                ,NULL AS ZIP_CODE
+                ,NULL AS ZIP_CODE_4
+                ,NULL AS COUNTY
+                ,NULL AS FIPS_CODE
+                ,NULL AS DESTINATION_POINT
+                ,NULL AS CHECK_DIGIT
+                ,VALID_ADDRESS
+                ,COUNTRY_CODE
+           FROM ADDRESS_BRB
         )M,
-        (
-            SELECT A.ADMIN_COST_CNTR_TYPE,
-                   C.COST_CENTER_CODE AS COST_CENTER_CD,
-                   CASE
-                     WHEN C.CATEGORY = 'T' THEN 
-                       NVL ( T.HOME_STORE, C.COST_CENTER_CODE )
-                     ELSE 
-                       C.COST_CENTER_CODE
-                     END AS CC_CODE
-              FROM COST_CENTER C,
-                   TERRITORY T,
-                   ADMINISTRATION A
-             WHERE C.COST_CENTER_CODE = T.COST_CENTER_CODE(+)
-               AND C.COST_CENTER_CODE = A.COST_CENTER_CODE(+)
+        (SELECT A.ADMIN_COST_CNTR_TYPE,
+                C.COST_CENTER_CODE AS COST_CENTER_CD,
+                CASE
+                  WHEN C.CATEGORY = 'T' THEN 
+                    NVL ( T.HOME_STORE, C.COST_CENTER_CODE )
+                  WHEN C.CATEGORY = 'D' THEN
+                    NVL ( D.HOME_STORE, C.COST_CENTER_CODE )
+                  ELSE 
+                    C.COST_CENTER_CODE
+                  END AS CC_CODE
+           FROM COST_CENTER C,
+                TERRITORY T,
+                DISPATCH_TERMINAL D,
+                ADMINISTRATION A
+          WHERE C.COST_CENTER_CODE = T.COST_CENTER_CODE(+)
+            AND C.COST_CENTER_CODE = D.COST_CENTER_CODE(+)
+            AND C.COST_CENTER_CODE = A.COST_CENTER_CODE(+)
         )C
   WHERE C.CC_CODE = M.COST_CENTER_CODE;
