c1717729554fe5c203dc0c3ce257be9a4b458d3c
sudha <sudha.rani.pinnamaraju@sherwin.com>
2017-06-26 11:59:02 -0400

    ASP_805 : sxp130 - -ve onhand QTY set back to 0 for invalid/closed cost centers

---
+++ BANKING/DML/dml_upd_bank_dep_bags_tkts_onhand_qty_asp_805.sql
@@ -0,0 +1,23 @@
+/*******************************************************************************
+--This script updates Bank deposit tickets/bags -ve onhand quantiry to zero
+--Invalid cost centers or status_code <>2 those cost centers will not be applicable for
+--place order for deposit tickets/bags, hence those cost centers onhand qty set back to 0
+--This is one off script
+
+Created : 06/23/2017 sxp130 CCN Project Team....
+Changed :
+*******************************************************************************/
+UPDATE bank_dep_tick
+   SET dep_tkts_onhand_qty = 0
+ WHERE business_rules_pkg.is_placing_dpt_tkt_bag_ord_ok(cost_center_code) = UPPER('N')
+   AND dep_tkts_onhand_qty < 0;
+
+COMMIT;
+
+UPDATE bank_dep_bag_tick
+   SET depbag_onhand_qty = 0
+ WHERE business_rules_pkg.is_placing_dpt_tkt_bag_ord_ok(cost_center_code) = UPPER('N')
+   AND depbag_onhand_qty < 0;
+
+COMMIT;
+
