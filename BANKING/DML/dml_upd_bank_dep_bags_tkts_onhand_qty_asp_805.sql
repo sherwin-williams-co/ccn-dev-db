32f34b8640eaa572203e6914754378196c567b16
sudha <sudha.rani.pinnamaraju@sherwin.com>
2017-06-27 08:17:11 -0400

    ASP_805 : sxp130 - Bank Deposit tkts/bags onhand qty - added select in dml

--- BANKING/DML/dml_upd_bank_dep_bags_tkts_onhand_qty_asp_805.sql
+++ BANKING/DML/dml_upd_bank_dep_bags_tkts_onhand_qty_asp_805.sql
@@ -4,15 +4,25 @@
 --place order for deposit tickets/bags, hence those cost centers onhand qty set back to 0
 --This is one off script
 
-Created : 06/23/2017 sxp130 CCN Project Team....
+Created : 06/23/2017 sxp130 - ASP_805 CCN Project Team ....
 Changed :
 *******************************************************************************/
+SELECT * 
+  FROM bank_dep_tick 
+ WHERE business_rules_pkg.is_placing_dpt_tkt_bag_ord_ok(cost_center_code) = UPPER('N')
+   AND dep_tkts_onhand_qty < 0;
+
 UPDATE bank_dep_tick
    SET dep_tkts_onhand_qty = 0
  WHERE business_rules_pkg.is_placing_dpt_tkt_bag_ord_ok(cost_center_code) = UPPER('N')
    AND dep_tkts_onhand_qty < 0;
 
 COMMIT;
+
+SELECT * 
+  FROM bank_dep_bag_tick 
+ WHERE business_rules_pkg.is_placing_dpt_tkt_bag_ord_ok(cost_center_code) = UPPER('N')
+   AND depbag_onhand_qty < 0;
 
 UPDATE bank_dep_bag_tick
    SET depbag_onhand_qty = 0
