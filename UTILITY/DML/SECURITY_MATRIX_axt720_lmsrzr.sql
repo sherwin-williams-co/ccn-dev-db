5cd45d29a87eb98e767c8e4e1d2e453e55e40c70
Patra Bipasa <Patra.Bipasa@sherwin.com>
2017-07-06 09:31:52 -0400

    Read Only access assignment

---
+++ UTILITY/DML/SECURITY_MATRIX_axt720_lmsrzr.sql
@@ -0,0 +1,12 @@
+-- Created : pxb712 06/30/2017
+-- Purpose : This is to give SELECT access for Cost Center and Global Hierarchy  to User Toni Thomey('axt720') and Lauren M. South('lmsrzr')..
+
+
+INSERT INTO SECURITY_MATRIX values ('axt720','axt720','CCNUS1');
+INSERT INTO SECURITY_MATRIX values ('axt720','axt720','HWGUS');
+INSERT INTO SECURITY_MATRIX values ('lmsrzr','lmsrzr','CCNUS1');
+INSERT INTO SECURITY_MATRIX values ('lmsrzr','lmsrzr','HWGUS');
+
+
+ 
+COMMIT;
