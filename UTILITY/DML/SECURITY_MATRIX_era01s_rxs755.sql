b19af83c7206401335a6f141b17b890c630f096a
Patra Bipasa <Patra.Bipasa@sherwin.com>
2017-07-11 13:53:32 -0400

    Add Select access

--- UTILITY/DML/SECURITY_MATRIX_era01s_rxs755.sql
+++ UTILITY/DML/SECURITY_MATRIX_era01s_rxs755.sql
@@ -1,8 +1,6 @@
 -- Created : pxb712 07/11/2017
--- Purpose : This is to give SELECT access for New CCN Cost Center to User Beth R. Archacki('era01s') and Ramprakash Sundararajan('rxs755')..
+-- Purpose : This is to give SELECT access for New CCN Cost Center to User Ramprakash Sundararajan('rxs755')..
 
-
-INSERT INTO security_matrix VALUES( 'era01s', 'era01s', 'CCNUS3'); 
 INSERT INTO security_matrix VALUES( 'rxs755', 'rxs755', 'CCNUS3'); 
  
 COMMIT;
