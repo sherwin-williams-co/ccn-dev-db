4730bf7dad96fec43f7a8b47125d07246df73a28
Patra Bipasa <Patra.Bipasa@sherwin.com>
2017-07-18 09:24:00 -0400

    Clear of crtmp files

--- STORDRFT/UNIX/App Server/sdreport.sh
+++ STORDRFT/UNIX/App Server/sdreport.sh
@@ -7,6 +7,8 @@
 #           added the new summary report in the run1.txt file and concaneting the summary report in the final report
 #            : 05/09/2017 rxa457 - asp-781 CCN Project Team... 
 #            Handle exceptions and exit during errors
+#            : 07/17/2017 pxb712 - CCN Project Team... 
+#            called the Script remove_crtmp.sh to Clean Crystal Report temp files 
 ##########################################################
 
 DATE=`date +"%m/%d/%Y"`
@@ -55,6 +57,13 @@ echo "\n Copying file to tmp folder as a backup"
 cp $FPATH/glreport.txt $FPATH/tmp/glreport"_"$DATE.txt
 
 echo "\n Done Copying file to tmp folder as a backup"
+
+echo "\nStarted running clean temp files at $TIME on $DATE "
+
+# Call the Script to Clean Crystal Report temp files
+./remove_crtmp.sh
+
+echo "\n ended running clean temp files at $TIME on $DATE "
 
 echo "\nCompleted running files at $TIME on $DATE "
 
