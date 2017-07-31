5c1f4beb03af1990b9a57922475eefc8a355533a
Aswadh Talasila <aswadh.talasila@sherwin.com>
2017-07-31 14:40:24 -0400

    added Closing comment

--- CCNDbCode/DDL/ALTER_CCN_ACCOUNTING_TBL.sql
+++ CCNDbCode/DDL/ALTER_CCN_ACCOUNTING_TBL.sql
@@ -2,7 +2,7 @@
 created : 07/11/2017 pxb712 CCN Project Team...
         : Script to add a new column STATUS_CODE in CCN_ACCOUNTING_TBL
 
-*****************************************************************************
+*****************************************************************************/
 
 ALTER TABLE CCN_ACCOUNTING_TBL ADD STATUS_CODE VARCHAR2(1);
 
