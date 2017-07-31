fef5d9d6d2f5f4faf14956908c8b28ed3cc8cdd1
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2017-07-31 09:26:09 -0400

    Changes as per peer review..

--- CCNDbCode/DDL/ccn_accounting_view_ddl.sql
+++ CCNDbCode/DDL/ccn_accounting_view_ddl.sql
@@ -1,10 +1,11 @@
 CREATE OR REPLACE VIEW CCN_ACCOUNTING_VIEW AS
-SELECT *
+SELECT
 /*
      Created: 06/26/2017 axt754 CCN Project Team..
      This script creates a new view CCN_ACCOUNTING_VIEW for ccn 
      accounting view report
 */
+     *
   FROM CCN_ACCOUNTING_TBL
  WHERE LOAD_DATE = (SELECT MAX(LOAD_DATE)
                       FROM CCN_ACCOUNTING_TBL);
