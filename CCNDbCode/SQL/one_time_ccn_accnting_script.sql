ea0917bbc4fe076dd61710a42e86976a4c61da96
Aswadh Talasila <aswadh.talasila@sherwin.com>
2017-08-01 11:44:20 -0400

    Changes to script to run on july 31st

--- CCNDbCode/SQL/one_time_ccn_accnting_script.sql
+++ CCNDbCode/SQL/one_time_ccn_accnting_script.sql
@@ -1,10 +1,11 @@
 /* 
    created: axt754 
    Creation date: 07/05/2017
+   Modified date: 08/01/2017 axt754 Changed date to run one time on july 31st. 
    one time script to run cccn_accounting_view and report */
 
 DECLARE
-    IN_DATE DATE := TO_DATE('06/30/2017','MM/DD/YYYY');
+    IN_DATE DATE := TO_DATE('07/31/2017','MM/DD/YYYY');
 BEGIN
   CCN_ACCOUNTING_PKG.LOAD_CCN_ACCOUNTING_TABLE( IN_DATE);
 END;
