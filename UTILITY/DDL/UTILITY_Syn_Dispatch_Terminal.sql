af7d8115f561b9b0545a3a95321d043b98851fac
Rajasekar Ariga <rajasekar.ariga@sherwin.com>
2017-08-02 15:30:27 -0400

    Delete UTILITY_Syn_Dispatch_Terminal.sql

--- UTILITY/DDL/UTILITY_Syn_Dispatch_Terminal.sql
+++
@@ -1,7 +0,0 @@
-/********************************************************************************
-Creating Synonyms for DISPATCH_TERMINAL
-
-Created : 04/11/2017 rxa457 CCN Project Team....
-Changed:
-********************************************************************************/
-CREATE OR REPLACE SYNONYM DISPATCH_TERMINAL FOR COSTCNTR.DISPATCH_TERMINAL;
