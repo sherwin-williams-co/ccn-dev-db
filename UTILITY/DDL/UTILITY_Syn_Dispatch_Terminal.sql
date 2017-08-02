d794335aae034c660889401815b818daa9aebccf
rxa457 <rajasekar.ariga@sherwin.com>
2017-08-02 15:22:25 -0400

    this file will be deleted..

--- UTILITY/DDL/UTILITY_Syn_Dispatch_Terminal.sql
+++ UTILITY/DDL/UTILITY_Syn_Dispatch_Terminal.sql
@@ -2,6 +2,6 @@
 Creating Synonyms for DISPATCH_TERMINAL
 
 Created : 04/11/2017 rxa457 CCN Project Team....
-Created:
+Changed:
 ********************************************************************************/
 CREATE OR REPLACE SYNONYM DISPATCH_TERMINAL FOR COSTCNTR.DISPATCH_TERMINAL;
