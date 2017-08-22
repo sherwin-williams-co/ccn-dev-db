556c54ad2e624628738d610fe5dba41cff1f9c48
Nirajan Karki <nirajan.karki@sherwin.com>
2017-08-22 10:40:17 -0400

    Update SRA11000_cashflow_corrects_FTP.sh

--- BANKING/UNIX/App Server/SRA11000_cashflow_corrects_FTP.sh
+++ BANKING/UNIX/App Server/SRA11000_cashflow_corrects_FTP.sh
@@ -9,11 +9,13 @@
 #             added a check to see if the file has data
 # Modified : 08/14/2017 nxk927 CCN Project Team.....
 #            changed the variable name in the config file
+# Modified : 08/22/2017 nxk927 CCN Project Team.....
+#            updated the proc name
 #################################################################
 # below command will get the path for banking.config respective to the environment from which it is run from
 . /app/banking/dev/banking.config
 
-proc_name="SRA11000_corrects_FTP"
+proc_name="SRA11000_cashflow_corrects_FTP"
 DATE=`date +"%m/%d/%Y"`
 TIME=`date +"%H:%M:%S"`
 echo "Processing Started for $proc_name at $TIME on $DATE"
