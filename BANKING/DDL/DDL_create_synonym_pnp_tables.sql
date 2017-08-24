d3676a25bd9ada722c7c2b0af2346258621308b7
Ganesh Gosavi <ganesh.gosavi@sherwin.com>
2017-08-24 14:47:44 -0400

    Added a command for CCN_SALES_LINES table

--- BANKING/DDL/DDL_create_synonym_pnp_tables.sql
+++ BANKING/DDL/DDL_create_synonym_pnp_tables.sql
@@ -3,6 +3,7 @@ Description : This file has commands to create synonyms for tables that are pres
               and required to be accessed on Banking schema
 Created  : 05/10/2017 gxg192 CCN Project Team.....
 Modified : 06/19/2017 gxg192 Added a command for PNP_CCN_INTERIM_DEPOSITS
+         : 06/19/2017 gxg192 Added a command for CCN_SALES_LINES
 ****************************************************************************************************/
 
 CREATE OR REPLACE SYNONYM PNP_CCN_LOAD_STATUS FOR PNP.CCN_LOAD_STATUS;
@@ -16,3 +17,5 @@ CREATE OR REPLACE SYNONYM PNP_CCN_BATCH_SUMMARY FOR PNP.CCN_BATCH_SUMMARY;
 CREATE OR REPLACE SYNONYM PNP_CCN_HEADERS FOR PNP.CCN_HEADERS;
 
 CREATE OR REPLACE SYNONYM PNP_CCN_INTERIM_DEPOSITS FOR PNP.CCN_INTERIM_DEPOSITS;
+
+CREATE OR REPLACE SYNONYM PNP_CCN_SALES_LINES FOR PNP.CCN_SALES_LINES;
