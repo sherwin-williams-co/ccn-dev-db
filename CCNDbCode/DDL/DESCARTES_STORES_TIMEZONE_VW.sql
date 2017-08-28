f88a2bac5a7ace4e715cadd4b76252de9840c770
rxa457 <rajasekar.ariga@sherwin.com>
2017-08-28 16:44:51 -0400

    Descartes TIME ZONE view Changes

---
+++ CCNDbCode/DDL/DESCARTES_STORES_TIMEZONE_VW.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE VIEW DESCARTES_STORES_TIMEZONE_VW AS 
+  SELECT 
+        /********************************************************************************
+        This script Creates new view DESCARTES_STORES_TIMEZONE_VW 
+        to provide drivers cost centers time zone to GEMS for Descartes Feed.
+
+        Currently, This view handles US Stores and returns NULL for other countries
+
+        Created : 08/28/2017 rxa457 CCN Project Team....
+
+        ********************************************************************************/
+        COST_CENTER_CODE,
+        COMMON_TOOLS.GET_TIMEZONE(COST_CENTER_CODE) TIMEZONE
+    FROM COST_CENTER;
