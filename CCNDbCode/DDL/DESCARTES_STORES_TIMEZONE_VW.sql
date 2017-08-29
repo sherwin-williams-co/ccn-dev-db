a93378ae1fbbd17be981ee94f4b67fb456cae98b
Rajasekar Ariga <rajasekar.ariga@sherwin.com>
2017-08-29 10:42:28 -0400

    Changed field name to TIME_ZONE from "TIMEZONE"

--- CCNDbCode/DDL/DESCARTES_STORES_TIMEZONE_VW.sql
+++ CCNDbCode/DDL/DESCARTES_STORES_TIMEZONE_VW.sql
@@ -10,5 +10,5 @@ CREATE OR REPLACE VIEW DESCARTES_STORES_TIMEZONE_VW AS
 
         ********************************************************************************/
         COST_CENTER_CODE,
-        COMMON_TOOLS.GET_TIMEZONE(COST_CENTER_CODE) TIMEZONE
+        COMMON_TOOLS.GET_TIMEZONE(COST_CENTER_CODE) TIME_ZONE
     FROM COST_CENTER;
