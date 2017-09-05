fee5bf7555505fddfd6411ae932b67c1aa96987b
Nirajan Karki <nirajan.karki@sherwin.com>
2017-09-05 10:56:45 -0400

    fix for the 1st week day of the month

--- STORDRFT/UNIX/DB Server/run_installer_retainage_rfnd_rpt.sh
+++ STORDRFT/UNIX/DB Server/run_installer_retainage_rfnd_rpt.sh
@@ -6,7 +6,8 @@
 #                 This process will check if current date matches the 1st business day of the month.
 #                 If it matches then the process will start
 # Created       : 10/26/2016 nxk927.....
-# Modified      : 
+# Modified      : 09/05/2017 nxk927.....
+#                 changed the logic to get the first week day and use that date instead of using the second weekday parameter
 #################################################################
 # below command will get the path for stordrft.config respective to the environment from which it is run from
 . /app/stordrft/host.sh
@@ -20,10 +21,11 @@ DATE=`date +"%m/%d/%Y"`
 echo "RUN_INSTALLER_RETAINAGE_RFND_RPT PROCESS STARTED AT ${TIME} ON ${DATE}"
 
 TIME=`date +"%H:%M:%S"`
-SECOND_WEEKDAY=`cat $HOME/initLoad/SECOND_WEEKDAY.TXT`
-DT=$(date -d "$SECOND_WEEKDAY - 1 days" +"%m/%d/%Y")
+FIRST_WEEKDAY=`cat $HOME/initLoad/FIRST_WEEKDAY.TXT`
+echo "RUN DATE IS $DATE"
+echo "RUN DATE SHOULD BE $FIRST_WEEKDAY"
 
-if [ $DT == $DATE ]
+if [ $DATE == $FIRST_WEEKDAY ]
    then echo "MONTHLY INSTALLER RETAINAGE REFUND REPORT PROCESS STARTED AT ${TIME} ON ${DATE}"
    cd $HOME/initLoad
    sh installer_retainage_rfnd_rpt.sh
