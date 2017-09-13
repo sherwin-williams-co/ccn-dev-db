f9d4d8701cae24be998721d496285c06a7861297
Vigneshkumar Rajagopal <Vigneshkumar.Rajagopal@sherwin.com>
2017-09-13 08:25:38 -0400

    Added code to archive ".queue_trgrfile"

--- CCNDbCode/UNIX/DB Server/exec_new_stores_init_loads_bg.sh
+++ CCNDbCode/UNIX/DB Server/exec_new_stores_init_loads_bg.sh
@@ -4,7 +4,8 @@
 # Description   : This shell script will call the script to  
 #                 produce the init loads for store, terr and Param 
 # Created       : rxv940 CCN Project Team.....
-# Modified      : 
+# Modified      : rxv940 CCN Project Team.....
+#               : Added code to archive ".queue_trgrfile"
 #################################################################
 
 #nohup sh /app/ccn/dev/exec_new_stores_init_loads_bg.sh > /app/ccn/dev/datafiles/log/exec_new_stores_init_loads_bg.log 2>&1 &
@@ -22,6 +23,7 @@ then
     $HOME/call_to_init_loads_sql.sh
     echo "Trigger file to denote the completion of Init loads. " > "$DATADIR/$CCD.queue_trgrfile"
     $HOME/polling_dwnld_files_ftp_to_app_server.sh "$CCD.queue_trgrfile"
+    mv "$CCD.queue_trgrfile" "$ARCHIVEDIR"
 fi
 
 done
