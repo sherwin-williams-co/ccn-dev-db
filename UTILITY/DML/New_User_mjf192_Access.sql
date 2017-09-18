45bd5d86a14631d5a2dcfc08d96c890e52f55e46
Shahla Husain <shahla.husain@sherwin.com>
2017-09-18 13:07:33 -0400

    Adding user Michael J. Fuchs (mjf192) and deleting Alysa Gailey (amg626)

---
+++ UTILITY/DML/New_User_mjf192_Access.sql
@@ -0,0 +1,10 @@
+/**************************************************************************************
+Created : sxh487 09/18/2017
+          Adding new user Michael J. Fuchs (user ID mjf192) with the same Roles as
+          Alysa Gailey 'amg626'
+**************************************************************************************/
+SET DEFINE OFF;
+Insert into SECURITY_MATRIX (USER_ID,PASSWORD,ROLE_CODE) values ('mjf192','mjf192','CCNUS1');
+Insert into SECURITY_MATRIX (USER_ID,PASSWORD,ROLE_CODE) values ('mjf192','mjf192','SDU');
+
+COMMIT;
