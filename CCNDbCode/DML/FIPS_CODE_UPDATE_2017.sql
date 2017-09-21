728d8767c87aabaf72cd80bdcb36531a1fd6b909
Shahla Husain <shahla.husain@sherwin.com>
2017-09-21 12:38:31 -0400

    Added locking

--- CCNDbCode/DML/FIPS_CODE_UPDATE_2017.sql
+++ CCNDbCode/DML/FIPS_CODE_UPDATE_2017.sql
@@ -5,16 +5,21 @@ modified:
 *******************************************************************************/
 SET SERVEROUTPUT ON;
 BEGIN
-   FOR each_rec IN (select * from ADDRESS_USA where FIPS_CODE IS NULL AND EXPIRATION_DATE IS NULL order by COST_CENTER_CODE) 
-   LOOP
-     UPDATE ADDRESS_USA
-        SET FIPS_CODE = COMMON_TOOLS.GET_FIPS_CODE(each_rec.STATE_CODE, each_rec.COUNTY,each_rec.CITY, each_rec.ZIP_CODE)
-       WHERE COST_CENTER_CODE = each_rec.COST_CENTER_CODE
-         AND EXPIRATION_DATE IS NULL;
-   END LOOP;
-   COMMIT;
-EXCEPTION
-    WHEN OTHERS THEN
-      dbms_output.put_line('In main exception ');
+    --Audit has to go
+    CCN_BATCH_PKG.LOCK_DATABASE_SP(); --lock the database
+	BEGIN
+	   FOR each_rec IN (select * from ADDRESS_USA where FIPS_CODE IS NULL AND EXPIRATION_DATE IS NULL order by COST_CENTER_CODE) 
+	   LOOP
+	     UPDATE ADDRESS_USA
+		SET FIPS_CODE = COMMON_TOOLS.GET_FIPS_CODE(each_rec.STATE_CODE, each_rec.COUNTY,each_rec.CITY, each_rec.ZIP_CODE)
+	       WHERE COST_CENTER_CODE = each_rec.COST_CENTER_CODE
+		 AND EXPIRATION_DATE IS NULL;
+	   END LOOP;
+	   COMMIT;
+	EXCEPTION
+	    WHEN OTHERS THEN
+	      dbms_output.put_line('In main exception ');
+	END;
+   CCN_BATCH_PKG.UNLOCK_DATABASE_SP(); --unlock the database
 END;
 /
