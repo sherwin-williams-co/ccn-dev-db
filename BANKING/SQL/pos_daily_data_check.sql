69bf6318dfbda3508a5ac3ffe44b8f972d43ecc0
rxa457 <rajasekar.ariga@sherwin.com>
2017-09-27 11:11:36 -0400

    Load Date Changes

--- BANKING/SQL/pos_daily_data_check.sql
+++ BANKING/SQL/pos_daily_data_check.sql
@@ -3,6 +3,8 @@ Script to check the data source tables to confirm data are loaded for given runc
 
 USED BY  :  banking_pos_data_load_cbp.sh
 Created  :  06/08/2017 gxg192 CCN Project....
+Changed  :   09/27/2017 rxa457 CCN Project Team...
+             
 -----------------------------------------------------------------------------------------------------------*/
 SET SERVEROUTPUT ON
 SET FEEDBACK OFF
@@ -13,8 +15,7 @@ BEGIN
     SELECT COUNT(*)
       INTO V_DATA_CHECK
       FROM PNP_CCN_LOAD_STATUS
-     WHERE TRUNC(START_TS) > (SELECT MAX(TRUNC(END_TS))
-                                FROM POS_CCN_LOAD_STATUS)
+     WHERE TRUNC(START_TS) = TRUNC(SYSDATE)
        AND STATUS_CODE = 'C';
 
     IF V_DATA_CHECK = 0 THEN
