1564e5b03abd9d38a850ec6a5b6e2ef20f34ae17
Nirajan Karki <nirajan.karki@sherwin.com>
2017-09-28 16:15:00 -0400

    update

--- STORDRFT/UNIX/DB Server/sd_daily_data_check.sql
+++ STORDRFT/UNIX/DB Server/sd_daily_data_check.sql
@@ -3,6 +3,8 @@ Script to check the data source tables to confirm data are loaded for given runc
 
 USED BY  :  sd_data_status_check.sh
 Created  :  06/02/2017 nxk927
+changed  :  09/28/2017 nxk927
+            changed the logic to check the data depending on date
 -----------------------------------------------------------------------------------------------------------*/
 SET SERVEROUTPUT ON
 SET FEEDBACK OFF
@@ -13,8 +15,9 @@ BEGIN
     SELECT COUNT(*)
       INTO V_DATA_CHECK
       FROM PNP_CCN_LOAD_STATUS
-     WHERE RLS_RUN_CYCLE > (SELECT MAX(RLS_RUN_CYCLE)
-                             FROM POS_CCN_LOAD_STATUS)
+      WHERE CH.START_TS > NVL((SELECT MAX(START_TS)
+                                       FROM POS_CCN_LOAD_STATUS),
+                                     CH.START_TS)
        and STATUS_CODE = 'C';
 
     IF V_DATA_CHECK = 0 THEN
