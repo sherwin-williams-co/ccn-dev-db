a28a3df6d20774396d863bffcc04865e52b23069
Nirajan Karki <nirajan.karki@sherwin.com>
2017-09-28 14:38:19 -0400

    changes for the POS changes

--- BANKING/SQL/pos_daily_data_check.sql
+++ BANKING/SQL/pos_daily_data_check.sql
@@ -3,18 +3,23 @@ Script to check the data source tables to confirm data are loaded for given runc
 
 USED BY  :  banking_pos_data_load_cbp.sh
 Created  :  06/08/2017 gxg192 CCN Project....
-Changed  :   09/27/2017 rxa457 CCN Project Team...
-             Function GET_LATEST_POS_RUN_CYCLE called to check for new run cycles 
+Changed  :  09/27/2017 rxa457 CCN Project Team...
 -----------------------------------------------------------------------------------------------------------*/
 SET SERVEROUTPUT ON
 SET FEEDBACK OFF
 
 DECLARE
-   V_RLS_RUN_CYCLE POS_CCN_LOAD_STATUS.RLS_RUN_CYCLE%TYPE;
+    V_DATA_CHECK     NUMBER;
 BEGIN
-    V_RLS_RUN_CYCLE := POS_BANKING_DAILY_LOAD.GET_LATEST_POS_RUN_CYCLE;
-    
-    IF V_RLS_RUN_CYCLE IS NULL THEN
+    SELECT COUNT(*)
+      INTO V_DATA_CHECK
+      FROM PNP_CCN_LOAD_STATUS
+     WHERE CH.START_TS > NVL((SELECT MAX(START_TS)
+                                       FROM POS_CCN_LOAD_STATUS),
+                                     CH.START_TS)
+       AND STATUS_CODE = 'C';
+
+    IF V_DATA_CHECK = 0 THEN
        DBMS_OUTPUT.PUT_LINE('NOTREADY'); 
     ELSE
        DBMS_OUTPUT.PUT_LINE('READY'); 
