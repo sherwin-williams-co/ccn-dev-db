0a35110178d914baef8e62ea77415a4a6b3c62bb
Shahla Husain <shahla.husain@sherwin.com>
2017-10-02 09:37:40 -0400

    Grants to CC table for CSTMR Deposit

---
+++ CCNDbCode/DDL/grant_for_CUST_DEP.sql
@@ -0,0 +1,6 @@
+/********************************************************************************
+Adding Grants for COST_CENTER to CSTMR_DPSTS
+This will be run from COSTCNTR
+Created : 09/29/2017 sxh487 CCN Project Team....
+********************************************************************************/
+GRANT SELECT ON COST_CENTER TO CSTMR_DPSTS;
