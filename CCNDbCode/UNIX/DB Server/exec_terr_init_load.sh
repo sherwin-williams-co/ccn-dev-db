0e1afd9056dbc59fc528b42f79b7402c9d225870
Vigneshkumar Rajagopal <Vigneshkumar.Rajagopal@sherwin.com>
2017-10-11 08:43:23 -0400

    EOD Init load processing for STORE and TERR

---
+++ CCNDbCode/UNIX/DB Server/exec_terr_init_load.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#################################################################
+# Script name   : exec_terr_init_loads.sh
+# Description   : This shell script will call the script to  
+#                 produce the init loads for store
+# Created       : 10/10/2017 rxv940 CCN Project Team.....
+# Modified      : 
+#################################################################
+. /app/ccn/host.sh
+PROC="exec_terr_init_loads.sh"
+DATE=`date +"%Y-%m-%d"`
+TIME=`date +"%H:%M:%S"`
+LOGDIR=/app/ccn/dev/datafiles/log
+LOGFILE=exec_terr_init_loads.log
+
+echo " $PROC --> Processing Started at $TIME on $DATE"  >> $LOGDIR/$LOGFILE
+
+sqlplus -s -l $ccn_utility_un/$ccn_utility_pwd <<EOF
+set heading off;
+set serveroutput on;
+set verify off;
+var exitCode NUMBER;
+var exitCode NUMBER;
+WHENEVER OSERROR EXIT 1
+WHENEVER SQLERROR EXIT 1
+
+BEGIN
+    :exitCode := 0;
+    POS_DATA_GENERATION.INIT_LOAD_TERR_SP();
+	
+Exception
+when others then
+    :exitCode := 1;
+
+END;
+/
+
+EOF
+
+##############################################
+#  ERROR STATUS CHECK
+##############################################
+status=$?
+if [ $status -ne 0 ]
+then
+    ./send_mail.sh "POLLING_FAILURE_MAIL" "Error while processing the Init load for Territory from exec_store_init_loads.sh "
+    TIME=`date +"%H:%M:%S"`
+    echo " $PROC --> processing failed at ${TIME} on ${DATE}" >> $LOGDIR/$LOGFILE
+    exit 1
+fi
+
+TIME=`date +"%H:%M:%S"`
+echo " $PROC --> process finished successfully at ${TIME} on ${DATE} " >> $LOGDIR/$LOGFILE
+
+exit 0
