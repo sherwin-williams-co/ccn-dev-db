4f34f506b5cc5994d049cf52b1f2a2e5fcacb1dd
Nirajan Karki <nirajan.karki@sherwin.com>
2017-10-13 15:45:21 -0400

    update

--- CCNDbCode/DDL/ALTER_EMPLOYEE_DETAILS_EMAIL_ADDRESS.sql
+++ CCNDbCode/DDL/ALTER_EMPLOYEE_DETAILS_EMAIL_ADDRESS.sql
@@ -4,5 +4,6 @@ Created : 10/05/2017 rxa457 CCN project team...
 ************************************************************/
 
 ALTER TABLE EMPLOYEE_DETAILS 
- ADD EMPLOYEE_EMAIL_ADDRESS VARCHAR2(720);
+ ADD (EMPLOYEE_EMAIL_ADDRESS VARCHAR2(720),
+      USER_ID                VARCHAR2(100));
  
