da7a54c95813c12088ed3eaa4f82bb594a6edd6a
Nirajan Karki <nirajan.karki@sherwin.com>
2017-10-18 11:29:10 -0400

    update with the role codes for cvs187

--- UTILITY/DML/Security_matrix_cvs187_10172017.sql
+++ UTILITY/DML/Security_matrix_cvs187_10172017.sql
@@ -1,8 +1,16 @@
 /*
 Giving access to CCN application to cvs187
 created : nxk927 10/17/2017
+changed : nxk927 10/17/2017 removed the old role codes and added new role code as per Pat
 */
 
-INSERT INTO SECURITY_MATRIX VALUES ('cvs187','cvs187','CCNUS');
+--deleting previously given role codes
+DELETE
+  FROM SECURITY_MATRIX
+ WHERE USER_ID = 'cvs187';
+/
+--Inserting into security matrix with new role codes
+INSERT INTO SECURITY_MATRIX VALUES ('cvs187','cvs187','CCNUS1');
+INSERT INTO SECURITY_MATRIX VALUES ('cvs187','cvs187','HWGUS');
 
 COMMIT;
