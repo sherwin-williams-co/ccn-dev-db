584a1048a24d7acaa5f9e81e5270f420c5d67be4
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2017-10-30 09:45:48 -0400

    Terminal View

---
+++ CCNDbCode/DDL/Terminal_VW.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE VIEW TERMINAL_VW AS
+SELECT 
+/*******************************************************************************
+This View holds all the ACTIVE TERMINALS for cost centers
+
+Created  : 10/30/2017 axt754 CCN project Team....
+Modified :
+********************************************************************************/  
+T.COST_CENTER_CODE
+,T.POLLING_STATUS_CODE
+,T.TERMINAL_NUMBER
+,T.EFFECTIVE_DATE
+,T.EXPIRATION_DATE
+,T.POS_LAST_TRAN_DATE
+,T.POS_LAST_TRAN_NUMBER
+  FROM TERMINAL T 
+ WHERE T.EXPIRATION_DATE IS NULL 
+   AND T.POLLING_STATUS_CODE IN (SELECT P.POLLING_STATUS_CODE
+                                   FROM POLLING P
+                                  WHERE UPPER(T.COST_CENTER_CODE) = UPPER(P.COST_CENTER_CODE)
+                                    AND P.CURRENT_FLAG = 'Y' );
