22f13b2cca4432ee709d90a7079382c3bdd274b8
alekhya <alekhya@github.com>
2017-11-03 16:54:47 -0400

    VALUE LINK SHELL SCRIPTS

---
+++ CCNDbCode/UNIX/DB Server/ccn_value_link_sra31000.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+###############################################################################################################################
+# Script name   : ccn_generate_link_value.sh
+#
+# Description   : This script is to run the generate_value_link procedure
+#
+# Created  : 10/31/2017
+# 
+###############################################################################################################################
+# below command will get the path for stordrft.config respective to the environment from which it is run from
+. /app/ccn/host.sh
+
+proc="ccn_generate_link_value"
+LOGDIR="$HOME/datafiles"
+DATE=`date +"%m/%d/%Y"`
+
+TimeStamp=`date '+%Y%m%d%H%M%S'`
+
+TIME=`date +"%H:%M:%S"`
+echo "Processing Started for $proc at $TIME on $DATE"
+
+sqlplus -s -l $sqlplus_user/$sqlplus_pw >> $LOGDIR/$proc"_"$TimeStamp.log <<END
+set serveroutput on;
+set heading off;
+set verify off;
+WHENEVER OSERROR EXIT 1
+WHENEVER SQLERROR EXIT 1
+
+execute CCN_BATCH_PROCESS.GENERATE_VALUE_LINK_PROCESS;
+
+exit;
+END
+
+############################################################################
+#                           ERROR STATUS CHECK
+############################################################################
+status=$?
+TIME=`date +"%H:%M:%S"`
+if [ $status -ne 0 ]
+then
+    echo " $proc_name --> processing FAILED while executing ccn_generate_link_value.sh at $DATE:$TIME "
+    ./send_mail.sh "VALUELINK_FILE_FAILURE"
+     exit 1
+fi
+
+
+$HOME/ccn_value_link_sra31000_ftp.sh 
+
+###############################################################################################################################
+							END OF THE SCRIPT
+###############################################################################################################################
+
---
+++ CCNDbCode/UNIX/DB Server/ccn_value_link_sra31000_ftp.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+###############################################################################################################################
+# Script name   : ccn_value_link_sra31000_ftp.sh
+# Description   : This script will FTP VALUELINK FILE from db server to app server. This runs on DB Server
+#
+# Created  : 10/31/2017 rxv940 CCN Project Team.....
+# Modified : 
+###############################################################################################################################
+. /app/ccn/host.sh
+
+DATE=`date +"%m%d%y"`
+PROC_NAME=ccn_value_link_sra31000_ftp.sh
+DATADIR="$HOME"/datafiles
+FILENAME=SRA31000_${DATE}.TXT
+
+ftp -inv "${CCNAPPSERVER_HOST}"<<END_SCRIPT
+quote USER ${CCNAPPSERVER_USERNAME}
+quote PASS ${CCNAPPSERVER_PASSWORD}
+put $DATADIR/$FILENAME ${SRA31000_APP_SRVR_PATH}/SRA31000
+quit
+END_SCRIPT
+
+status=$?
+DATE=`date +"%m%d%y"`
+TIME=$(date +"%H%M%S")
+if [ $status -gt 0 ]
+then
+    $HOME/send_mail.sh "VALUELINK_FILE_FAILURE" 
+    echo " $PROC_NAME -->  processing FAILED while ftping the files VALUELINK_SRA31000 at $DATE:$TIME "
+       exit 1
+fi
+
+echo " $PROC_NAME -->  Files $FILENAME  are transferred to the app server on $DATE:$TIME "
+
+exit 0
+
