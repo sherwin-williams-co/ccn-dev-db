a05725c044e27d9ea4ca93e483f5804f9b1a3457
Vigneshkumar Rajagopal <Vigneshkumar.Rajagopal@sherwin.com>
2017-11-22 10:28:21 -0500

    Polling changes

--- CCNDbCode/UNIX/App Server/polling_new_str_load.sh
+++ CCNDbCode/UNIX/App Server/polling_new_str_load.sh
@@ -7,8 +7,6 @@
 # Created  : 10/16/2017 jxc517 CCN Project Team.....
 # Modified : 10/26/2017 rxv940 CCN Project Team.....
 #          : Added calls to PrimeSub
-# Modified : 11/03/2017 rxv940 CCN Project Team.....
-#          : Added calls to TAXCURR
 ###############################################################################################################################
 
 . /app/ccn/ccn_app_server.config
@@ -38,6 +36,8 @@ then
     exit 1
 fi
 
+echo "$QueueMessage" >> $LOGDIR/$LOGFILE 
+
 feedLog=$(java com.polling.downloads.InitialLoadProcess "NEW_STR_LD" "STORE" "$QueueMessage")
 TIME=$(date +"%H%M%S")
 echo " $PROC_NAME --> The output of the class file is $feedLog at $DATE:$TIME " >> $LOGDIR/$LOGFILE
@@ -47,10 +47,6 @@ TIME=$(date +"%H%M%S")
 echo " $PROC_NAME --> The output of the class file is $feedLog at $DATE:$TIME " >> $LOGDIR/$LOGFILE
 
 feedLog=$(java com.polling.downloads.InitialLoadProcess "NEW_STR_LD" "PrimeSub" "$QueueMessage")
-TIME=$(date +"%H%M%S")
-echo " $PROC_NAME --> The output of the class file is $feedLog at $DATE:$TIME " >> $LOGDIR/$LOGFILE
-
-feedLog=$(java com.polling.downloads.InitialLoadProcess "NEW_STR_LD" "TAXCURR" "$QueueMessage")
 TIME=$(date +"%H%M%S")
 echo " $PROC_NAME --> The output of the class file is $feedLog at $DATE:$TIME " >> $LOGDIR/$LOGFILE
 
