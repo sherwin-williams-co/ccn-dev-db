627e72385dcb0c71400969a96410489d55d53f35
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2017-12-19 10:07:19 -0500

    Added DML for BRB Country code transalation to two digit

---
+++ CCNDbCode/DML/DML_BRB_CD_TRNSLATION_TWO_DIGIT.sql
@@ -0,0 +1,10 @@
+/*
+This script inserts data into our translation tables to store Country code, and 2 digit translation 
+for BRB.. 
+
+Created : 12/19/2017 axt754 CCN Project Team....
+Changed :
+*/
+
+INSERT INTO TRANSLATION_DETAIL VALUES ('COUNTRY_CODE','BRB',12,'BB','N',NULL,NULL);
+COMMIT;
