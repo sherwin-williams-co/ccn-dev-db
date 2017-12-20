0f7970819a800cea332278251e77567423ccfe2a
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2017-12-20 09:05:42 -0500

    view creation and table/column name changes

---
+++ STORDRFT/DDL/INSTALLER_GAIN_LOSS_REPORT_VW.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW INSTALLER_GAIN_LOSS_REPORT_VW AS 
+SELECT 
+/*******************************************************************************
+This view holds all the required monthly installer gain and loss report data
+
+Created  : 12/13/2017 nxk927 CCN Project Team...
+Modified : 
+*******************************************************************************/
+       *
+  FROM SD_INSTALLER_GAIN_LOSS_REPORT;
+
