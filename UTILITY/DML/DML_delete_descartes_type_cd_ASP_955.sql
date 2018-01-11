2fd45578db88f8d2926d4c948d5aef3bc4fa4cb0
unknown <sxg151@MJ02XADH.sw.sherwin.com>
2018-01-11 16:00:52 -0500

    ASP-955 : Added comments

--- UTILITY/DML/DML_delete_descartes_type_cd_ASP_955.sql
+++ UTILITY/DML/DML_delete_descartes_type_cd_ASP_955.sql
@@ -1,4 +1,7 @@
---Delete DSCRTS_CTGRY_TYP_CD  from translation header and detail tables
+/******************************************************************************************************************* 
+Below script is created to delete Descartes category type data from  TRANSLATION_DETAIL and TRANSLATION_HEADER table.
+Created : 01/11/2018 sxp130 CCN Project ASP-955....
+********************************************************************************************************************/
 
 DELETE FROM TRANSLATION_DETAIL WHERE HEADER_NAME = 'DSCRTS_CTGRY_TYP_CD';
 DELETE FROM TRANSLATION_HEADER WHERE HEADER_NAME = 'DSCRTS_CTGRY_TYP_CD';
