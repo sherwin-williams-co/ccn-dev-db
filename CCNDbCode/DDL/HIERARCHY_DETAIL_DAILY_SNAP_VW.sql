06fb0f6ca760ed812b021996398407e031869c4e
unknown <sxg151@MJ02XADH.sw.sherwin.com>
2018-01-17 14:07:47 -0500

    ASP - 791 : Hierarchy Detail snapshots

---
+++ CCNDbCode/DDL/HIERARCHY_DETAIL_DAILY_SNAP_VW.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW HIERARCHY_DETAIL_DAILY_SNAP_VW AS
+SELECT 
+/*******************************************************************************
+Created  : 01/05/2017 SXG151 CCN
+*******************************************************************************/
+*
+From HIERARCHY_DETAIL_DAILY_SNAP;
