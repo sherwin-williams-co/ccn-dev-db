0f7646c727c1b4f330af0d65f2dd06cee7b68beb
Nirajan Karki <nirajan.karki@sherwin.com>
2018-01-22 15:05:49 -0500

    Update DDL_TERMINAL_TBL_POS_FLD.sql

--- CCNDbCode/DDL/DDL_TERMINAL_TBL_POS_FLD.sql
+++ CCNDbCode/DDL/DDL_TERMINAL_TBL_POS_FLD.sql
@@ -1,1 +1,5 @@
+/*******************************************************************************
+This ddl will add new column POS_VERSION_NBR in the terminal table
+Created  : 01/22/2018 nxk927 CCN Project....
+*******************************************************************************/
 ALTER TABLE TERMINAL ADD POS_VERSION_NBR VARCHAR2(20);
