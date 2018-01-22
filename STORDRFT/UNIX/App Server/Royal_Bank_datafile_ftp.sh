6a72c88485848e597e68235b0dbf81ddae4e8ca4
Nirajan Karki <nirajan.karki@sherwin.com>
2018-01-22 14:19:03 -0500

    Update Royal_Bank_datafile_ftp.sh

--- STORDRFT/UNIX/App Server/Royal_Bank_datafile_ftp.sh
+++ STORDRFT/UNIX/App Server/Royal_Bank_datafile_ftp.sh
@@ -3,15 +3,19 @@
 # Script Name : Royal_Bank_datafile_ftp.sh
 # Description : This Shell Script ftps the DAREPORT.txt to DB server.
 # Created     : 11/20/2017 sxh487 CCN Project
+# Modified    : 01/22/2018 nxk927 CCN Project
+#               ftping the paid files to storedraft initload folders
 ######################################################################################
 # below command will get the path for respective to the environment from which it runs from.
 . /app/strdrft/storedraft.config
 
 DATAFILES_PATH="/app/stordrft/dev/datafiles/"
+INITLOAD_PATH="/app/stordrft/dev/initLoad"
 ARCHIVE_PATH="/app/strdrft/sdReport/data/Archive/"
 SCRIPT_PATH="/app/strdrft/sdReport/scripts/"
 TIME=`date +"%H:%M:%S"`
 DATE=`date +"%m/%d/%Y"`
+TIMESTAMP="D`date +"%y%m%d"`_T`date +"%H%M%S"`"
 FTPLOG=/app/strdrft/sdReport/logs/Royal_Bank_datafile_ftp.log
 
 printf " Starting FTP Process for DAREPORT file to DB server on $DATE at $TIME \n"
@@ -22,6 +26,9 @@ quote USER ${dbserver_user}
 quote PASS ${dbserver_pw}
 cd $DATAFILES_PATH
 put DAREPORT.* DAREPORT.txt
+cd $INITLOAD_PATH
+put DAREPORT.* STBD0601_PAID_$TIMESTAMP.TXT
+put FBX275A.* STBD0101_PAID_$TIMESTAMP.TXT
 quit
 END_SCRIPT
 
@@ -37,7 +44,7 @@ TIME=`date +"%H:%M:%S"`
 
 printf "Moving $APP_USR_PATH/DAREPORT.* file to Archive folder at $TIME on $DATE \n"
 mv $APP_USR_PATH/DAREPORT.* $ARCHIVE_PATH
-
+mv $APP_USR_PATH/FBX275A.* $ARCHIVE_PATH
 if test $status -ne 0
 then
    TIME=`date +"%H:%M:%S"`
