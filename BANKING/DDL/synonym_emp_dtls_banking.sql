f9331e69c1fde42dd6a80aaab296b190701a9938
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2018-01-29 15:07:40 -0500

    Changed the source of EMPLOYEE DETAILS by using Table EMPLOYEE_DETAILS instead SWC_HR_GEMS_TB

---
+++ BANKING/DDL/synonym_emp_dtls_banking.sql
@@ -0,0 +1,7 @@
+/*
+     synonym_emp_dtls_banking.sql
+     Created : 01/29/2018 CCN Project Team....
+     This Script creates synonym for EMPLOYEE_DETAILS Table in Banking
+*/
+CREATE OR REPLACE SYNONYM EMPLOYEE_DETAILS FOR COSTCNTR.EMPLOYEE_DETAILS;
+
---
+++ CCNDbCode/DDL/grant_emp_dtls_banking.sql
@@ -0,0 +1,6 @@
+/*
+     grant_emp_dtls_banking.sql
+     Created : 01/29/2018 CCN Project Team....
+     This Script Grants access on EMPLOYEE_DETAILS Table to Banking
+*/
+GRANT ALL ON EMPLOYEE_DETAILS TO BANKING;
