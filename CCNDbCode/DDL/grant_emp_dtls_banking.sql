d1b73bec27aa56f6a6f7b84625e5ad4f79d2c39f
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2018-01-29 16:12:42 -0500

    Grant only SELECT

--- CCNDbCode/DDL/grant_emp_dtls_banking.sql
+++ CCNDbCode/DDL/grant_emp_dtls_banking.sql
@@ -3,4 +3,4 @@
      Created : 01/29/2018 CCN Project Team....
      This Script Grants access on EMPLOYEE_DETAILS Table to Banking
 */
-GRANT ALL ON EMPLOYEE_DETAILS TO BANKING;
+GRANT SELECT ON EMPLOYEE_DETAILS TO BANKING;
