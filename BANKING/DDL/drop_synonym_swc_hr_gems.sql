732ecd58a6e5f37e8cdf634415c32ddeb21c52ca
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2018-01-31 14:10:57 -0500

    ASP-985 Revoke access on SWC_HR_GEMS_TB

---
+++ BANKING/DDL/drop_synonym_swc_hr_gems.sql
@@ -0,0 +1,6 @@
+/*
+     drop_synonym_swc_hr_gems.sql
+     Created : 01/31/2018 CCN Project Team....
+     This Script DROP Synonym SWC_HR_GEMS_TB in Banking
+*/
+DROP SYNONYM SWC_HR_GEMS_TB;
---
+++ CCNDbCode/DDL/ddl_revoke_swc_hr_gems.sql
@@ -0,0 +1,6 @@
+/*
+     ddl_revoke_swc_hr_gems.sql
+     Created : 01/31/2018 CCN Project Team....
+     This Script REVOKES access on SWC_HR_GEMS_TB Table from Banking
+*/
+REVOKE SELECT ON SWC_HR_GEMS_TB FROM BANKING;
