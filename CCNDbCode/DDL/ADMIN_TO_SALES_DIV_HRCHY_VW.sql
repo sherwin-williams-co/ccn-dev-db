9d34b1ad4618583cb2472a7be4f3ebbe554563f2
sxg151 <shashank.gangimalla@sherwin.com>
2018-02-05 08:57:44 -0500

    Access on Admin to Sales views to Netezza team

---
+++ CCNDbCode/DDL/ADMIN_TO_SALES_DIV_HRCHY_VW.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW ADMIN_TO_SALES_DIV_HRCHY_VW AS
+SELECT
+/*******************************************************************************
+This View will give all the details of ADMIN_TO_SALES_DIVISION hierarchy associated to the cost center
+
+Created  : 2/2/2018 jxc517 CCN Project Team ....
+*******************************************************************************/
+STATEMENT_TYPE, STATEMENT_TYPE_DESCRIPTION, HRCHY_HDR_NAME, COST_CENTER_CODE, HRCHY_DTL_EFF_DATE, HRCHY_DTL_EXP_DATE, DOMAIN, "GROUP", DIVISION, DOMAIN_NAME, GROUP_NAME, DIVISION_NAME, COST_CENTER_NAME
+FROM ADMIN_TO_SALES_HIERARCHY_VW
+WHERE HRCHY_HDR_NAME = 'ADMIN_TO_SALES_DIVISION';
