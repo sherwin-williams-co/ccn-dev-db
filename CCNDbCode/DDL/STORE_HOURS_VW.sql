bc27aa0f70cb5dc916eb1f405ce4d6b16a50e08f
unknown <sxg151@MJ02XADH.sw.sherwin.com>
2018-02-14 16:01:26 -0500

    ASP-976: Added new View

---
+++ CCNDbCode/DDL/STORE_HOURS_VW.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FORCE VIEW STORE_HOURS_VW
+    AS 
+SELECT 
+/*******************************************************************************
+This View holds all the Store hours data
+created  : 02/12/2018 sxg151 CCN Team...
+Modified :
+********************************************************************************/
+    C.COST_CENTER_CODE,
+    SH.MON_OPEN,
+    SH.MON_CLOSE,
+    SH.TUE_OPEN,
+    SH.TUE_CLOSE,
+    SH.WED_OPEN,
+    SH.WED_CLOSE,
+    SH.THU_OPEN,
+    SH.THU_CLOSE,
+    SH.FRI_OPEN,
+    SH.FRI_CLOSE,
+    SH.SAT_OPEN,
+    SH.SAT_CLOSE,
+    SH.SUN_OPEN,
+    SH.SUN_CLOSE
+FROM
+   STORE_HOURS_V SH,COST_CENTER C
+ WHERE SH.STORE_NBR = SUBSTR(C.COST_CENTER_CODE,3);
