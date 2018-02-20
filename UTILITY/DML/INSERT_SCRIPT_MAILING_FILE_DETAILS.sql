14fa6096b9ab411a5a43e408b8d40cbd67887562
alekhya <alekhya@github.com>
2018-02-20 15:07:51 -0500

    ASP -987 CHANGES

---
+++ UTILITY/DML/INSERT_SCRIPT_MAILING_FILE_DETAILS.sql
@@ -0,0 +1,9 @@
+/*******************************************************************************
+  Insert scripts to insert record for filename'STORE_PDH_HRCHY' in 
+  Mailing file deatils table 
+
+  CREATED : 02/20/2018 bxa919 CCN PROJECT....
+*******************************************************************************/
+INSERT INTO MAILING_FILE_DETAILS (FILE_ID,GROUP_ID,FILE_NAME,DIRECTORY_NAME,IS_ACTIVE) 
+values (27,'63','STORE_PDH_HRCHY','CCN_DATAFILES','A');
+COMMIT;
