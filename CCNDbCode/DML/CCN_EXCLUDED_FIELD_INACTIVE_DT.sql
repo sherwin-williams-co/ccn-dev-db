ee874d54a7bb178d0094a9c0af582c283aa25cc4
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2018-03-01 11:03:34 -0500

    update

---
+++ CCNDbCode/DML/CCN_EXCLUDED_FIELD_INACTIVE_DT.sql
@@ -0,0 +1,12 @@
+/*******************************************************
+These scripts will insert data into the  table that holds the data for audit
+suppressed field
+
+Created : 02/28/2018 nxk927 CCN Project Team. . . .
+Changed :
+*******************************************************/
+INSERT INTO CCN_AUDIT_EXCLUDED_FIELDS VALUES ('COST_CENTER', 'INACTIVE_DATE', 'Y');
+
+COMMIT;
+
+  
