2f281102e0ce3c889ce14c5ae176a83f9f0414ef
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2018-03-05 16:07:07 -0500

    removed the comments

--- CCNDbCode/DDL/COST_POS_VIEW.sql
+++ CCNDbCode/DDL/COST_POS_VIEW.sql
@@ -10,8 +10,6 @@ Modified : 01/22/2015 SXT410 Added Columns OPEN_DATE and CLOSE_DATE.
          : 08/14/2015 NXK927 Only including current polling status
          : 03/01/2016 MXR916 Added STATE_CODE_DESCRIPTION,POLLING_STATUS_COD_DESCRIPTION,STATEMENT_TYPE_DESCRIPTION,TYPE_CODE_DESCRIPTION Columns.
          : 11/30/2016 vxv336 Added CURRENCY_CODE and CURRENCY_CODE_DESCRIPTION fields
-         : 02/13/2018 nxk927 Added inactive_date field for the closed cost center
-		 : 03/05/2018 nxk927 Removed inactive_date field
 *******************************************************************************/ 
        CC.COST_CENTER_CODE,
        CC.CATEGORY,
