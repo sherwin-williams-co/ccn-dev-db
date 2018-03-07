ed14c4a705c3ea0e1f17dd0143f523ad0a034b84
Shashank Gangimalla <shashank@github.com>
2018-03-07 15:03:26 -0500

    ASP-916 : Added new fields

--- UTILITY/DML/dml_insert_role_details_asp_916.sql
+++ UTILITY/DML/dml_insert_role_details_asp_916.sql
@@ -1,37 +1,37 @@
 /*##############################################################################################################
-     Created: 11/14/2017 sxg151 CCN Project Team..
-     This script creates a new role 'CCNMU' and assign to Marketing user 
-     'GAP54C'(Greg A. Passov),'TLC82C'(Todd Clark) 'BEO162'( Brian Otonicar )
---#############################################################################################################	 
-*/
+Created : 11/14/2017 sxg151 CCN Project Team..
+          This script creates a new role 'CCNMU' and assign to Marketing user
+          'GAP54C'(Greg A. Passov),'TLC82C'(Todd Clark) 'BEO162'( Brian Otonicar )
+Modified : 03/07/2018 sxg151... Added new fields(SALES_SQ_FT,TOTAL_SQ_FT,WAREHOUSE_SQ_FT)
+--#############################################################################################################*/
 
 Insert into ROLE_DETAILS values ('CCNMU','CCN Marketing User','Y','Y','Y','Y',
 '<USER_RULES>
-   <COST_CENTER_WINDOW>
-      <VALUE>MARKETING_FIELDS</VALUE>
-   </COST_CENTER_WINDOW>
+    <COST_CENTER_WINDOW>
+        <VALUE>MARKETING_FIELDS</VALUE>
+    </COST_CENTER_WINDOW>
 </USER_RULES>',
 '<USER_RULES_DESCRIPTION>
-   <COST_CENTER_WINDOW>
-      <MARKETING_FIELDS>
-         <VALUE>MARKETING_TYPE</VALUE>
-         <VALUE>EFFECTIVE_DATE</VALUE>
-         <VALUE>CATEGORY</VALUE>
-	 <VALUE>MKT_BRAND</VALUE>
-	 <VALUE>MKT_MISSION</VALUE>
-	 <VALUE>MKT_REAL_ESTATE_SETTING</VALUE>
-	 <VALUE>MKT_SALES_FLOOR_SIZE</VALUE>
-	 <VALUE>MKT_WAREHOUSE_SIZE</VALUE>
-   <VALUE>SALES_SQ_FT</VALUE>
-   <VALUE>TOTAL_SQ_FT</VALUE>
-   <VALUE>WAREHOUSE_SQ_FT</VALUE>
-      </MARKETING_FIELDS>
-   </COST_CENTER_WINDOW>
+    <COST_CENTER_WINDOW>
+        <MARKETING_FIELDS>
+            <VALUE>MARKETING_TYPE</VALUE>
+            <VALUE>EFFECTIVE_DATE</VALUE>
+            <VALUE>CATEGORY</VALUE>
+            <VALUE>MKT_BRAND</VALUE>
+            <VALUE>MKT_MISSION</VALUE>
+            <VALUE>MKT_REAL_ESTATE_SETTING</VALUE>
+            <VALUE>MKT_SALES_FLOOR_SIZE</VALUE>
+            <VALUE>MKT_WAREHOUSE_SIZE</VALUE>
+            <VALUE>SALES_SQ_FT</VALUE>
+            <VALUE>TOTAL_SQ_FT</VALUE>
+            <VALUE>WAREHOUSE_SQ_FT</VALUE>
+        </MARKETING_FIELDS>
+    </COST_CENTER_WINDOW>
 </USER_RULES_DESCRIPTION>');
 
+-- Below script for only in QA and PROD
 Insert into SECURITY_MATRIX values ('gap54c','gap54c','CCNMU');
 Insert into SECURITY_MATRIX values ('tlc82c','tlc82c','CCNMU');
 Insert into SECURITY_MATRIX values ('beo162','beo162','CCNMU');
 
 COMMIT;
-	 
