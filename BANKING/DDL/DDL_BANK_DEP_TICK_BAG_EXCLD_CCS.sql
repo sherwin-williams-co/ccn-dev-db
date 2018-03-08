4e39aec95662dc356e1f76d75fdaf7b04eb11859
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2018-03-08 15:26:26 -0500

    Changes as per peer review

--- BANKING/DDL/DDL_BANK_DEP_TICK_BAG_EXCLD_CCS.sql
+++ BANKING/DDL/DDL_BANK_DEP_TICK_BAG_EXCLD_CCS.sql
@@ -4,10 +4,10 @@
                 This Script Creates table BANK_DEP_TICK_BAG_EXCLD_CCS
 */
 
-CREATE TABLE BANK_DEP_TICK_BAG_EXCLD_CCS (COST_CENTER_CODE    VARCHAR2(6),
+CREATE TABLE BANK_DEP_TICK_BAG_EXCLD_CCS (COST_CENTER_CODE   VARCHAR2(6),
                                          COST_CENTER_NAME    VARCHAR2(100),
                                          POLLING_STATUS_CODE VARCHAR2(1),
                                          CATEGORY            VARCHAR2(1),
                                          COUNTRY_CODE        VARCHAR2(3),
-                                         OPEN_DATE           VARCHAR2(30),
+                                         OPEN_DATE           DATE,
                                          TICKET_BAG_TYPE     VARCHAR2(5));
