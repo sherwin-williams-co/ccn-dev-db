12e1d637cc88ac5e2c663a13db1e3ef3d0b2c0a1
Aswadh Talasila <Aswadh.Talasila@sherwin.com>
2018-03-08 14:06:58 -0500

    Changes as per peer review

--- BANKING/DDL/DDL_BANK_DEP_TICK_BAG_EXCLD_CCS.sql
+++ BANKING/DDL/DDL_BANK_DEP_TICK_BAG_EXCLD_CCS.sql
@@ -1,12 +1,13 @@
 /*
-   script name: DDL_TEMP_DEP_TICK_BAG_EXCLUDED.sql
+   script name: DDL_BANK_DEP_TICK_BAG_EXCLD_CCS.sql
    Created by : 03/07/2018 axt754 CCN Project Team...
-                This Script Creates table TEMP_DEP_TICK_EXCLUDED
+                This Script Creates table BANK_DEP_TICK_BAG_EXCLD_CCS
 */
 
-CREATE TABLE TEMP_DEP_TICK_BAG_EXCLUDED (COST_CENTER_CODE    VARCHAR2(6),
+CREATE TABLE BANK_DEP_TICK_BAG_EXCLD_CCS (COST_CENTER_CODE    VARCHAR2(6),
                                          COST_CENTER_NAME    VARCHAR2(100),
                                          POLLING_STATUS_CODE VARCHAR2(1),
                                          CATEGORY            VARCHAR2(1),
                                          COUNTRY_CODE        VARCHAR2(3),
-                                         OPEN_DATE           VARCHAR2(20));
+                                         OPEN_DATE           VARCHAR2(30),
+                                         TICKET_BAG_TYPE     VARCHAR2(5));
