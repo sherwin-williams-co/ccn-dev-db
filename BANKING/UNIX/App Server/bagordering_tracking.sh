b58d357b3e14209e1abf3c39814fea6ba7236922
Shashank Gangimalla <shashank.gangimalla@sherwin.com>
2018-03-20 15:52:56 -0400

    ASP-575 Updated script

--- BANKING/UNIX/App Server/bagordering_tracking.sh
+++ BANKING/UNIX/App Server/bagordering_tracking.sh
@@ -5,16 +5,14 @@
 #  If procedure execution succeedes then file will be archived and email will be generated.
 #  If procedure execution fails then send an email with failure text.
 # Created  : 06/23/2016 mxk766
-# Modified : 
+# Modified : 03/20/2018 sxg151 ASP-575 Updated Exception
 #################################################################
 
 # Setting config variables.
 
 . /app/banking/dev/banking.config
 
-
 #Read Variables 
-
 PROC_NAME="DPST_BAGS_UPDATE_BATCH_PKG.UPDATE_BAGORDER_TRACK_NUM"
 DATA_FILES_PATH="$HOME/initLoad"
 ARCHIVE_PATH="$HOME/datafiles/archieve"
@@ -23,13 +21,9 @@ THISSCRIPT="bagordering_tracking"
 BAGORDER_TRACKING_FILE="bagordertracking"
 DATE=`date +"%m%d%Y"`
 TIME=`date +"%H%M%S"` 
+RUN_DATE=`date +"%m/%d/%Y"`
 LOG_NAME=${THISSCRIPT}_${DATE}_${TIME}.log
 EQUAL_VAL=0
-
-#The below variable is populated from the bank_batch_date config file.
-RUN_DATE=$BAG_TRACKING_RUNDATE
-
-#echo $RUN_DATE 
 
 touch $LOGDIR/$LOG_NAME
 
@@ -42,21 +36,23 @@ set verify off;
 var exitCode number;
 WHENEVER OSERROR EXIT 1
 WHENEVER SQLERROR EXIT 1
+
 BEGIN
-:exitCode := 0;
-DPST_BAGS_UPDATE_BATCH_PKG.UPDATE_BAGORDER_TRACK_NUM('$RUN_DATE');
-Exception
-when others then
-if sqlcode = -20001 then
-:exitCode:=2;
-else
-:exitCode:=3;
-end if;
+    :exitCode := 0;
+    DPST_BAGS_UPDATE_BATCH_PKG.UPDATE_BAGORDER_TRACK_NUM('$RUN_DATE');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE(SQLCODE || ',' || SQLERRM);
+        :exitCode:=2;
 END;
 /
 
 exit :exitCode
 EOF
+
+############################################################################
+#                           ERROR STATUS CHECK
+############################################################################
 
 status=$?
 
@@ -73,8 +69,7 @@ then
     fi
 exit 1
 else
-    #Moving the processed file to archive folder first before sending the success message.
-    #This is done to make sure that we do not reprocess the same file again in case if the success email fails.
+    #Moving the processed file to archive folder.
     
     if [ -d $ARCHIVE_PATH/${BAGORDER_TRACKING_FILE}_${DATE} ]
     then
@@ -87,14 +82,10 @@ else
     mv $DATA_FILES_PATH/${BAGORDER_TRACKING_FILE}.txt $ARCHIVE_PATH/${BAGORDER_TRACKING_FILE}_${DATE}/${BAGORDER_TRACKING_FILE}_${DATE}_${TIME}.txt
     echo "File moved to archive folder "$ARCHIVE_PATH/${BAGORDER_TRACKING_FILE}_${DATE} " and renamed "${BAGORDER_TRACKING_FILE}.txt" to  "${BAGORDER_TRACKING_FILE}_${DATE}_${TIME}.txt>>$LOGDIR/${LOG_NAME}
 
-    $HOME/send_mail.sh "BAGORDER_PROCESS_COMPLETE">>$LOGDIR/${LOG_NAME}
-    status=$?
-
-    if [ $status -ne $EQUAL_VAL ]
-    then
-        echo "Mailing process failed for Mail category BAGORDER_PROCESS_COMPLETE ">>$LOGDIR/${LOG_NAME}
-        exit 1
-    fi
 fi
 
 exit 0
+
+##################################################################################################################################
+#                                                   END of PROGRAM.  
+##################################################################################################################################
