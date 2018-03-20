405da3b67aac9aba8d5486664ac978fa2c3109ff
Nirajan Karki <nirajan.karki@sherwin.com>
2018-03-20 09:11:32 -0400

    Add new file

---
+++ UTILITY/DML/cmaarr_jep01r_kxo210.sql
@@ -0,0 +1,13 @@
+/*
+Created : nxk927 03/20/2018
+          Giving polling window access to users cmaarr, jep01r, kxo210
+*/
+
+SELECT * FROM SECURITY_MATRIX WHERE USER_ID IN ('cmaarr','jep01r','kxo210');
+
+INSERT INTO SECURITY_MATRIX VALUES ('cmaarr','cmaarr','CCNUS4');
+INSERT INTO SECURITY_MATRIX VALUES ('jep01r','jep01r','CCNUS4');
+INSERT INTO SECURITY_MATRIX VALUES ('kxo210','kxo210','CCNUS4');
+COMMIT;
+
+SELECT * FROM SECURITY_MATRIX WHERE USER_ID IN ('cmaarr','jep01r','kxo210');
