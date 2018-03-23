a7054132ad5fe9fe6ba5c460c09933a8fba317f8
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2018-03-23 13:34:38 -0400

    splitting the royal_bank and suntrust bank process

--- STORDRFT/UNIX/App Server/Suntrust_Bank_datafile_ftp.sh
+++ STORDRFT/UNIX/App Server/Suntrust_Bank_datafile_ftp.sh
@@ -1,26 +1,20 @@
 #! /bin/sh
 #######################################################################################
-# Script Name : Royal_Bank_datafile_ftp.sh
-# Description : This Shell Script ftps the DAREPORT.txt to DB server.
-# Created     : 11/20/2017 sxh487 CCN Project
-# Modified    : 01/22/2018 nxk927 CCN Project
-#               ftping the paid files to storedraft initload folders
-#             : 02/09/2018 nxk927 CCN Project
-#               ftp'ing the file in binary format
-#             : 03/23/2018 nxk927 CCN Project
-#               ftp'ing the royal and suntrust bank paid files seperately.
+# Script Name : Suntrust_Bank_datafile_ftp.sh
+# Description : This Shell Script ftps the FBX275A.* to DB server.
+# Created     : 03/23/2018 nxk927 CCN Project
+# Modified    : 
 ######################################################################################
 # below command will get the path for respective to the environment from which it runs from.
 . /app/strdrft/storedraft.config
 
-DATAFILES_PATH="/app/stordrft/dev/datafiles/"
 INITLOAD_PATH="/app/stordrft/dev/initLoad"
 ARCHIVE_PATH="/app/strdrft/sdReport/data/Archive/"
 SCRIPT_PATH="/app/strdrft/sdReport/scripts/"
 TIME=`date +"%H:%M:%S"`
 DATE=`date +"%m/%d/%Y"`
 TIMESTAMP="D`date +"%y%m%d"`_T`date +"%H%M%S"`"
-FTPLOG=/app/strdrft/sdReport/logs/Royal_Bank_datafile_ftp.log
+FTPLOG=/app/strdrft/sdReport/logs/Suntrust_Bank_datafile_ftp.log
 
 printf " Starting FTP Process for DAREPORT file to DB server on $DATE at $TIME \n"
 cd $APP_USR_PATH
@@ -28,11 +22,9 @@ echo " FTP Process Started "
 ftp -inv ${dbserver_host} <<END_SCRIPT > $FTPLOG
 quote USER ${dbserver_user}
 quote PASS ${dbserver_pw}
-cd $DATAFILES_PATH
 binary
-put DAREPORT.* DAREPORT.txt
 cd $INITLOAD_PATH
-put DAREPORT.* STBD0601_PAID_$TIMESTAMP.TXT
+put FBX275A.* STBD0101_PAID_$TIMESTAMP.TXT
 quit
 END_SCRIPT
 
@@ -46,18 +38,18 @@ cd $SCRIPT_PATH
 status=$?
 TIME=`date +"%H:%M:%S"`
 
-printf "Moving $APP_USR_PATH/DAREPORT.* file to Archive folder at $TIME on $DATE \n"
-mv -f $APP_USR_PATH/DAREPORT.* $ARCHIVE_PATH
+printf "Moving $APP_USR_PATH/FBX275A.* file to Archive folder at $TIME on $DATE \n"
+mv -f $APP_USR_PATH/FBX275A.* $ARCHIVE_PATH
 if test $status -ne 0
 then
    TIME=`date +"%H:%M:%S"`
-   echo "The transfer of $APP_USR_PATH/DAREPORT.* FAILED at ${TIME} on ${DATE}"
+   echo "The transfer of $APP_USR_PATH/FBX275A.* FAILED at ${TIME} on ${DATE}"
    exit 1
 else
-   echo "The transfer of $APP_USR_PATH/DAREPORT.* completed successfully at ${TIME} on ${DATE}"
+   echo "The transfer of $APP_USR_PATH/FBX275A.* completed successfully at ${TIME} on ${DATE}"
 fi
 
-printf "FTPing finished for DAREPORT.txt file to the DB server at $TIME on $DATE \n"
+printf "FTPing finished for FBX275A.* file to the DB server at $TIME on $DATE \n"
 exit 0
 ############################################################################
 # END of PROGRAM.
