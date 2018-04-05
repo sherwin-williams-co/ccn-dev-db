0ffa14366cef3add0f7d3d5de4a46e2b1a00779a
Shahla Husain <shahla.husain@sherwin.com>
2018-04-05 13:15:26 -0400

    Update email message for Marketing Square Footage

---
+++ UTILITY/DML/EMAIL_UPDATE_DML.sql
@@ -0,0 +1,8 @@
+--updating MESSAGE for the square footage email
+--sxh487 04/05/2018
+
+UPDATE MAILING_DETAILS
+   SET MESSAGE ='Please find the attachment that contains the reason for which cost centers lease/own code (or) sq footage update failed.'
+ WHERE GROUP_ID ='104';
+ 
+COMMIT;
