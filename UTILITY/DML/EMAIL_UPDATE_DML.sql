dd6ba8e28b8ef8a066fe52d85a5d226a8c614e1b
Shahla Husain <shahla.husain@sherwin.com>
2018-04-05 13:41:00 -0400

    Changed subject for sq footage email

--- UTILITY/DML/EMAIL_UPDATE_DML.sql
+++ UTILITY/DML/EMAIL_UPDATE_DML.sql
@@ -2,7 +2,8 @@
 --sxh487 04/05/2018
 
 UPDATE MAILING_DETAILS
-   SET MESSAGE ='Please find the attachment that contains the reason for which cost centers lease/own code (or) sq footage update failed.'
+   SET MESSAGE ='Please find the attachment that contains the reason for which cost centers lease/own code (or) sq footage update failed.',
+   SUBJECT ='CCN Sq Footage Batch Process Error'
  WHERE GROUP_ID ='104';
  
 COMMIT;
