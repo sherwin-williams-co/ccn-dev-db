0cccd401e098f24d6f769da469351abb6f341885
alekhya <alekhya@github.com>
2018-04-11 13:48:48 -0400

    added Jira task number in the comments for potential open date

--- CCNDbCode/DDL/ALTER_STORE_POTENTIAL_DT.sql
+++ CCNDbCode/DDL/ALTER_STORE_POTENTIAL_DT.sql
@@ -1,6 +1,6 @@
 /*******************************************************************************
   Alter table script to add POTENTIAL_OPEN_DATE  columns to STORE table.
-  CREATED : 04/04/2018 bxa919 CCN Project...
+  CREATED : 04/04/2018 bxa919 CCN Project...ASP 867
 *******************************************************************************/
 
 --Deleting the POTENTIAL_OPEN_DATE column from COST_CENTER table .
--- CCNDbCode/DML/INSERT_CCN_AUDIT_EXCLUDE_STORE.sql
+++ CCNDbCode/DML/INSERT_CCN_AUDIT_EXCLUDE_STORE.sql
@@ -2,7 +2,7 @@
 Below script is created to insert data into CCN_AUDIT_EXCLUDED_FIELDS table.
 This table holds the table names and fields that are not part of the CCN audit.
 
-Created : 04/04/2018 bxa919 CCN Project....
+Created : 04/04/2018 bxa919 CCN Project....ASP 867
 *****************************************************************************/
 
 --Deleting POTENTIAL_OPEN_DATE column from CCN_AUDIT_EXCLUDED_FIELDS table .
