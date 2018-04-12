9958d141d330dbf6aba8cb54b94e2e39f1d6d254
Shashank Gangimalla <shashank@github.com>
2018-04-12 09:05:46 -0400

    GIT Version for Prod

--- CCNDbCode/DML/DML_stop_mailing_ASP_1016.sql
+++ CCNDbCode/DML/DML_stop_mailing_ASP_1016.sql
@@ -2,8 +2,13 @@
 Below script is created to delete MAIL_CATEGORY To stop sending emails to SIMS Group .
 Created : 04/11/2018 sxg151 CCN Project ASP-1016....
 ********************************************************************************************************************/
+SELECT * FROM MAILING_DETAILS WHERE GROUP_ID IN (64,65);
+SELECT * FROM MAILING_GROUP WHERE GROUP_ID GROUP_ID IN (64,65);
 
 DELETE MAILING_DETAILS WHERE GROUP_ID IN (64,65);
 DELETE MAILING_GROUP WHERE GROUP_ID GROUP_ID IN (64,65);
+
+SELECT * FROM MAILING_DETAILS WHERE GROUP_ID IN (64,65);
+SELECT * FROM MAILING_GROUP WHERE GROUP_ID GROUP_ID IN (64,65);
 
 COMMIT;
