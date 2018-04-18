f9cbe004cb5db3f58166ed6c424a95e062e675aa
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2018-04-18 10:23:52 -0400

    Update terminal records for last transaction details

--- CCNDbCode/DML/initial_pos_term_fields_upd.sql
+++ CCNDbCode/DML/initial_pos_term_fields_upd.sql
@@ -1,28 +1,31 @@
 /*********************************************************** 
  This is one time update script to update the pos_last_tran_date, number and pos version number field
- created : 01/24/2018 nxk927 CCN project
- revisions: 
+ created  : 01/24/2018 nxk927 CCN project
+ revisions: 04/18/2018 nxk927 CCN project
+            Updated the script to just update POS_VERSION_NBR from POS data
 ************************************************************/
 
 DECLARE
 CURSOR CUR IS
-   SELECT TRAN_DATE, STORE_NO, TERMNBR, TRANNBR, POS_VERSION_NUMBER 
-     FROM PNP.CCN_HEADERS CH
-    WHERE TRAN_TIMESTAMP =(SELECT MAX(TRAN_TIMESTAMP)
-                             FROM PNP.CCN_HEADERS
-                            WHERE STORE_NO = CH.STORE_NO
-                              AND TERMNBR = CH.TERMNBR);
+   SELECT *
+     FROM (WITH T AS
+                (SELECT TRAN_DATE, STORE_NO, TERMNBR, POS_VERSION_NUMBER
+                   FROM PNP.CCN_HEADERS CH
+                  WHERE RLS_RUN_CYCLE >= '1432'
+                    AND CH.TRAN_TIMESTAMP = (SELECT MAX(TRAN_TIMESTAMP)
+                                               FROM PNP.CCN_HEADERS
+                                              WHERE RLS_RUN_CYCLE >= '1432'
+                                                AND STORE_NO = CH.STORE_NO
+                                                AND TERMNBR = CH.TERMNBR))
+          SELECT *
+            FROM T);
 BEGIN
     FOR REC IN CUR LOOP
        UPDATE TERMINAL
-          SET POS_LAST_TRAN_DATE                        = REC.TRAN_DATE,
-              POS_LAST_TRAN_NUMBER                      = LPAD(REC.TRANNBR, 5, '0'),
-              POS_VERSION_NBR                           = REC.POS_VERSION_NUMBER
+          SET POS_VERSION_NBR                           = REC.POS_VERSION_NUMBER
         WHERE SUBSTR(COST_CENTER_CODE,3)                = REC.STORE_NO
           AND TERMINAL_NUMBER                           = REC.TERMNBR
-          AND (NVL(POS_LAST_TRAN_DATE,(TRUNC(SYSDATE))) <> NVL(REC.TRAN_DATE,TRUNC(SYSDATE)+1) OR
-               NVL(POS_LAST_TRAN_NUMBER,-1)             <> NVL(LPAD(REC.TRANNBR, 5, '0'),-2) OR
-               NVL(POS_VERSION_NBR,-1)                  <> NVL(REC.POS_VERSION_NUMBER,-2))
+          AND NVL(POS_VERSION_NBR,-1)                  <> NVL(REC.POS_VERSION_NUMBER,-2)
           AND EXPIRATION_DATE IS NULL;
     END LOOP;
     COMMIT;
