1d195f089653abbd9401bfc5aefbe42e97859625
Shashank Gangimalla <shashank@github.com>
2018-05-08 15:25:35 -0400

    ASP-916-Added New fields into Marketing Vew

--- CCNDbCode/DDL/MARKETING_VW.sql
+++ CCNDbCode/DDL/MARKETING_VW.sql
@@ -4,7 +4,8 @@ SELECT
 /*******************************************************************************
 This View will give all the details of MARKETING linked to the cost center
 Created  : 08/05/2015 axk326 CCN Project....
-Modified :
+Modified : 05/08/2018 sxg151 CCN Team... 
+           Added new fields (TOTAL_SQ_FT,SALES_SQ_FT,WAREHOUSE_SQ_FT)
 *******************************************************************************/
        COST_CENTER_CODE,
        CATEGORY,
@@ -15,5 +16,8 @@ Modified :
        MKT_MISSION,
        MKT_SALES_FLOOR_SIZE,
        MKT_WAREHOUSE_SIZE,
-       MKT_REAL_ESTATE_SETTING
+       MKT_REAL_ESTATE_SETTING,
+       TOTAL_SQ_FT,
+       SALES_SQ_FT,
+       WAREHOUSE_SQ_FT
   FROM MARKETING;
