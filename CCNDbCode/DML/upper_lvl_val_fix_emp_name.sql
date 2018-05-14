63800dd787068e1b9aadd9089a21bc3d682e5499
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2018-05-14 12:34:56 -0400

    update

--- CCNDbCode/DML/upper_lvl_val_fix_emp_name.sql
+++ CCNDbCode/DML/upper_lvl_val_fix_emp_name.sql
@@ -27,4 +27,14 @@ UPDATE HIERARCHY_DETAIL
    AND REPLACE(REPLACE(EXTRACT(UPPER_LVL_VER_VALUE,'/attributes').GETSTRINGVAL(), CHR(10)),' ') LIKE '%<Description>Picklist Value</Description></upper_lvl_ver_desc>%';
 
 
+SELECT * FROM HIERARCHY_DETAIL WHERE HRCHY_HDR_NAME = 'ADMINORG_HIERARCHY'
+AND REPLACE(EXTRACT(UPPER_LVL_VER_VALUE,'/attributes').GETSTRINGVAL(), CHR(10)) LIKE '%<Description>Picklist Value</Description></upper_lvl_ver_desc>%';
+
+UPDATE HIERARCHY_DETAIL
+   SET UPPER_LVL_VER_VALUE = REPLACE(REPLACE(EXTRACT(UPPER_LVL_VER_VALUE,'/attributes').GETSTRINGVAL(), CHR(10)),
+                                             '<Description>Picklist Value</Description></upper_lvl_ver_desc>',
+                                             '<Description>Picklist Value</Description><Value></Value></upper_lvl_ver_desc>')
+ WHERE HRCHY_HDR_NAME = 'ADMINORG_HIERARCHY'
+   AND REPLACE(EXTRACT(UPPER_LVL_VER_VALUE,'/attributes').GETSTRINGVAL(), CHR(10)) LIKE '%<Description>Picklist Value</Description></upper_lvl_ver_desc>%';
+ -- AND HRCHY_DTL_CURR_LVL_VAL = '0000200020002010800949';
 COMMIT;
