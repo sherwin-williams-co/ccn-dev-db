f2daac414db2a8012f9a8fce1f2aeafee4d11593
Shashank Gangimalla <shashank@github.com>
2018-05-15 17:28:06 -0400

    Gtant acess

---
+++ UTILITY/DML/SECURITY_MATRIX_srn584.sql
@@ -0,0 +1,11 @@
+/*########################################################################
+CREATED : sxg151 05/15/2018
+          giving access to Steven Neff   SRN584. to view CCN Main window
+		  
+###########################################################################*/
+
+Insert into SECURITY_MATRIX values ('srn584','srn584','CCNUS1');
+     
+commit;
+     
+select * from security_matrix where user_id = 'srn584';
