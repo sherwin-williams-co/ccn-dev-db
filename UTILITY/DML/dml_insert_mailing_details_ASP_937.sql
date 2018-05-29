1ee4b6ba7c8110e4fc609405085906d82f79dc40
Shashank Gangimalla <shashank@github.com>
2018-05-29 16:38:57 -0400

    ASP-937 : Updated script.

--- UTILITY/DML/dml_insert_mailing_details_ASP_937.sql
+++ UTILITY/DML/dml_insert_mailing_details_ASP_937.sql
@@ -11,11 +11,25 @@ Insert into MAILING_GROUP(GROUP_ID,MAIL_ID) values ('108','ccnoracle.team@sherwi
 -- INSERTING into MAILING_DETAILS
 
 Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE)
-VALUES ('GENERATE_EPM_FEED_FILE',
+VALUES ('GENERATE_EPM_FEED_FILE_FAIL',
         '108',
         'Generate EPM Feed File Failed.',
         'ccnoracle.team@sherwin.com',
         'Generating the EPM Feed File failed Please check the log file.',
+        'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+
+-- Insert Mailing details for FTP
+
+Insert into MAILING_GROUP(GROUP_ID,MAIL_ID) values ('111','ccnoracle.team@sherwin.com');
+
+-- INSERTING into MAILING_DETAILS
+
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE)
+VALUES ('FTP_EPM_FEED_FILE_ERROR',
+        '111',
+        'FTP EPM Feed File Failed.',
+        'ccnoracle.team@sherwin.com',
+        'EPM Feed File FTP failed Please check the log file.',
         'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
 
 
