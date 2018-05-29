5ee8e4aa308b622e12881721ac6038d9a71984cc
Nirajan Karki <nirajan.karki@sherwin.com>
2018-05-29 14:32:46 -0400

    Update initial_pos_term_fields_upd.sql

--- CCNDbCode/DML/initial_pos_term_fields_upd.sql
+++ CCNDbCode/DML/initial_pos_term_fields_upd.sql
@@ -3,22 +3,51 @@
  created  : 01/24/2018 nxk927 CCN project
  revisions: 04/18/2018 nxk927 CCN project
             Updated the script to just update POS_VERSION_NBR from POS data
+ revisions: 05/29/2018 nxk927 CCN project
+            Creating a local table and loading the data in the local table before updating the version 
+            number
 ************************************************************/
+
+  CREATE TABLE POS_VER_TERM
+   (TRAN_DATE           DATE,
+    STORE_NO            VARCHAR2(4),
+    TERMNBR             NUMBER,
+    POS_VERSION_NUMBER  NUMBER,
+    RLS_RUN_CYCLE       NUMBER);
+
+/
+
+DECLARE
+CURSOR CUR IS 
+SELECT DISTINCT TRAN_DATE, STORE_NO, TERMNBR, POS_VERSION_NUMBER, RLS_RUN_CYCLE
+  FROM PNP.CCN_HEADERS CH
+ WHERE RLS_RUN_CYCLE >= '1432';
+ 
+ V_COUNT NUMBER := 0;
+ 
+BEGIN
+    FOR REC IN CUR LOOP
+      INSERT INTO POS_VER_TERM VALUES REC;
+      V_COUNT := V_COUNT+1;
+      IF V_COUNT = 500 then
+         COMMIT;
+         V_COUNT := 0;
+      END IF;
+      END LOOP;
+END;
+
+/
+
 
 DECLARE
 CURSOR CUR IS
-   SELECT *
-     FROM (WITH T AS
-                (SELECT TRAN_DATE, STORE_NO, TERMNBR, POS_VERSION_NUMBER
-                   FROM PNP.CCN_HEADERS CH
-                  WHERE RLS_RUN_CYCLE >= '1432'
-                    AND CH.TRAN_TIMESTAMP = (SELECT MAX(TRAN_TIMESTAMP)
-                                               FROM PNP.CCN_HEADERS
-                                              WHERE RLS_RUN_CYCLE >= '1432'
-                                                AND STORE_NO = CH.STORE_NO
-                                                AND TERMNBR = CH.TERMNBR))
-          SELECT *
-            FROM T);
+   SELECT DISTINCT STORE_NO, TERMNBR, POS_VERSION_NUMBER
+     FROM POS_VER_TERM A
+    WHERE RLS_RUN_CYCLE IN (SELECT MAX(RLS_RUN_CYCLE)
+                              FROM POS_VER_TERM B
+                             WHERE B.STORE_NO = A.STORE_NO
+                               AND B.TERMNBR = A.TERMNBR);
+
   V_COUNT NUMBER := '0';
 BEGIN
     FOR REC IN CUR LOOP
@@ -40,3 +69,8 @@ BEGIN
     END LOOP;
     COMMIT;
 END;
+
+/
+/*
+--DROP TABLE TABLE POS_VER_TERM;
+*/
