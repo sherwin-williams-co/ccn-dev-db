8e470a97f23f84d2cea0e33d4c9b6b1c92f4cff1
nxk927 <nxk927@DF4KFH2.sw.sherwin.com>
2018-07-02 11:38:08 -0400

    SRA11000 changes for summary extract data

---
+++ BANKING/DDL/POS_SUMMARY_EXTRCT_CNTRL_FL.sql
@@ -0,0 +1,12 @@
+/*
+Created : 06/22/2018 nxk927 CCN Project....
+          This table will hold the data for SUMMARY_EXTRCT_CNTRL_FL
+*/
+ CREATE TABLE POS_SUMMARY_EXTRCT_CNTRL_FL
+   (COST_CENTER_CODE           VARCHAR2(6),
+    BANK_DEP_AMT               NUMBER,
+    BANK_ACCOUNT_NBR           VARCHAR2(17),
+    TRANSACTION_DATE           DATE, 
+    LOAD_DATE                  DATE,
+    ORIGINATED_BANK_ACCNT_NBR  VARCHAR2(17),
+    RLS_RUN_CYCLE              NUMBER);
