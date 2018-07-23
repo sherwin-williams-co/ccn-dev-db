4d0817f9b8fc83f618b46a626646350813b6322c
Shashank Gangimalla <shashank@github.com>
2018-07-23 15:41:48 -0400

    GIT Reversion for TEST.

--- CCNDbCode/DDL/TempTables/COST_CENTER_STORE.sql
+++ CCNDbCode/DDL/TempTables/COST_CENTER_STORE.sql
@@ -12,6 +12,10 @@ Changed : 04/25/2017 gxg192 CCN Project....
           Added Field POTENTIAL_OPEN_DATE
         : 02/02/2018 axt754 CCN Project....
           Added Fields TOTAL_SQ_FT, SALES_SQ_FT
+        : 06/28/2018 nxk927 CCN Project....
+          Added POS_NON_STORE_IND Field
+        : 07/11/2018 nxk927 CCN Project....
+          Changed the address column size from 35 to 100
 */
 
   DROP TABLE COST_CENTER_STORE;
@@ -79,9 +83,9 @@ Changed : 04/25/2017 gxg192 CCN Project....
       USA_ADDRESS_TYPE                 VARCHAR2(2),
       USA_EFFECTIVE_DATE               VARCHAR2(8),
       USA_EXPIRATION_DATE              VARCHAR2(8),
-      USA_ADDRESS_LINE_1               VARCHAR2(35),
-      USA_ADDRESS_LINE_2               VARCHAR2(35),
-      USA_ADDRESS_LINE_3               VARCHAR2(35),
+      USA_ADDRESS_LINE_1               VARCHAR2(100),
+      USA_ADDRESS_LINE_2               VARCHAR2(100),
+      USA_ADDRESS_LINE_3               VARCHAR2(100),
       USA_CITY                         VARCHAR2(25),
       USA_STATE_CODE                   VARCHAR2(2),
       USA_ZIP_CODE                     VARCHAR2(5),
@@ -95,9 +99,9 @@ Changed : 04/25/2017 gxg192 CCN Project....
       CAN_ADDRESS_TYPE                 VARCHAR2(2),
       CAN_EFFECTIVE_DATE               VARCHAR2(8),
       CAN_EXPIRATION_DATE              VARCHAR2(8),
-      CAN_ADDRESS_LINE_1               VARCHAR2(35),
-      CAN_ADDRESS_LINE_2               VARCHAR2(35),
-      CAN_ADDRESS_LINE_3               VARCHAR2(35),
+      CAN_ADDRESS_LINE_1               VARCHAR2(100),
+      CAN_ADDRESS_LINE_2               VARCHAR2(100),
+      CAN_ADDRESS_LINE_3               VARCHAR2(100),
       CAN_CITY                         VARCHAR2(25),
       CAN_PROVINCE_CODE                VARCHAR2(2),
       CAN_POSTAL_CODE                  VARCHAR2(6),
@@ -106,9 +110,9 @@ Changed : 04/25/2017 gxg192 CCN Project....
       MEX_ADDRESS_TYPE                 VARCHAR2(2),
       MEX_EFFECTIVE_DATE               VARCHAR2(8),
       MEX_EXPIRATION_DATE              VARCHAR2(8),
-      MEX_ADDRESS_LINE_1               VARCHAR2(35),
-      MEX_ADDRESS_LINE_2               VARCHAR2(35),
-      MEX_ADDRESS_LINE_3               VARCHAR2(35),
+      MEX_ADDRESS_LINE_1               VARCHAR2(100),
+      MEX_ADDRESS_LINE_2               VARCHAR2(100),
+      MEX_ADDRESS_LINE_3               VARCHAR2(100),
       MEX_CITY                         VARCHAR2(25),
       MEX_PROVINCE_CODE                VARCHAR2(5),
       MEX_POSTAL_CODE                  VARCHAR2(5),
@@ -117,9 +121,9 @@ Changed : 04/25/2017 gxg192 CCN Project....
       OTHER_ADDRESS_TYPE               VARCHAR2(2),
       OTHER_EFFECTIVE_DATE             VARCHAR2(8),
       OTHER_EXPIRATION_DATE            VARCHAR2(8),
-      OTHER_ADDRESS_LINE_1             VARCHAR2(35),
-      OTHER_ADDRESS_LINE_2             VARCHAR2(35),
-      OTHER_ADDRESS_LINE_3             VARCHAR2(35),
+      OTHER_ADDRESS_LINE_1             VARCHAR2(100),
+      OTHER_ADDRESS_LINE_2             VARCHAR2(100),
+      OTHER_ADDRESS_LINE_3             VARCHAR2(100),
       OTHER_CITY                       VARCHAR2(25),
       OTHER_PROVINCE_CODE              VARCHAR2(25),
       OTHER_STATE_CODE                 VARCHAR2(25),
@@ -161,10 +165,11 @@ Changed : 04/25/2017 gxg192 CCN Project....
       MANAGER_ID                       VARCHAR2(100),
       POTENTIAL_OPEN_DATE              VARCHAR2(8),
       TOTAL_SQ_FT                      VARCHAR2(10),
-      SALES_SQ_FT                      VARCHAR2(10)
-
-   )
-   ORGANIZATION EXTERNAL
+      SALES_SQ_FT                      VARCHAR2(10),
+      POS_NON_STORE_IND                VARCHAR2(1)
+      
+   ) 
+   ORGANIZATION EXTERNAL 
     ( TYPE ORACLE_LOADER
       DEFAULT DIRECTORY "CCN_LOAD_FILES"
       ACCESS PARAMETERS
@@ -318,7 +323,8 @@ Changed : 04/25/2017 gxg192 CCN Project....
                                         MANAGER_ID,
                                         POTENTIAL_OPEN_DATE,
                                         TOTAL_SQ_FT,
-                                        SALES_SQ_FT)
+                                        SALES_SQ_FT,
+                                        POS_NON_STORE_IND)
                      )
       LOCATION
        ( 'COST_CENTER_STORE.csv'
