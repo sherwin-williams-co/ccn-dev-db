21ff44022cd2bda118902f5697c4250d019aaa27
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2018-08-02 13:44:13 -0400

    Email id update for unbooked report - ASP-1110

--- UTILITY/DML/upd_mail_id_gl_unbooked_rpt.sql
+++ UTILITY/DML/upd_mail_id_gl_unbooked_rpt.sql
@@ -1,5 +1,7 @@
 /*
      Created: 10/03/2017 axt754 CCN Project Team..
+	 Revised: 08/02/2018 kxm302 CCN Project Team..
+              Update email id from Mary F. Kirkpatrick to Marissa Papas
      This script updates,to mail_id's
      for 'Installer G/L Unbooked Transactions Report'
 */
@@ -10,9 +12,9 @@ SELECT MG.*
  WHERE MD.GROUP_ID = MG.GROUP_ID
    AND UPPER(MD.SUBJECT) = UPPER('Installer G/L Unbooked Transactions Report');
    
--- Update MAIL_ID to 'ccnoracle.team@sherwin.com;mary.f.kirkpatrick@sherwin.com'
+-- Update MAIL_ID to 'ccnoracle.team@sherwin.com;marissa.papas@sherwin.com'
 UPDATE MAILING_GROUP
-   SET MAIL_ID = 'ccnoracle.team@sherwin.com;mary.f.kirkpatrick@sherwin.com'
+   SET MAIL_ID = 'ccnoracle.team@sherwin.com;marissa.papas@sherwin.com'
  WHERE GROUP_ID = 26; -- '26' is for 'Installer G/L Unbooked Transactions Report'
       
 --Commit The transaction
