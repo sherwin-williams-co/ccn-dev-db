41b065d7878f1d7f5bd9ac1205f36b0766b52570
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2018-08-10 11:31:48 -0400

    Mobius Report CCN05000 : Report 43 - ASP - 1114

--- CCNDbCode/UNIX/DB Server/generate_cc_numeric_seq_roster_rpt.sh
+++ CCNDbCode/UNIX/DB Server/generate_cc_numeric_seq_roster_rpt.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ##########################################################################################
-# Script Name   :  generate_ccn5000_RPRT43.sh
-# purpose of this script will be to generate CCN05000 REPORT 43 for Mobius Team
+# Script Name   :  generate_cc_numeric_seq_roster_rpt.sh
+# purpose of this script will be to generate cc_numeric_seq_roster_rpt for Mobius Team
 # details and perform the following steps:
 # 1. establish connection to SQLPLUS
-# 2. Execute CCN5000_RPRT43 procedure to get report
+# 2. Execute CC_NUMERIC_SEQ_ROSTER_RPT procedure to get report
 #    and load to CSV  file
 #
 # Date Created: 08/09/2018 kxm302
@@ -18,7 +18,7 @@ cd /app/ccn/dev
 # below command will get the path for ccn.config respective to the environment from which it is run from
 . /app/ccn/BMChost.sh
 
- proc="CCN5000_RPRT43"
+ proc="CC_NUMERIC_SEQ_ROSTER_RPT"
  LOGDIR="$HOME/datafiles/log"
  TIME=`date +"%H:%M:%S"`
  DATE=`date +"%m/%d/%Y"`
@@ -34,7 +34,7 @@ WHENEVER OSERROR EXIT 1
 WHENEVER SQLERROR EXIT 1
 BEGIN
     :exitCode := 0;
-    CCN_BATCH_PROCESS.CCN5000_RPRT43();
+    CCN_BATCH_PROCESS.CC_NUMERIC_SEQ_ROSTER_RPT();
 EXCEPTION
 WHEN OTHERS THEN
     :exitCode := 2;
@@ -50,7 +50,7 @@ if test $status -ne 0
 then
      TIME=`date +"%H:%M:%S"`
      cd $HOME
-     ./send_mail.sh "CCN5000_RPRT43" "The CCN05000 REPORT 43 file generate failed"
+     ./send_mail.sh "CC_NUMERIC_SEQ_ROSTER_RPT" "The CCN NUmeric Sequence Roster Report generate failed"
      echo "processing FAILED for $proc at ${TIME} on ${DATE}"
      exit 1;
 fi
--- UTILITY/DML/DML_CC_NUMERIC_SEQ_ROSTER_RPT.sql
+++ UTILITY/DML/DML_CC_NUMERIC_SEQ_ROSTER_RPT.sql
@@ -1,7 +1,7 @@
 /**********************************************************************************
 Below script is created to insert data into MAILING_GROUP and MAILING_DETAILS table
-for CCN05000 REPORT 43 to Mobuis Team.
-Created : 08/09/2018 kxm302 CCN Project 
+for CC_NUMERIC_SEQ_ROSTER_RPT to Mobius Team.
+Created : 08/10/2018 kxm302 CCN Project 
 Modified:
 **********************************************************************************/
 
@@ -12,16 +12,16 @@ Insert into MAILING_GROUP (GROUP_ID,MAIL_ID) values ('120','ccnoracle.team@sherw
 
 REM INSERTING into MAILING_FILE_DETAILS
 SET DEFINE OFF;
-Insert into MAILING_FILE_DETAILS (FILE_ID,GROUP_ID,FILE_NAME,DIRECTORY_NAME,IS_ACTIVE) values (1,'120','CCN5000_RPRT43','CCN_DATAFILES','A');
+Insert into MAILING_FILE_DETAILS (FILE_ID,GROUP_ID,FILE_NAME,DIRECTORY_NAME,IS_ACTIVE) values (1,'120','CC_NUMERIC_SEQ_ROSTER_RPT','CCN_DATAFILES','A');
 
 REM INSERTING into MAILING_DETAILS
 SET DEFINE OFF;
 Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) 
-values ('CCN5000_RPRT43',
+values ('CC_NUMERIC_SEQ_ROSTER_RPT',
         '120',
-        'CCN05000 REPORT 43',
+        'CC_NUMERIC_SEQ_ROSTER_RPT',
         'ccnoracle.team@sherwin.com',
-        'Please find the attached CCN05000 RPT 43.',
+        'Please find the attached Cost Center Numeric Sequence Roster Report.',
         'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
 
 COMMIT;
