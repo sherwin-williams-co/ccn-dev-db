86ea610148b08114c50dcc7ad239b568bdbea77a
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-08-10 14:41:39 -0400

    ASP-1109 Store Descartes delivery code changes after review

--- CCNDbCode/DDL/Adddescartesdeliverycodefield.sql
+++ CCNDbCode/DDL/Adddescartesdeliverycodefield.sql
@@ -5,5 +5,12 @@ CREATED : 08/09/2018 pxa852 CCN Project...
 *******************************************************************************/
 
    alter table store add descartes_delivery_code VARCHAR2(1);
+  
+   commit;
 
-commit;
+
+ -- Execute below procedure to add new column to ARC_STORE table.
+
+BEGIN
+   CC_ARCHIVE_DELET_RECREATE_PKG.RECREATE_MISMTCH_ARCHV_TBL_SP()
+END;
