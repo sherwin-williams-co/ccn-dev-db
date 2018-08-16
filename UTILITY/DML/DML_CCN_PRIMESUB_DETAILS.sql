1c5bb3f2cc6b3082971a1d83b55b859fbefacb48
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2018-08-16 13:45:42 -0400

    DML for PrimeSub changes - ASP - 1120

---
+++ UTILITY/DML/DML_CCN_PRIMESUB_DETAILS.sql
@@ -0,0 +1,27 @@
+/**********************************************************************************
+Below script is created to insert data into MAILING_GROUP and MAILING_DETAILS table
+for PRIME_SUB_DETAILS to Pats Team.
+Created : 08/16/2018 kxm302 CCN Project 
+Modified:
+**********************************************************************************/
+
+REM INSERTING into MAILING_GROUP
+SET DEFINE OFF;
+Insert into MAILING_GROUP (GROUP_ID,MAIL_ID) values ('122','ccnoracle.team@sherwin.com');
+
+
+REM INSERTING into MAILING_FILE_DETAILS
+SET DEFINE OFF;
+Insert into MAILING_FILE_DETAILS (FILE_ID,GROUP_ID,FILE_NAME,DIRECTORY_NAME,IS_ACTIVE) values (1,'122','CCN_PRIMESUB_DETAILS','CCN_DATAFILES','A');
+
+REM INSERTING into MAILING_DETAILS
+SET DEFINE OFF;
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) 
+values ('CCN_PRIMESUB_DETAILS',
+        '122',
+        'PrimeSub details report',
+        'ccnoracle.team@sherwin.com',
+        'Please find the attached Prime Sub details Report.',
+        'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+
+COMMIT;
