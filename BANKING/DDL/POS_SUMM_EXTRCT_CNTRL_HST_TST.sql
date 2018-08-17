796e7ebdf7e1d4a9f0f9a9f349e58ec92bee9baf
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-08-17 08:37:38 -0400

    ASP-1051 SRA11000 process missed transactions issue fix

---
+++ BANKING/DDL/POS_SUMM_EXTRCT_CNTRL_HST_TST.sql
@@ -0,0 +1,14 @@
+  /*
+Created : 08/14/2018 pxa852 CCN Project....
+          This table will hold the history information.
+*/
+
+CREATE TABLE POS_SUMM_EXTRCT_CNTRL_HST_TST
+   (COST_CENTER_CODE          VARCHAR2(6),
+    BANK_DEP_AMT              NUMBER,
+    BANK_ACCOUNT_NBR          VARCHAR2(17),
+    TRANSACTION_DATE          DATE,
+    LOAD_DATE                 DATE,
+    ORIGINATED_BANK_ACCNT_NBR VARCHAR2(17),
+    RLS_RUN_CYCLE             NUMBER
+   );
