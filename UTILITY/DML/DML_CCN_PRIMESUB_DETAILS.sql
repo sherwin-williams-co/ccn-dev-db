e30ab1d689e0e879e49d6c4f2e7c5190fb3b292b
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2018-08-20 14:07:48 -0400

    Added new failure category - ASP - 1120

--- UTILITY/DML/DML_CCN_PRIMESUB_DETAILS.sql
+++ UTILITY/DML/DML_CCN_PRIMESUB_DETAILS.sql
@@ -7,7 +7,9 @@ Modified:
 
 REM INSERTING into MAILING_GROUP
 SET DEFINE OFF;
-Insert into MAILING_GROUP (GROUP_ID,MAIL_ID) values ('122','ccnoracle.team@sherwin.com');
+Insert into MAILING_GROUP (GROUP_ID,MAIL_ID) values ('122','ccnoracle.team@sherwin.com;smis@sherwin.com');
+
+Insert into MAILING_GROUP (GROUP_ID,MAIL_ID) values ('123','ccnoracle.team@sherwin.com');
 
 
 REM INSERTING into MAILING_FILE_DETAILS
@@ -19,10 +21,18 @@ Insert into MAILING_FILE_DETAILS (FILE_ID,GROUP_ID,FILE_NAME,DIRECTORY_NAME,IS_A
 
 REM INSERTING into MAILING_DETAILS
 SET DEFINE OFF;
-Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) 
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE)
 values ('CCN_PRIMESUB_DETAILS',
         '122',
         'PrimeSub details report',
+        'ccnoracle.team@sherwin.com;smis@sherwin.com',
+        'Please find the attached Prime Sub details Report.',
+        'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+
+Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE)
+values ('PRIMESUB_DETAILS_FAIL',
+        '123',
+        'PrimeSub details Generation Report Failed',
         'ccnoracle.team@sherwin.com',
         'Please find the attached Prime Sub details Report.',
         'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
