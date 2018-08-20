12d323420eb2c922ec667bd1df91e2919e5c9418
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-08-20 12:05:17 -0400

    AP-1083 SRA11000 Parallel process

--- BANKING/UNIX/App Server/SRA11000_load_data_test.sh
+++ BANKING/UNIX/App Server/SRA11000_load_data_test.sh
@@ -1,53 +1,39 @@
 #!/bin/sh
 #################################################################
-# Script name   : SRA11000_load_data.sh
+# Script name   : SRA11000_load_data_test.sh
 #
 # Description   : This shell script will perform below tasks
 #                 1. load the data needed for the position_file
-#                 2. Generate the file for the same day
-# Created       : 08/11/2017 nxk927 CCN Project Team.....
-# Modified 
+# Created       : 07/27/2017 nxk927 CCN Project Team.....
+# Modified      : This script is created for running parallel process.
 #################################################################
 # below command will get the path for banking.config respective to the environment from which it is run from
-. /app/banking/dev/banking.config
+. /app/banking/test/banking.config
 
 proc_name="SRA11000_load_data"
 DATE=`date +"%m/%d/%Y"`
 TIME=`date +"%H:%M:%S"`
-LOAD_DATE=$1
+LOAD_DATE=`date +"%d-%^b-%Y"`
 echo "Processing Started for $proc_name at $TIME on $DATE"
 
 #################################################################
-#                  STR_BNK_DPST_DLY_RCNCL_PROCESS.LOAD_DATA
+#                  STR_BNK_DPST_RCNCL_PRCSS_TST.LOAD_DATA
 #################################################################
-echo "Processing started for STR_BNK_DPST_DLY_RCNCL_PROCESS.LOAD_DATA at ${TIME} on ${DATE}"
-./EXEC_PROC_1PARAM.sh "STR_BNK_DPST_DLY_RCNCL_PROCESS.LOAD_DATA" "$LOAD_DATE"
+echo "Processing started for STR_BNK_DPST_RCNCL_PRCSS_TST.LOAD_DATA at ${TIME} on ${DATE}"
+./EXEC_PROC_1PARAM.sh "STR_BNK_DPST_RCNCL_PRCSS_TST.LOAD_DATA" "$LOAD_DATE"
 
 status=$?
 if test $status -ne 0
 then
     TIME=`date +"%H:%M:%S"`
-    echo "processing FAILED for STR_BNK_DPST_DLY_RCNCL_PROCESS.LOAD_DATA at ${TIME} on ${DATE}"
+    echo "processing FAILED for STR_BNK_DPST_RCNCL_PRCSS_TST.LOAD_DATA at ${TIME} on ${DATE}"
     exit 1;
 fi
 
-TIME=`date +"%H:%M:%S"`
-echo "Processing finished for $proc_name at ${TIME} on ${DATE}"  
+echo "Processing started for generating UAR.POS file at ${TIME} on ${DATE}"
+./SRA11000_generate_files_test.sh
+echo "Processing finished for generating UAR.POS file at ${TIME} on ${DATE}"
 
-proc_name="SRA11000_GENERATE_FILES"
-TIME=`date +"%H:%M:%S"`
-#################################################################
-#                  STR_BNK_DPST_DLY_RCNCL_PROCESS.GENERATE_FILES
-#################################################################
-echo "Processing started for STR_BNK_DPST_DLY_RCNCL_PROCESS.GENERATE_FILES at ${TIME} on ${DATE}"
-./EXEC_PROC_1PARAM.sh "STR_BNK_DPST_DLY_RCNCL_PROCESS.GENERATE_FILES" "$LOAD_DATE"
-status=$?
-if test $status -ne 0
-then
-    TIME=`date +"%H:%M:%S"`
-    echo "processing FAILED for STR_BNK_DPST_DLY_RCNCL_PROCESS.GENERATE_FILES at ${TIME} on ${DATE}"
-    exit 1;
-fi
 
 TIME=`date +"%H:%M:%S"`
 echo "Processing finished for $proc_name at ${TIME} on ${DATE}"  
