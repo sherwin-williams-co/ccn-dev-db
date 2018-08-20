c0f0432644e59a4cd51c66bdb21bf004c56ec6d3
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-08-20 13:51:40 -0400

    ASP-1083 Parallel process script changes after peer review

--- BANKING/UNIX/App Server/SRA11000_load_data_test.sh
+++ BANKING/UNIX/App Server/SRA11000_load_data_test.sh
@@ -10,7 +10,7 @@
 # below command will get the path for banking.config respective to the environment from which it is run from
 . /app/banking/test/banking.config
 
-proc_name="SRA11000_load_data"
+proc_name="SRA11000_load_data_test"
 DATE=`date +"%m/%d/%Y"`
 TIME=`date +"%H:%M:%S"`
 LOAD_DATE=`date +"%d-%^b-%Y"`
@@ -30,10 +30,23 @@ then
     exit 1;
 fi
 
-echo "Processing started for generating UAR.POS file at ${TIME} on ${DATE}"
-./SRA11000_generate_files_test.sh
-echo "Processing finished for generating UAR.POS file at ${TIME} on ${DATE}"
+TIME=`date +"%H:%M:%S"`
+echo "Processing finished for $proc_name at ${TIME} on ${DATE}"
 
+proc_name="SRA11000_generate_files_test"
+TIME=`date +"%H:%M:%S"`
+#################################################################
+#                  STR_BNK_DPST_RCNCL_PRCSS_TST.GENERATE_FILES
+#################################################################
+echo "Processing started for STR_BNK_DPST_RCNCL_PRCSS_TST.GENERATE_FILES at ${TIME} on ${DATE}"
+./EXEC_PROC_1PARAM.sh "STR_BNK_DPST_RCNCL_PRCSS_TST.GENERATE_FILES" "$LOAD_DATE"
+status=$?
+if test $status -ne 0
+then
+    TIME=`date +"%H:%M:%S"`
+    echo "processing FAILED for STR_BNK_DPST_RCNCL_PRCSS_TST.GENERATE_FILES at ${TIME} on ${DATE}"
+    exit 1;
+fi
 
 TIME=`date +"%H:%M:%S"`
 echo "Processing finished for $proc_name at ${TIME} on ${DATE}"  
