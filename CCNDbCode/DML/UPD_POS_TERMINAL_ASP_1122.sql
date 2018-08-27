38a1f0c74d9f975d4126127baf925ce67d1ca98f
Shashank Gangimalla <shashank@github.com>
2018-08-27 10:06:52 -0400

    ASP-1122 : Updated script

--- CCNDbCode/DML/UPD_POS_TERMINAL_ASP_1122.sql
+++ CCNDbCode/DML/UPD_POS_TERMINAL_ASP_1122.sql
@@ -1,7 +1,8 @@
 /*********************************************************** 
  This is one time update script to update the pos_last_tran_date, number and pos version number field
  created  : 08/22/2018 sxg151 CCN project
-          : ASP-1122
+          : ASP-1122. update these fields using POS data for all run cycles since 8/2/2018 to catch up with current values.
+            Run cycle "1637" has take from Production to run since 8/2/2018.
 ************************************************************/
 
 DECLARE
@@ -10,6 +11,7 @@ SELECT DISTINCT TRAN_DATE, STORE_NO, TERMNBR,TRANNBR, POS_VERSION_NUMBER, RLS_RU
   FROM PNP.CCN_HEADERS CH
  WHERE RLS_RUN_CYCLE > '1637';
 
+V_COUNT NUMBER := '0';
 BEGIN
     FOR REC IN CUR LOOP
        UPDATE TERMINAL T
@@ -22,8 +24,11 @@ BEGIN
                 NVL(POS_LAST_TRAN_NUMBER,-1)             <> NVL(LPAD(REC.TRANNBR, 5, '0'),-2) OR
                 NVL(POS_VERSION_NBR,-1)                  <> NVL(REC.POS_VERSION_NUMBER,-2))
            AND EXPIRATION_DATE IS NULL;
-
-          COMMIT;
+        V_COUNT := V_COUNT +1;
+        IF V_COUNT = 500 then
+           V_COUNT:= 0;
+           COMMIT;
+        END IF;
     END LOOP;
     COMMIT;
 END;
