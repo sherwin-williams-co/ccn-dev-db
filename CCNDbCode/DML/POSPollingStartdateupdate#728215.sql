69ae98846def562a91ce6ac077f1ba5d039a8c26
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-09-04 10:47:58 -0400

    ASP-1125

--- CCNDbCode/DML/POSPollingStartdateupdate#728215.sql
+++ CCNDbCode/DML/POSPollingStartdateupdate#728215.sql
@@ -3,7 +3,7 @@ Script to update polling start date for cost center 728215.
 
 CREATED : 08/31/2018 pxa852 CCN Project...
 *******************************************************************************/
-SELECT * FROM POLLING WHERE COST_CENTER_CODE='728215' AND AND CURRENT_FLAG = 'Y';
+SELECT * FROM POLLING WHERE COST_CENTER_CODE='728215' AND CURRENT_FLAG = 'Y';
 
 UPDATE POLLING SET POLLING_START_DATE  = '29-AUG-2018',
                    POLLING_STOP_DATE   = NULL
@@ -11,3 +11,5 @@ UPDATE POLLING SET POLLING_START_DATE  = '29-AUG-2018',
      AND CURRENT_FLAG       = 'Y';
 
 commit;
+
+SELECT * FROM POLLING WHERE COST_CENTER_CODE='728215' AND CURRENT_FLAG = 'Y';
