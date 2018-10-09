3f639baee9e29f25488335b77042b6cc141d99e9
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-10-09 10:56:53 -0400

    SRA11000 Merge components

---
+++ BANKING/DDL/SRA11000ParallelProcessdropstmnts.sql
@@ -0,0 +1,10 @@
+  ********************************************************************************** 
+This script is used to drop package and table created for SRA11000 parallel process. These objects will be dropped from the database and new changes will be merged into actual process.
+
+Created : 10/03/2018 pxa852 CCN Project Team....
+Modified: 
+**********************************************************************************/
+
+DROP TABLE SUMMAR_EXTRCT_CNTRL_FL_TST;
+
+DROP PACKAGE STR_BNK_DPST_RCNCL_PRCSS_TST;
---
+++ BANKING/DML/SRA11000Renametablenames.sql
@@ -0,0 +1,10 @@
+  ********************************************************************************** 
+This script is used to rename table created for SRA11000 parallel process. These tables will be used in the actual process.
+
+Created : 10/03/2018 pxa852 CCN Project Team....
+Modified: 
+**********************************************************************************/
+
+ALTER TABLE POS_SUMM_EXTRCT_CNTRL_HST_TST RENAME TO POS_SUMMARY_EXTRCT_CNTRL_HST;
+
+ALTER TABLE POS_SUMM_EXTRCT_CNTRL_FL_TST RENAME TO POS_SUMMARY_EXTRCT_CNTRL_FL;
