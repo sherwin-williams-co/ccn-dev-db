bdadd87fa4b960caf8856502af349858827a21bb
Shashank Gangimalla <shashank@github.com>
2018-10-17 14:57:26 -0400

    ASP-1148 : Create a Polling view

---
+++ CCNDbCode/DDL/POLLING_VW.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW POLLING_VW AS
+SELECT 
+/*******************************************************************************
+This View contains all the records from POLLING table
+
+Created  : 10/17/2018 sxg151 CCN Project....
+
+*******************************************************************************/
+       *
+  FROM POLLING;
