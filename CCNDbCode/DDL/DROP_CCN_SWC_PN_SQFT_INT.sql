c1f82cc92f66356c21ebabab9baf2c60f7b41ad0
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-10-18 13:24:14 -0400

    ASP-886 Marketin initial load review changes

---
+++ CCNDbCode/DDL/DROP_CCN_SWC_PN_SQFT_INT.sql
@@ -0,0 +1,10 @@
+  /********************************************************************************** 
+This script is used to drop tables created to store sq footage information form GSV.
+
+Created : 10/18/2018 pxa852 CCN Project Team....
+Modified: 
+**********************************************************************************/
+
+DROP TABLE CCN_SWC_PN_SQFT_INT;
+
+DROP TABLE CCN_SWC_PN_SQFT_INT_HIST;
