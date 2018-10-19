6bec05ba32ef89ebed5cb9b0cc491ef8f53a595a
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-10-19 16:18:44 -0400

    ASP-886 drop index on marketing

---
+++ CCNDbCode/DDL/DROP_INDEX_MARKTING_PK.sql
@@ -0,0 +1,8 @@
+  /********************************************************************************** 
+This script is used to drop index created on marketing table.
+
+Created : 10/19/2018 pxa852 CCN Project Team....
+Modified: 
+**********************************************************************************/
+
+DROP INDEX MARKETING_PK;
