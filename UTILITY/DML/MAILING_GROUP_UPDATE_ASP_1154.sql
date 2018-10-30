875a05c322660076c8c13af3e3ed7b7a510d761a
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2018-10-30 15:08:09 -0400

    ASP-1154 update the distribution list for Numeric Sequence Roster Report

---
+++ UTILITY/DML/MAILING_GROUP_UPDATE_ASP_1154.sql
@@ -0,0 +1,15 @@
+/**********************************************************************************
+Below script  change mail_id for MAILING_GROUP from karen.e.wiker@sherwin.com
+to katie.m.pschesang@sherwin.com
+Created : kxm302 10/30/2018 CCN Project 
+Modified:
+**********************************************************************************/
+SELECT * FROM MAILING_GROUP WHERE GROUP_ID = '120';
+
+UPDATE MAILING_GROUP
+   SET MAIL_ID = 'ccnoracle.team@sherwin.com;katie.m.pschesang@sherwin.com'
+ WHERE GROUP_ID = '120';
+
+COMMIT;
+
+SELECT * FROM MAILING_GROUP WHERE GROUP_ID = '120';
