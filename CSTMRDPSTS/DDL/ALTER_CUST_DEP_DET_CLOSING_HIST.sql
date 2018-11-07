9b44ed1df77004ea3673dacc96cc4816428996ca
Shashank Gangimalla <shashank@github.com>
2018-11-07 11:02:30 -0500

    ASP-1150 - Spec-Updated script

---
+++ CSTMRDPSTS/DDL/ALTER_CUST_DEP_DET_CLOSING_HIST.sql
@@ -0,0 +1,9 @@
+/***************************************************
+This script to add new field to CUST_DEP_DET_CLOSING_HIST
+
+Created : 11/06/2018 sxg151 CCN Project Team....
+        : ASP-1150
+***************************************************/
+
+ALTER TABLE CUST_DEP_DET_CLOSING_HIST
+  ADD CSTMR_DPST_SALES_LN_ITM_AMT NUMBER;
