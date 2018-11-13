b042bc777461a41de21d50a58c3629ba1de55475
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-11-13 13:25:31 -0500

    ASP-1068 missing componants

--- UTILITY/DML/Hrchyfilegenrolecreation_ASP_1068.sql
+++ UTILITY/DML/Hrchyfilegenrolecreation_ASP_1068.sql
@@ -1,6 +1,4 @@
-
-
-**********************************************************************************
+/**********************************************************************************
 This script is created to create a role in ROLE_DETAILS table for providing access to Hierarchy Report button.
 
 Created : 07/13/2018 pxa852 CCN Project ASP-1068....
@@ -10,7 +8,7 @@ Modified:
 
 SET DEFINE OFF;
 
-INSERT INTO ROLE_DETAILS VALUES ('HWRPT', 'Hierarchy Window Actions - Hierarchy Report', 'N','N','N','Y',
+INSERT INTO ROLE_DETAILS VALUES ('HWRU', 'HWRU - Cost center hierarchies report user', 'N','N','N','Y',
 '<USER_RULES>
    <HIERARCHY_WINDOW>
       <VALUE>HIERARCHY_ACTIONS</VALUE>
