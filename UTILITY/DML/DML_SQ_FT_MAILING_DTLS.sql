83edcc85024ccc76a37ea07eba04b62c7cc3b6ca
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-12-14 10:02:16 -0500

    Marketing Sq footage error report changes - added cost center name, category and global hierarchy to error report

--- UTILITY/DML/DML_SQ_FT_MAILING_DTLS.sql
+++ UTILITY/DML/DML_SQ_FT_MAILING_DTLS.sql
@@ -7,7 +7,7 @@ Created : 02/20/2018 axt754 CCN Project....
 Changed : 04/13/2018 changed the body and subject
 */
 
-INSERT INTO MAILING_GROUP (GROUP_ID,MAIL_ID) VALUES ('104','ccnoracle.team@sherwin.com');
+INSERT INTO MAILING_GROUP (GROUP_ID,MAIL_ID) VALUES ('104','ccnoracle.team@sherwin.com;pmmalloy@sherwin.com');
 
 INSERT INTO MAILING_DETAILS (MAIL_CATEGORY, GROUP_ID, SUBJECT, FROM_P, MESSAGE, SIGNATURE) 
                      VALUES ('SQ_FT_CC_NOT_EXISTS_IN_CCN', 
