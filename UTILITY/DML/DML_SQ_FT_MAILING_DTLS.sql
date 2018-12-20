80aef6df67e8cc5ebd858c473dc757183ef3fefd
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2018-12-20 11:47:23 -0500

    Error rpt mailing details update

--- UTILITY/DML/DML_SQ_FT_MAILING_DTLS.sql
+++ UTILITY/DML/DML_SQ_FT_MAILING_DTLS.sql
@@ -1,21 +1,14 @@
 /*
-Script Name: DML_SQ_FT_MAILING_DTLS.sql
-Purpose    : 
-             
-
-Created : 02/20/2018 axt754 CCN Project....
-Changed : 04/13/2018 changed the body and subject
-        : 12/14/2018 pxa852 CCn Project Team...
-          Included SMIS team in the mailing group
+created: 12/14/2018 pxa852 CCn Project Team...
+         Included SMIS team in the mailing group
 */
 
-INSERT INTO MAILING_GROUP (GROUP_ID,MAIL_ID) VALUES ('104','ccnoracle.team@sherwin.com;smis@sherwin.com');
+select * from mailing_group where group_id = '104';
 
-INSERT INTO MAILING_DETAILS (MAIL_CATEGORY, GROUP_ID, SUBJECT, FROM_P, MESSAGE, SIGNATURE) 
-                     VALUES ('SQ_FT_CC_NOT_EXISTS_IN_CCN', 
-                             '104', 
-                             'CCN Sq Footage Batch Process Error', 
-                             'ccnoracle.team@sherwin.com', 
-                             'Please find the attachment that contains the reason for which cost centers lease/own code (or) sq footage update failed.', 
-                             'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+update mailing_group
+set MAIL_ID = 'ccnoracle.team@sherwin.com;smis@sherwin.com'
+where group_id = '104';
+
+select * from mailing_group where group_id = '104';
+
 COMMIT;
