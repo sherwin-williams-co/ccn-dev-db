a3481023c92f033eb52b21ee747d6e2c1b3a4842
Shashank Gangimalla <shashank@github.com>
2019-01-17 16:55:29 -0500

    ASP-1187 : Updated script.

---
+++ CSTMRDPSTS/DDL/DROP_BATCH_JOB.sql
@@ -0,0 +1,9 @@
+/*******************************************************************************
+Created : 01/17/2019 sxg151 CCN Project Team...
+        : ASP-1187 This script Drop The BATCH_JOB table.
+	
+*******************************************************************************/
+
+SELECT * FROM BATCH_JOB;
+
+DROP TABLE BATCH_JOB;
