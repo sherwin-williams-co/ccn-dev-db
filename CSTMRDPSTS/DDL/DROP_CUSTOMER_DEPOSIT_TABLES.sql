ac5beef0cac1da42ac213426c68f018f9c27bd5a
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2019-02-13 08:41:18 -0500

    ASP-1209 Drop tables

---
+++ CSTMRDPSTS/DDL/DROP_CUSTOMER_DEPOSIT_TABLES.sql
@@ -0,0 +1,9 @@
+/*******************************************************************************
+This script is to drop customer deposit tables.
+We need to drop these tables as we created new data model.
+CREATED : 02/12/2019 pxa852 CCN Project...
+*******************************************************************************/
+DROP TABLE CUSTOMER_DEPOSIT_DETAILS;
+DROP TABLE CUST_DEP_CREDIT_DETAILS;
+DROP TABLE CUST_DEP_REDEMPTION_DETAILS;
+DROP TABLE CUSTOMER_DEPOSIT_DETAILS_HIST;
