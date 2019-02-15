c3f032cb71a179951a98350a46af6147aa882bf0
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2019-02-15 10:41:47 -0500

    rename column names for COST_CENTER_VIEW_MONTHLY_SNAP table

--- CCNDbCode/DDL/COST_CENTER_VIEW_MONTHLY_SNAP_VW.sql
+++ CCNDbCode/DDL/COST_CENTER_VIEW_MONTHLY_SNAP_VW.sql
@@ -1,6 +1,8 @@
 /*******************************************************************************
 This Script will Create a view COST_CENTER_VIEW_MONTHLY_SNAP_VW
 Created  : 01/23/2019 SXG151 CCN
+         : 02/15/2019 kxm302 CCN project
+         : Renamed the column names for COST_CENTER_VIEW_MONTHLY_SNAP table
 *******************************************************************************/
 CREATE OR REPLACE VIEW COST_CENTER_VIEW_MONTHLY_SNAP_VW AS
 SELECT *
