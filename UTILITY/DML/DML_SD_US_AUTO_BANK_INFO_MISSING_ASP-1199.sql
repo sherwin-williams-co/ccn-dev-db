c5c25de4717e5ef360fc53f9d1bcfc733398b23f
Kanagaraj Marappan <Kanagaraj.Marappan@sherwin.com>
2019-02-20 10:09:16 -0500

    comments corrected for missing payee information - ASP-1199

--- UTILITY/DML/DML_SD_US_AUTO_BANK_INFO_MISSING_ASP-1199.sql
+++ UTILITY/DML/DML_SD_US_AUTO_BANK_INFO_MISSING_ASP-1199.sql
@@ -13,7 +13,7 @@ SET DEFINE OFF;
 Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) 
 values ('SD_US_AUTO_BANK_INFO_MISSING',
         '128',
-        'Store Drafts US Auto Bank Info missing Report',
+        'Store Drafts US Auto Bank missing Payee Information Report',
         'ccnoracle.team@sherwin.com',
         'Below excel contains the details about Store Drafts US Auto Bank Info missing Report.',
         'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
--- UTILITY/DML/DML_SD_US_NONAUTO_BANK_INFO_MISSING_ASP-1199 .sql
+++ UTILITY/DML/DML_SD_US_NONAUTO_BANK_INFO_MISSING_ASP-1199 .sql
@@ -13,7 +13,7 @@ SET DEFINE OFF;
 Insert into MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE) 
 values ('SD_US_NONAUTO_BANK_INFO_MISSING',
         '129',
-        'Store Drafts US NON Auto Bank Info missing Report',
+        'Store Drafts US NON Auto Bank missing Payee Information Report',
         'ccnoracle.team@sherwin.com',
         'Below excel contains the details about Store Drafts US NON Auto Bank Info missing Report.',
         'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
