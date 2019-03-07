129a7c6ef9e8e407ee476cf5176835ae0bb454f8
Jaydeep Cheruku <jaydeep.cheruku@sherwin.com>
2019-03-07 14:30:59 -0500

    CCN View Access to Brian Vandivier

---
+++ UTILITY/DML/CCNViewAccess_BrianVandivier_BDVM7R.sql
@@ -0,0 +1,11 @@
+/*
+Grant view access on CCN application to Brian Vandivier BDVM7R
+
+Created : 3/7/2019 jxc517 CCN Project Team
+Changed : 
+*/
+
+INSERT INTO SECURITY_MATRIX VALUES ('bdvm7r', 'bdvm7r', 'CCNUS');
+INSERT INTO SECURITY_MATRIX VALUES ('bdvm7r', 'bdvm7r', 'HWUS');
+INSERT INTO SECURITY_MATRIX VALUES ('bdvm7r', 'bdvm7r', 'SDUS');
+COMMIT;
