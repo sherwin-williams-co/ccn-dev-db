b7b531b3a2d55bdac44c979f3093389cdd0cbc75
Pushpalatha Alla <Pushpalatha.Alla@sherwin.com>
2019-03-26 11:08:11 -0400

    ASP-1211 Alter statement

---
+++ CSTMRDPSTS/DDL/alter_customer_deposit transaction_dtl_asp_1211.sql
@@ -0,0 +1,7 @@
+/*
+This script is used to alter CUSTOMER_DEPOSIT_TRANSACTION_DTL tables to add user_id column
+
+Created : 03/26/2019 pxa852 CCN Project Team....
+Changed :
+*/
+Alter table CUSTOMER_DEPOSIT_TRANSACTION_DTL add USER_ID VARCHAR2(6);
