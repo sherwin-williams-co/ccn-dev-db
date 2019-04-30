0a1ede8dbbb7cc44ae142b9ea564f807bd50e8ff
sxs484 <shikher.saxena@sherwin.com>
2019-04-30 11:08:19 -0400

    JIRA ASP-1254 Grant CCN main window select access to Lauren Herrmann Developer = sxs484.

---
+++ CCNDbCode/DML/dml_security_matrix_code_LXH925.sql
@@ -0,0 +1,18 @@
+/*
+     dml_security_matrix_code_LXH925.sql
+     Created : 04/30/2019 CCN Project Team....
+     This Script assigns role code CCNUS1 for lxh925
+*/
+
+
+-- Check the roles for 'lxh925'
+SELECT * FROM SECURITY_MATRIX WHERE UPPER(USER_ID) = 'LXH925';
+
+-- assign newly created role code to 'lxh925'
+INSERT INTO SECURITY_MATRIX VALUES ('lxh925','lxh925','CCNUS1');
+
+-- Check the roles for 'lxh925'
+SELECT * FROM SECURITY_MATRIX WHERE UPPER(USER_ID) = 'LXH925';
+
+-- Commit The Transaction
+COMMIT;
