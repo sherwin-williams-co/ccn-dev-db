50be7a598328127e85b83832f798c8f578213b0c
Shashank Gangimalla <shashank@github.com>
2019-05-07 16:49:05 -0400

    ASP-1156 : Updated script.

---
+++ CCNDbCode/DDL/DDL_BULK_LOAD_PROCESSES.sql
@@ -0,0 +1,20 @@
+/********************************************************************************
+Create a table "BULK_LOAD_PROCESSES" and Grant Select access to CCNUSER
+
+Created : 12/12/2018 sxg151 CCN Project Team....
+        : ASP-1156
+Created:
+********************************************************************************/
+
+CREATE TABLE BULK_LOAD_PROCESSES
+(
+ BULK_LOAD_PROCESS_ID          NUMBER
+,BULK_LOAD_PROCESS_NAME        VARCHAR2(100)
+,BULK_LOAD_PROCESS_DESCRIPTION VARCHAR2(200)
+,BULK_LOAD_EXT_TBL_NAME        VARCHAR2(30)
+,BULK_LOAD_PORCESS_EFFCTV_DT   DATE
+,BULK_LOAD_PORCESS_EXPRTN_DT   DATE
+,USER_ID                       VARCHAR2(6)
+);
+
+GRANT SELECT ON BULK_LOAD_PROCESSES TO CCNUSER;
