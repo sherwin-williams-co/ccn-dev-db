eaa833f8a555cad94668bddbdf2317d9905ab87f
Shashank Gangimalla <shashank@github.com>
2019-05-09 10:10:49 -0400

    ASP-1259 : Updated script.

--- CSTMRDPSTS/DML/dml_customer_deposit_transaction_dtl_upd_ASP_1251.sql
+++ CSTMRDPSTS/DML/dml_customer_deposit_transaction_dtl_upd_ASP_1251.sql
@@ -2,7 +2,8 @@
 This script will  rounded to two number decimal places for the manual correction account numbers and update the GL_DIVISION with 'A222'
 
 Created  : 05/07/2019 CCN Team...
-         : ASP-1251 
+         : ASP-1251 : There was a rounding issue with the previous update. Showing more than 2 decimal points in "CSTMR_DPST_SALES_LN_ITM_AMT, CUSTOMER_NET_BALANCE fields,
+           It should show only two decimal points.intial update was made on '29-APR-19' since then Net Balance and Sales line item amount showing more than two decimal points for the updated accounts
 */
 UPDATE CUSTOMER_DEPOSIT_TRANSACTION_DTL
    SET CSTMR_DPST_SALES_LN_ITM_AMT = round(CSTMR_DPST_SALES_LN_ITM_AMT,2),
@@ -10,11 +11,7 @@ UPDATE CUSTOMER_DEPOSIT_TRANSACTION_DTL
  WHERE trunc(LOAD_DATE) >= '29-APR-19'
    AND (round(CSTMR_DPST_SALES_LN_ITM_AMT,2) <> CSTMR_DPST_SALES_LN_ITM_AMT OR round(CUSTOMER_NET_BALANCE,2) <> CUSTOMER_NET_BALANCE);
    
-   
-UPDATE CUSTOMER_DEPOSIT_TRANSACTION_DTL
-   SET GL_DIVISION = 'A222'
- WHERE USER_ID = 'lxn782'
-   AND trunc(LOAD_DATE) = '29-APR-19';
+
 
 SELECT CSTMR_DPST_SALES_LN_ITM_AMT,round(CSTMR_DPST_SALES_LN_ITM_AMT,2),CUSTOMER_NET_BALANCE,round(CUSTOMER_NET_BALANCE,2) 
   FROM CUSTOMER_DEPOSIT_TRANSACTION_DTL 
--- CSTMRDPSTS/DML/dml_gl_division_upd_ASP-1259.sql
+++ CSTMRDPSTS/DML/dml_gl_division_upd_ASP-1259.sql
@@ -4,19 +4,28 @@ ASP-1259 : Update all the transactions that have NULL value for GL_DIVISION.
          : This script that can be re-used till we implement the changes to make this field mandatory
 */
 DECLARE
+   V_COUNT NUMBER := 0;
+
    CURSOR CUR_COST_CENTER IS
         SELECT DISTINCT 
                COST_CENTER_CODE,
                COMMON_TOOLS.GET_GL_DIVISION(DTL.COST_CENTER_CODE) GL_DIVISION
           FROM CUSTOMER_DEPOSIT_TRANSACTION_DTL DTL
          WHERE DTL.GL_DIVISION IS NULL;
-   BEGIN
-      FOR rec IN  CUR_COST_CENTER
-         LOOP
-            UPDATE CUSTOMER_DEPOSIT_TRANSACTION_DTL DTL
-               SET GL_DIVISION          = rec.GL_DIVISION
-             WHERE DTL.COST_CENTER_CODE = rec.COST_CENTER_CODE
-               AND DTL.GL_DIVISION IS NULL;
-         END LOOP;
-   COMMIT;
+BEGIN
+    FOR rec IN  CUR_COST_CENTER
+       LOOP
+          UPDATE CUSTOMER_DEPOSIT_TRANSACTION_DTL DTL
+             SET GL_DIVISION          = rec.GL_DIVISION
+           WHERE DTL.COST_CENTER_CODE = rec.COST_CENTER_CODE
+             AND DTL.GL_DIVISION IS NULL;
+
+          IF V_COUNT > 500 THEN
+                COMMIT;
+             V_COUNT := 0;
+          ELSE
+              V_COUNT := V_COUNT + 1;
+          END IF;
+       END LOOP;
+COMMIT;
 END;
