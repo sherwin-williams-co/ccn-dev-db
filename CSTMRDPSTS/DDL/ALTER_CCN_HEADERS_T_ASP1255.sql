28484c69fdf8a1fa1828ee578fc86c2b8d91d414
sxs484 <shikher.saxena@sherwin.com>
2019-06-07 11:42:04 -0400

    ASP1255- SXS484 . Cstmr Dep. Made changes to remove field store_no.

--- CSTMRDPSTS/DDL/ALTER_CCN_HEADERS_T_ASP1255.sql
+++ CSTMRDPSTS/DDL/ALTER_CCN_HEADERS_T_ASP1255.sql
@@ -1,8 +1,12 @@
 /*
 ASP-1255 This script will add STORECCN column in CCN_HEADERS_T table.
+         AND drop obsolete 4 chars column store_no .       
 Created  : 05/31/2019 sxs484 
 */
     ALTER TABLE CCN_HEADERS_T
-    ADD (STORECCN VARCHAR2(6))
-;
+    ADD (STORECCN VARCHAR2(6));
+
+    ALTER TABLE CCN_HEADERS_T
+    DROP COLUMN STORE_NO
+    ;
 /
