ee2cfc8def10f0f7713af8ed7f7a3fc0d69e7082
sxs484 <shikher.saxena@sherwin.com>
2019-06-11 13:17:01 -0400

    ASP-1277- sxs484 . Added scripts for net bal correction.

---
+++ CSTMRDPSTS/DDL/TEMP_FIX_NET_BAL_06092019_ASP1277 .sql
@@ -0,0 +1,8 @@
+/*
+ASP-1277 create temp table TEMP_FIX_NET_BAL.
+06092019 SXS484 CCN Project Team...
+*/  
+
+  CREATE TABLE TEMP_FIX_NET_BAL_06092019
+   (	customer_account_number VARCHAR2(9))
+;
---
+++ CSTMRDPSTS/DML/INSERT_TEMP_FIX_NET_BAL_06092019_ASP1277 .sql
@@ -0,0 +1,22 @@
+/*
+ASP-1277 Insert cust deposits accts shared by User into temp table.
+06092019 SXS484 CCN Project Team...
+*/
+
+insert into TEMP_FIX_NET_BAL_06092019 values (298582800);
+insert into TEMP_FIX_NET_BAL_06092019 values (945561967);
+insert into TEMP_FIX_NET_BAL_06092019 values (528385313);
+insert into TEMP_FIX_NET_BAL_06092019 values (789536877);
+insert into TEMP_FIX_NET_BAL_06092019 values (783504095);
+insert into TEMP_FIX_NET_BAL_06092019 values (798871638);
+insert into TEMP_FIX_NET_BAL_06092019 values (425471661);
+insert into TEMP_FIX_NET_BAL_06092019 values (421208869);
+insert into TEMP_FIX_NET_BAL_06092019 values (580389609);
+insert into TEMP_FIX_NET_BAL_06092019 values (340455641);
+insert into TEMP_FIX_NET_BAL_06092019 values (424071082);
+insert into TEMP_FIX_NET_BAL_06092019 values (399852185);
+insert into TEMP_FIX_NET_BAL_06092019 values (569126246);
+
+
+commit;
+/
