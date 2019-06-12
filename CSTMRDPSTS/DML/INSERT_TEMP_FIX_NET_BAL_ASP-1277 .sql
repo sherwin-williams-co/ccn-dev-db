8903cbfadc915ade8a76400be42d376e47c8f33a
sxs484 <shikher.saxena@sherwin.com>
2019-06-12 13:16:17 -0400

    ASP1277 - sxs484 updated script naming and included quotes in insert command .

---
+++ CSTMRDPSTS/DML/INSERT_TEMP_FIX_NET_BAL_ASP-1277 .sql
@@ -0,0 +1,44 @@
+/*
+ASP-1277 Insert cust deposits accts shared by User into temp table.
+		 06092019 SXS484 CCN Project Team.
+changes --06112019- Added 35 more accts which have same issue in Prod. 
+		 These accts were identified by running query.
+
+*/
+insert into TEMP_FIX_NET_BAL_06092019 values ('1');
+insert into TEMP_FIX_NET_BAL_06092019 values ('3');
+insert into TEMP_FIX_NET_BAL_06092019 values ('190558320');
+insert into TEMP_FIX_NET_BAL_06092019 values ('231159468');
+insert into TEMP_FIX_NET_BAL_06092019 values ('248336554');
+insert into TEMP_FIX_NET_BAL_06092019 values ('279245237');
+insert into TEMP_FIX_NET_BAL_06092019 values ('295992978');
+insert into TEMP_FIX_NET_BAL_06092019 values ('298582800');
+insert into TEMP_FIX_NET_BAL_06092019 values ('324664291');
+insert into TEMP_FIX_NET_BAL_06092019 values ('340455641');
+insert into TEMP_FIX_NET_BAL_06092019 values ('350864708');
+insert into TEMP_FIX_NET_BAL_06092019 values ('386916639');
+insert into TEMP_FIX_NET_BAL_06092019 values ('399852185');
+insert into TEMP_FIX_NET_BAL_06092019 values ('421208869');
+insert into TEMP_FIX_NET_BAL_06092019 values ('421274267');
+insert into TEMP_FIX_NET_BAL_06092019 values ('423879972');
+insert into TEMP_FIX_NET_BAL_06092019 values ('424071082');
+insert into TEMP_FIX_NET_BAL_06092019 values ('425094885');
+insert into TEMP_FIX_NET_BAL_06092019 values ('425471661');
+insert into TEMP_FIX_NET_BAL_06092019 values ('528385313');
+insert into TEMP_FIX_NET_BAL_06092019 values ('546727553');
+insert into TEMP_FIX_NET_BAL_06092019 values ('550221071');
+insert into TEMP_FIX_NET_BAL_06092019 values ('564140960');
+insert into TEMP_FIX_NET_BAL_06092019 values ('569126246');
+insert into TEMP_FIX_NET_BAL_06092019 values ('580389609');
+insert into TEMP_FIX_NET_BAL_06092019 values ('597313170');
+insert into TEMP_FIX_NET_BAL_06092019 values ('671318756');
+insert into TEMP_FIX_NET_BAL_06092019 values ('783504095');
+insert into TEMP_FIX_NET_BAL_06092019 values ('789536877');
+insert into TEMP_FIX_NET_BAL_06092019 values ('798077749');
+insert into TEMP_FIX_NET_BAL_06092019 values ('798871638');
+insert into TEMP_FIX_NET_BAL_06092019 values ('936032747');
+insert into TEMP_FIX_NET_BAL_06092019 values ('945561967');
+insert into TEMP_FIX_NET_BAL_06092019 values ('960550077');
+
+commit;
+
