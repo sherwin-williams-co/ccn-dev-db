149477a21499a12c31d0b4adabe21b14ad5ef920
Aneel Maheedhara <aneel.maheedhara@sherwin.com>
2019-07-01 15:53:17 -0400

    CCNAS-4 : Added SELECT Statements.

--- UTILITY/DML/DML_UPDATE_MAILING_DETAILS_CCNAS_4.sql
+++ UTILITY/DML/DML_UPDATE_MAILING_DETAILS_CCNAS_4.sql
@@ -3,6 +3,10 @@ Replace CCN Project Oracle Development Group with CCN Application Support Group 
 Created : 07/01/2019 axm868 -- CCNAS-4
 **********************************************************************************************/
 
+SELECT * FROM MAILING_GROUP;
+
+SELECT * FROM MAILING_DETAILS;
+
 UPDATE MAILING_GROUP
    SET MAIL_ID = REPLACE(MAIL_ID,'ccnoracle.team@sherwin.com','ccnsupport.team@sherwin.com');
 
@@ -10,3 +14,7 @@ UPDATE MAILING_DETAILS
    SET FROM_P = REPLACE(FROM_P,'ccnoracle.team@sherwin.com','ccnsupport.team@sherwin.com');
 
 COMMIT;
+
+SELECT * FROM MAILING_GROUP;
+
+SELECT * FROM MAILING_DETAILS;
