18326f92a3a9cc52941d23560f16a52203efcb16
Aneel Maheedhara <aneel.maheedhara@sherwin.com>
2019-07-01 15:05:02 -0400

    Delete DML_UPDATE_MAILING_DETAILS_CCNAS_4.sql

--- UTILITY/DML/DML_UPDATE_MAILING_DETAILS_CCNAS_4.sql
+++
@@ -1,9 +0,0 @@
-/*********************************************************************************************
-Replace CCN Project Oracle Development Group with CCN Application Support Group in production.
-Created : 07/01/2019 axm868 -- CCNAS-4
-**********************************************************************************************/
-
-UPDATE MAILING_GROUP
-   SET MAIL_ID = 'ccnsupport.team@sherwin.com';
-   
-COMMIT;
