2943c015c9a36f973996477b42cd5dfdcd9f9503
Aneel Maheedhara <aneel.maheedhara@sherwin.com>
2019-07-08 16:00:09 -0400

    CCNCC-2 : Additional Changes

--- CCNDbCode/DDL/DDL_STORE_TABLE_CCNCC_2.sql
+++ CCNDbCode/DDL/DDL_STORE_TABLE_CCNCC_2.sql
@@ -2,10 +2,18 @@
 This script is used to create a new column called
 DESIGNATED_TERMINAL_NUMBER on the STORE Table.
 
+Added similar Alter scripts for STORE_HST and ARC_STORE Tables
+
 Created : 07/08/2019 axm868 CCN Project CCNCC-2....
 Modified: 
 **********************************************************************************/
 SET DEFINE OFF;
 
 ALTER TABLE STORE
+  ADD DESIGNATED_TERMINAL_NUMBER VARCHAR2(5);  
+  
+ALTER TABLE STORE_HST
+  ADD DESIGNATED_TERMINAL_NUMBER VARCHAR2(5);
+  
+ALTER TABLE ARC_STORE
   ADD DESIGNATED_TERMINAL_NUMBER VARCHAR2(5);
--- CCNDbCode/DML/DML_STORE_TABLE_CCNCC_2.sql
+++ CCNDbCode/DML/DML_STORE_TABLE_CCNCC_2.sql
@@ -3,12 +3,16 @@ This script is used to populate the DESIGNATED_TERMINAL_NUMBER Column of STORE
 Table with the first assigned value of TERMINAL_NUMBER Column from
 the TERMINAL Table.
 
+Run this script in COSTCNTR Schema
+
 Created : 07/08/2019 axm868 CCN Project CCNCC-2....
 Modified: 
 **********************************************************************************/
 SET SERVEROUTPUT ON SIZE UNLIMITED
 DECLARE
+    V_BULK_LOAD_LKUP_ID               NUMBER;
 BEGIN
+    POS_DATA_GENERATION.SET_FLAG_POLLING_BULK_LOADS('BULK_STORE_DESG_TML_NBR_LOAD', V_BULK_LOAD_LKUP_ID);
     FOR C1 IN (SELECT COST_CENTER_CODE FROM COST_CENTER WHERE CATEGORY = 'S') LOOP
         UPDATE STORE S
            SET S.DESIGNATED_TERMINAL_NUMBER = (SELECT LPAD(TO_CHAR(MIN(TO_NUMBER(T.TERMINAL_NUMBER))), 5, '0')
@@ -23,4 +27,12 @@ BEGIN
         WHERE COST_CENTER_CODE = C1.COST_CENTER_CODE;
         COMMIT;
     END LOOP;
+EXCEPTION
+    WHEN OTHERS THEN
+        -- The below call to reset the flag should always be the first line of the exception block
+        -- If re-set doesn't happen, polling process will be turned off for that duration. By putting
+	    -- this call in the first line of exception block we make sure no other errors blocks the reset process	 
+        POS_DATA_GENERATION.RESET_FLAG_POLLING_BULK_LOADS(V_BULK_LOAD_LKUP_ID);
+        -- Logging of error
+        errpkg.raise_err(SQLCODE, substr(SQLERRM,1, 500) ||'  BULK_STORE_DESG_TML_NBR_LOAD '  || ' errors found');
 END;
