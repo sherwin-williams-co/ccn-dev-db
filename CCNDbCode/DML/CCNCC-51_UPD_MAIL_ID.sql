ac41403d4a5a73834858edba7a903aac3dd7b35d
Sandeep Chopra <Sandeep.Chopra@sherwin.com>
2019-07-10 14:41:42 -0400

    Committing changes to mail_id

---
+++ CCNDbCode/DML/CCNCC-51_UPD_MAIL_ID.sql
@@ -0,0 +1,27 @@
+/*******************************************************************************
+CCNCC-51: Script to Replace Toni E Thomey <Antoinette.E.Thomey@sherwin.com> with virginia.knight@sherwin.com and Abbey.L.Dorn@sherwin.com 
+Created : 07/10/2019 sxc403
+*******************************************************************************/
+COLUMN GROUP_ID FORMAT A8 HEADING 'Group|Identity';
+COLUMN Mail_ID FORMAT A200 HEADING 'Mail|Identity';
+ 
+ select * from MAILING_GROUP
+    WHERE GROUP_ID IN (SELECT GROUP_ID
+                        FROM MAILING_DETAILS
+                        WHERE MAIL_CATEGORY = 'HIER_TRNSFR_TERRITORY');
+                        
+    UPDATE MAILING_GROUP
+       SET MAIL_ID = 'virginia.knight@sherwin.com;Abbey.L.Dorn@sherwin.com'
+    WHERE GROUP_ID IN (SELECT GROUP_ID
+                        FROM MAILING_DETAILS
+                        WHERE MAIL_CATEGORY = 'HIER_TRNSFR_TERRITORY');
+                        
+ select * from MAILING_GROUP
+    WHERE GROUP_ID IN (SELECT GROUP_ID
+                        FROM MAILING_DETAILS
+                        WHERE MAIL_CATEGORY = 'HIER_TRNSFR_TERRITORY');
+                        
+                        
+ commit;             
+ 
+ 
