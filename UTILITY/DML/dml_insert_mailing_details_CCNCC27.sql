a1cf3b0e45349c3a36cc9717f5df04d3de45cf49
Aneel Maheedhara <aneel.maheedhara@sherwin.com>
2019-07-15 11:57:09 -0400

    CCNCC27 : change to Utility dml CCNCC27

--- UTILITY/DML/dml_insert_mailing_details_CCNCC27.sql
+++ UTILITY/DML/dml_insert_mailing_details_CCNCC27.sql
@@ -14,15 +14,27 @@ SELECT * FROM MAILING_DETAILS;
 
 SELECT * FROM MAILING_FILE_DETAILS;
 
+--Development/Test/QA
 INSERT INTO MAILING_GROUP (GROUP_ID,MAIL_ID) VALUES (138,'ccnoracle.team@sherwin.com');
 
 INSERT INTO MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE)
 VALUES ('CCN_CLOSE_CC_ISSUE_DTLS_FILE_MAIL',
         138,
-		'Report for reasons for failure of closing Cost Center CCCCCC',
-		'ccnoracle.team@sherwin.com',
-		'Please find the attached the report with reasons for failure of closing the store',
-		'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
+        'Report for reasons for failure of closing Cost Center CCCCCC',
+        'ccnoracle.team@sherwin.com',
+        'Please find the attached the report with reasons for failure of closing the store',
+        'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT'); 
+
+--Production
+INSERT INTO MAILING_GROUP (GROUP_ID,MAIL_ID) VALUES (138,'ccnsupport.team@sherwin.com;smis@sherwin.com');
+
+INSERT INTO MAILING_DETAILS (MAIL_CATEGORY,GROUP_ID,SUBJECT,FROM_P,MESSAGE,SIGNATURE)
+VALUES ('CCN_CLOSE_CC_ISSUE_DTLS_FILE_MAIL',
+        138,
+        'Report for reasons for failure of closing Cost Center CCCCCC',
+        'ccnsupport.team@sherwin.com',
+        'Please find the attached the report with reasons for failure of closing the store',
+        'Thanks,'||CHR(10)||'Keith D. Parker'||CHR(10)||'IT Manager'||CHR(10)||'Sherwin Williams - Stores IT');
 		
 INSERT INTO MAILING_FILE_DETAILS VALUES (1,138,'CCN_CLOSE_CC_ISSUE_DTLS_FILE_MAIL','CCN_DATAFILES','A');
 
