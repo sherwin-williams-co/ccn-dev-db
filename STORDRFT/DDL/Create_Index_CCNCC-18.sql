774e037cb9fc1e55f7a8241d4690d15663178620
sxs484 <shikher.saxena@sherwin.com>
2019-07-25 15:02:36 -0400

    CCNCC-18. sxs484 . StoreDraft . version for Test DB.

---
+++ STORDRFT/DDL/Create_Index_CCNCC-18.sql
@@ -0,0 +1,13 @@
+/*
+CCNCC-18 This script to add index on POS_CUSTOMER tables.
+
+Created : 07/11/2019 sxs484 CCN Project Team....
+Changed :
+*/
+
+
+create index DLY_POS_CUSTOMER_IDX on DLY_POS_CUSTOMER ( cost_center_code) ;
+create index CUSTOMER_DETAILS_IDX2 on DLY_POS_CUSTOMER_DETAILS ( cost_center_code) ;
+create index POS_CSTMR_SALES_TAX_IDX on DLY_POS_CSTMR_SALES_TAX ( cost_center_code) ;
+create index POS_CSTMR_FORM_OF_PAY_IDX on DLY_POS_CSTMR_FORM_OF_PAY ( cost_center_code) ;
+
