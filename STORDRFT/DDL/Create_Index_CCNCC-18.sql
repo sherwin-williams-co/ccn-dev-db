46681039975909c7d6c27a7cc15d131b566d08ce
sxs484 <shikher.saxena@sherwin.com>
2019-07-26 09:31:32 -0400

    CNCC-18. sxs484. storedraft updated Index name and Drop obsolete external tables.

--- STORDRFT/DDL/Create_Index_CCNCC-18.sql
+++ STORDRFT/DDL/Create_Index_CCNCC-18.sql
@@ -7,7 +7,6 @@ Changed :
 
 
 create index DLY_POS_CUSTOMER_IDX on DLY_POS_CUSTOMER ( cost_center_code) ;
-create index CUSTOMER_DETAILS_IDX2 on DLY_POS_CUSTOMER_DETAILS ( cost_center_code) ;
-create index POS_CSTMR_SALES_TAX_IDX on DLY_POS_CSTMR_SALES_TAX ( cost_center_code) ;
-create index POS_CSTMR_FORM_OF_PAY_IDX on DLY_POS_CSTMR_FORM_OF_PAY ( cost_center_code) ;
-
+create index DLY_POS_CUSTOMER_DETAILS_IDX on DLY_POS_CUSTOMER_DETAILS ( cost_center_code) ;
+create index DLY_POS_CSTMR_SALES_TAX_IDX on DLY_POS_CSTMR_SALES_TAX ( cost_center_code) ;
+create index DLY_POS_CSTMR_FORM_OF_PAY_IDX on DLY_POS_CSTMR_FORM_OF_PAY ( cost_center_code) ;
