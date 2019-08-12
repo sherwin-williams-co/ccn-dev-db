bc76abeb4449d854f8b37f02a42c5f2407e4cd65
Abhishek K Jha <Abhishek.K.Jha@sherwin.com>
2019-08-12 17:11:46 -0400

    Applying changes on Prod version for deployment

--- CCNDbCode/DDL/TempTables/COST_CENTER_TERRITORY.sql
+++ CCNDbCode/DDL/TempTables/COST_CENTER_TERRITORY.sql
@@ -5,10 +5,10 @@ Purpose    : For dropping and creating the COST_CENTER_TERRITORY table.
              loaded through batch process.
 
 Created : 04/25/2017 gxg192 CCN Project....
-Changed : 11/06/2017 axt754 CCN Project....
-          Added Fields PRICE_DISTRICT,PRICE_DIST_EFFECTIVE_DT,CREDIT_HIERARCHY
-        : 06/28/2018 nxk927 CCN Project....
+Changed : 06/28/2018 nxk927 CCN Project....
           Added POS_NON_STORE_IND Field
+        : 06/26/2019 akj899 CCNCC4 CCN Project ....
+          Added TERRITORY_TYPE_OF_BUSN_SGMNT_CD Field 
         : 06/26/2019 akj899 CCNCC4 CCN Project ....
           Added TERRITORY_TYPE_OF_BUSN_SGMNT_CD Field
 */
@@ -17,14 +17,14 @@ DROP TABLE COST_CENTER_TERRITORY;
 
 CREATE TABLE COST_CENTER_TERRITORY
    (
-      COST_CENTER_CODE                  VARCHAR2(6),
-      COST_CENTER_NAME                  VARCHAR2(35),
-      CATEGORY                          VARCHAR2(1),
-      ENTITY_TYPE                       VARCHAR2(2),
-      STATEMENT_TYPE                    VARCHAR2(2),
-      COUNTRY_CODE                      VARCHAR2(3),
-      TRANSPORT_TYPE                    VARCHAR2(5),
-      BEGIN_DATE                        VARCHAR2(8),
+      COST_CENTER_CODE         VARCHAR2(6),
+      COST_CENTER_NAME         VARCHAR2(35),
+      CATEGORY                 VARCHAR2(1),
+      ENTITY_TYPE              VARCHAR2(2),
+      STATEMENT_TYPE           VARCHAR2(2),
+      COUNTRY_CODE             VARCHAR2(3),
+      TRANSPORT_TYPE           VARCHAR2(5),
+      BEGIN_DATE               VARCHAR2(8),
       OPEN_DATE                         VARCHAR2(8),
       MOVE_DATE                         VARCHAR2(8),
       CLOSE_DATE                        VARCHAR2(8),
@@ -77,9 +77,6 @@ CREATE TABLE COST_CENTER_TERRITORY
       FACTS_DIVISION                    VARCHAR2(100),
       LEGACY_GL_DIVISION                VARCHAR2(100),
       GLOBAL_HIERARCHY                  VARCHAR2(100),
-      PRICE_DISTRICT                    VARCHAR2(100),
-      PRICE_DIST_EFFECTIVE_DT           VARCHAR2(8),
-      CREDIT_HIERARCHY                  VARCHAR2(100),
       MANAGER_ID                        VARCHAR2(100),
       POS_NON_STORE_IND                 VARCHAR2(1)
    )
@@ -91,75 +88,24 @@ CREATE TABLE COST_CENTER_TERRITORY
       logfile "CCN_LOAD_FILES":'COST_CENTER_TERRITORY.log'
       FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LDRTRIM
       MISSING FIELD VALUES ARE NULL
-      REJECT ROWS WITH ALL NULL FIELDS (COST_CENTER_CODE,
-                                        COST_CENTER_NAME,
-                                        CATEGORY,
-                                        ENTITY_TYPE,
-                                        STATEMENT_TYPE,
-                                        COUNTRY_CODE,
-                                        TRANSPORT_TYPE,
-                                        BEGIN_DATE,
-                                        OPEN_DATE,
-                                        MOVE_DATE,
-                                        CLOSE_DATE,
-                                        FINANCIAL_CLOSE_DATE,
-                                        POS_PROG_VER_EFF_DATE,
-                                        UPS_ZONE_CODE,
-                                        RPS_ZONE_CODE,
-                                        CURRENCY_CODE,
-                                        POS_PROG_VER_NBR,
-                                        LEASE_OWN_CODE,
-                                        MISSION_TYPE_CODE,
-                                        DUNS_NUMBER,
-                                        PRI_LOGO_GROUP_IND,
-                                        SCD_LOGO_GROUP_IND,
-                                        BANKING_TYPE,
-                                        DEPOSIT_BAG_REORDER,
-                                        DEPOSIT_TICKET_REORDER,
-                                        POP_KIT_CODE,
-                                        GLOBAL_HIERARCHY_IND,
-                                        STD_COST_IDENTIFIER,
-                                        PRIM_COST_IDENTIFIER,
-                                        STATUS_CODE,
-                                        STATUS_EFFECTIVE_DATE,
-                                        STATUS_EXPIRATION_DATE,
-                                        TYPE_CODE,
-                                        TYPE_EFFECTIVE_DATE,
-                                        TYPE_EXPIRATION_DATE,
-                                        TERRITORY_SLS_MGR_CODE,
-                                        TERRITORY_CATEGORY,
-                                        LEASE_CAR_INDICATOR,
-                                        TERRITORY_TYPE_BUSN_CODE,
-                                        TERRITORY_TYP_OF_BUSN_SGMNT_CD,
-                                        HOME_STORE,
-                                        PRI_PHONE_NUMBER_TYPE,
-                                        PRI_PHONE_AREA_CODE,
-                                        PRI_PHONE_NUMBER,
-                                        PRI_PHONE_EXTENSION,
-                                        SCD_PHONE_NUMBER_TYPE,
-                                        SCD_PHONE_AREA_CODE,
-                                        SCD_PHONE_NUMBER,
-                                        SCD_PHONE_EXTENSION,
-                                        FAX_PHONE_NUMBER_TYPE,
-                                        FAX_PHONE_AREA_CODE,
-                                        FAX_PHONE_NUMBER,
-                                        FAX_PHONE_EXTENSION,
-                                        ADMIN_TO_SALES_AREA,
-                                        ADMIN_TO_SALES_DISTRICT,
-                                        ADMIN_TO_SALES_DIVISION,
-                                        ALTERNATE_DAD,
-                                        FACTS_DIVISION,
-                                        LEGACY_GL_DIVISION,
-                                        GLOBAL_HIERARCHY,
-                                        PRICE_DISTRICT,
-                                        PRICE_DIST_EFFECTIVE_DT,
-                                        CREDIT_HIERARCHY,
-                                        MANAGER_ID,
-                                        POS_NON_STORE_IND)
-                     )
-      LOCATION
-       ( 'COST_CENTER_TERRITORY.csv'
-       )
-    )
-   REJECT LIMIT UNLIMITED ;
-            
+      REJECT ROWS
+WITH ALL NULL FIELDS (COST_CENTER_CODE, COST_CENTER_NAME, CATEGORY, ENTITY_TYPE
+   , STATEMENT_TYPE, COUNTRY_CODE, TRANSPORT_TYPE, BEGIN_DATE, OPEN_DATE,
+   MOVE_DATE, CLOSE_DATE, FINANCIAL_CLOSE_DATE, POS_PROG_VER_EFF_DATE,
+   UPS_ZONE_CODE, RPS_ZONE_CODE, CURRENCY_CODE, POS_PROG_VER_NBR,
+   LEASE_OWN_CODE, MISSION_TYPE_CODE, DUNS_NUMBER, PRI_LOGO_GROUP_IND,
+   SCD_LOGO_GROUP_IND, BANKING_TYPE, DEPOSIT_BAG_REORDER,
+   DEPOSIT_TICKET_REORDER, POP_KIT_CODE, GLOBAL_HIERARCHY_IND,
+   STD_COST_IDENTIFIER, PRIM_COST_IDENTIFIER, STATUS_CODE,
+   STATUS_EFFECTIVE_DATE, STATUS_EXPIRATION_DATE, TYPE_CODE,
+   TYPE_EFFECTIVE_DATE, TYPE_EXPIRATION_DATE, TERRITORY_SLS_MGR_CODE,
+   TERRITORY_CATEGORY, LEASE_CAR_INDICATOR, TERRITORY_TYPE_BUSN_CODE,TERRITORY_TYP_OF_BUSN_SGMNT_CD,
+   HOME_STORE, PRI_PHONE_NUMBER_TYPE, PRI_PHONE_AREA_CODE, PRI_PHONE_NUMBER,
+   PRI_PHONE_EXTENSION, SCD_PHONE_NUMBER_TYPE, SCD_PHONE_AREA_CODE,
+   SCD_PHONE_NUMBER, SCD_PHONE_EXTENSION, FAX_PHONE_NUMBER_TYPE,
+   FAX_PHONE_AREA_CODE, FAX_PHONE_NUMBER, FAX_PHONE_EXTENSION,
+   ADMIN_TO_SALES_AREA, ADMIN_TO_SALES_DISTRICT, ADMIN_TO_SALES_DIVISION,
+   ALTERNATE_DAD, FACTS_DIVISION, LEGACY_GL_DIVISION, GLOBAL_HIERARCHY,
+   MANAGER_ID,POS_NON_STORE_IND) ) LOCATION ( 'COST_CENTER_TERRITORY.csv' )
+   )
+   REJECT LIMIT UNLIMITED;
