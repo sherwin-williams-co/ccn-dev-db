fc241cd4e0e1931e200e8a0a90d2df5fda1ba469
Sandeep Chopra <Sandeep.Chopra@sherwin.com>
2019-08-19 16:11:09 -0400

    committing for CCNBN-15

--- BANKING/SQL/CCNBN15-TerminalNo_FullTableUpdate.sql
+++ BANKING/SQL/CCNBN15-TerminalNo_FullTableUpdate.sql
@@ -10,23 +10,31 @@ SET ECHO ON
 SET VERIFY OFF
 SET HEADING OFF
 
-SELECT 'Count of Rows with trailing 0 in Terminal Number BEFORE UPDATE :' FROM DUAL;
+-- Count of Rows with trailing 0 in Terminal Number BEFORE UPDATE :
 
   SELECT COUNT(*)
     FROM POS_DPST_TICKET_COUNTS
-   WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
+   where LENGTH(TERMINAL_NUMBER) > 5
+     and SUBSTR(TERMINAL_NUMBER,-1) = '0';
    
    UPDATE POS_DPST_TICKET_COUNTS
-    SET TERMINAL_NUMBER = RTRIM(TERMINAL_NUMBER,'0')
-    WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
-    
- SELECT 'Count of Rows with trailing 0 in Terminal Number AFTER UPDATE :' FROM DUAL;   
+    SET TERMINAL_NUMBER = substr(TERMINAL_NUMBER,1,5)
+   where LENGTH(TERMINAL_NUMBER) > 5
+     and SUBSTR(TERMINAL_NUMBER,-1) = '0';
+
+-- Count of Rows with trailing 0 in Terminal Number AFTER UPDATE :
  
    SELECT COUNT(*)
     FROM POS_DPST_TICKET_COUNTS
-   WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
+   where LENGTH(TERMINAL_NUMBER) > 5
+     and SUBSTR(TERMINAL_NUMBER,-1) = '0';
    
- COMMIT;
+COMMIT;
+ 
+
+
+
+
 
 
 
