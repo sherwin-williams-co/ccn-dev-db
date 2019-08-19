4a8a16ccfce55b6d85709c43fe17988f3d877994
Sandeep Chopra <Sandeep.Chopra@sherwin.com>
2019-08-19 10:25:09 -0400

    comitting for CCNBN15 changes

---
+++ BANKING/SQL/CCNBN15-TerminalNo_FullTableUpdate.sql
@@ -0,0 +1,27 @@
+set serveroutput on size unlimited
+set linesize 300
+set pagesize 500
+SET ECHO ON
+SET VERIFY OFF
+SET HEADING OFF
+
+SELECT 'Count of Rows with trailing 0 in Terminal Number BEFORE UPDATE :' FROM DUAL;
+
+  SELECT COUNT(*)
+    FROM POS_DPST_TICKET_COUNTS
+   WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
+   
+   UPDATE POS_DPST_TICKET_COUNTS
+    SET TERMINAL_NUMBER = RTRIM(TERMINAL_NUMBER,'0')
+    WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
+    
+ SELECT 'Count of Rows with trailing 0 in Terminal Number AFTER UPDATE :' FROM DUAL;   
+   SELECT COUNT(*)
+    FROM POS_DPST_TICKET_COUNTS
+   WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
+   
+ COMMIT;
+
+
+
+
