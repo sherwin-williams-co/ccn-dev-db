083e2ac3c05e5be42aec39dfc39b04532881f0ae
Sandeep Chopra <Sandeep.Chopra@sherwin.com>
2019-08-19 13:19:53 -0400

    Comitting for CCNBN-15

--- BANKING/SQL/CCNBN15-TerminalNo_FullTableUpdate.sql
+++ BANKING/SQL/CCNBN15-TerminalNo_FullTableUpdate.sql
@@ -1,3 +1,8 @@
+/*******************************************************************************
+CCNBN-15 : One time datafix script to remove trailing 0 from  the terminal_number column of the 
+POS_DPST_TICKET_COUNTS table as this is no longer needed. 
+Created : 08/19/2019 sxc403
+*******************************************************************************/
 set serveroutput on size unlimited
 set linesize 300
 set pagesize 500
@@ -16,6 +21,7 @@ SELECT 'Count of Rows with trailing 0 in Terminal Number BEFORE UPDATE :' FROM D
     WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
     
  SELECT 'Count of Rows with trailing 0 in Terminal Number AFTER UPDATE :' FROM DUAL;   
+ 
    SELECT COUNT(*)
     FROM POS_DPST_TICKET_COUNTS
    WHERE SUBSTR(TERMINAL_NUMBER,-1) = '0';
