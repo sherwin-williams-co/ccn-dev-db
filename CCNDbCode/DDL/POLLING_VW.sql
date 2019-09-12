18b3215378d8064c67fefe586a22b0b3bc975251
Aneel Maheedhara <aneel.maheedhara@sherwin.com>
2019-09-12 08:49:03 -0400

    CCNCC-37 changes

--- CCNDbCode/DDL/POLLING_VW.sql
+++ CCNDbCode/DDL/POLLING_VW.sql
@@ -1,23 +1,12 @@
-CREATE OR REPLACE VIEW POLLING_VW
-AS
-  SELECT
+CREATE OR REPLACE VIEW POLLING_VW AS
+SELECT 
 /*******************************************************************************
 This View contains all the records from POLLING table
 
 Created  : 10/17/2018 sxg151 CCN Project....
-         : 01/11/2019 pxa852 CCN Project Team...
+Modified : 01/11/2019 pxa852 CCN Project Team...CCNCC-37
            Changed the EFFECTIVE_DATE to POLL_STATUS_EFF_DT
            EXPIRATION_DATE to POLL_STATUS_EXP_DT
 *******************************************************************************/
-       COST_CENTER_CODE,
-       POLLING_STATUS_CODE,
-       MULTICAST_IND,
-       TIME_ZONE,
-       POLLING_IND,
-       NEXT_DOWNLOAD_BLOCK_COUNT,
-       CURRENT_FLAG,
-       POLL_STATUS_EFF_DT,
-       POLL_STATUS_EXP_DT,
-       POLLING_START_DATE,
-       POLLING_STOP_DATE
+       *
   FROM POLLING;
